18d5960bb17ccd690b11d314af0bd4bfd331b2de
Cole Mickens <cole.mickens@gmail.com>
2018-10-02 13:32:50 +0000

    small update

--- utils/azure/az-upload-closure.sh
+++ utils/azure/az-upload-closure.sh
@@ -9,9 +9,9 @@ key="/etc/nixos/secrets/nix-cache.cluster.lol-1-secret"
 export AZURE_STORAGE_CONNECTION_STRING="$(cat /etc/nixos/secrets/kixstorage-secret)"
 
 # build cache
-#mkdir -p "/tmp/nixcache/nar"
-#nix copy --to 'file:///tmp/nixcache' "${target}"
-#nix sign-paths --store 'file:///tmp/nixcache' -k "${key}" "${target}" -r
+mkdir -p "/tmp/nixcache/nar"
+nix copy --to 'file:///tmp/nixcache' "${target}"
+nix sign-paths --store 'file:///tmp/nixcache' -k "${key}" "${target}" -r
 
 # prep staging dir (must do this before docker, else its done as root, I guess)
 rm -rf /tmp/nixcache-upload/*
--- utils/azure/build-upload-xeep.sh
+++ utils/azure/build-upload-xeep.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-NIX_PATH=/etc/nixos:nixpkgs=/etc/nixpkgs-cmpkgs:nixos-config=/etc/nixcfg/devices/xeep/default.nix
+export NIX_PATH=/etc/nixos:nixpkgs=/etc/nixpkgs-cmpkgs:nixos-config=/etc/nixcfg/devices/xeep/default.nix
 result="$(nix-build --no-out-link -A config.system.build.toplevel '<nixpkgs/nixos>')"
 
 ./az-upload-closure.sh ${result}
