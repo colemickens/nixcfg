b70107fc1d9cc7bf6727dfe35e002a4f7c39be89
Cole Mickens <cole@kix.cluster.lol>
2018-10-08 12:37:37 +0000

    stuff

--- devices/all.nix
+++ devices/all.nix
@@ -3,12 +3,19 @@
 let
   nixos = import "${nixpkgs}/nixos";
   mkMachine = c: (nixos { configuration = c; }).system;
+  mkMachine2 = c: (import "${nixpkgs}/nixos/lib/eval-config.nix" {
+    inherit (pkgs) system;
+    modules = [ c ];
+  }).config.system.build.toplevel;
   pkgs = import nixpkgs {};
   patches = import ./xeep/patches.nix { inherit pkgs; };
   result = {
     xeep    = (mkMachine { imports = [ ./xeep/default.nix {}]; });
     xeepV3 = (mkMachine { imports = [ ./xeep/default.nix {} ]; xeep.kernelPatches = [ patches.trackpadPatchV3 ]; });
     xeepV4 = (mkMachine { imports = [ ./xeep/default.nix {} ]; xeep.kernelPatches = [ patches.trackpadPatchV4 ]; });
+
+    altXeep = (mkMachine2 ./xeep/default.nix);
+
     #chimera = (mkMachine ./chimera/default.nix);
     #packet-kube = (mkMachine ./packet-kube/default.nix); # TODO: this needs a custom nixpkgs!
   };
