2b06a7ccfe790cf0dfbf0a47ec4f76d9c058588d
Cole Mickens <cole@kix.cluster.lol>
2018-10-08 10:42:19 +0000

    reliability

--- utils/prep-workspace.sh
+++ utils/prep-workspace.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-# TODO abstract this and make /etc/nixpkgs a link to a worktree for /etc/_nixpkgs
-if [[ ! -d /etc/nixpkgs ]]; then
-  sudo git clone https://github.com/colemickens/nixpkgs /etc/nixpkgs
-  sudo chown -R cole:cole /etc/nixpkgs
-  cd /etc/nixpkgs
-  sudo git worktree add /etc/nixpkgs/sway sway-wip
-  sudo git worktree add /etc/nixpkgs/cmpkgs cmpkgs
-  sudo git checkout kata3
-fi
-
-sudo chown -R cole:cole /etc/nixcfg
-sudo chown -R cole:cole /etc/nixpkgs
-sudo chown -R cole:cole /etc/nixpkgs-sway
-sudo chown -R cole:cole /etc/nixpkgs-cmpkgs
+[[ ! -d /etc/nixpkgs ]] && sudo git clone https://github.com/colemickens/nixpkgs /etc/nixpkgs -b kata3
+[[ ! -d /etc/nixpkgs-sway ]] && sudo git worktree add /etc/nixpkgs/sway sway-wip
+[[ ! -d /etc/nixpkgs-cmpkgs ]] && sudo git worktree add /etc/nixpkgs/cmpkgs cmpkgs
 
 sudo mkdir -p /etc/nixos/nixpkgs-mozilla
-sudo chown -R cole:cole /etc/nixos/nixpkgs-mozilla
-git clone https://github.com/mozilla/nixpkgs-mozilla /etc/nixos/nixpkgs-mozilla
+sudo git clone https://github.com/mozilla/nixpkgs-mozilla /etc/nixos/nixpkgs-mozilla
 
 sudo mkdir -p /etc/nixos/azure-cli-nix
+sudo git clone https://github.com/stesie/azure-cli-nix /etc/nixos/azure-cli-nix
+
+sudo chown -R cole:cole /etc/nixcfg
+sudo chown -R cole:cole /etc/nixpkgs*
+sudo chown -R cole:cole /etc/nixos/nixpkgs-mozilla
 sudo chown -R cole:cole /etc/nixos/azure-cli-nix
-git clone https://github.com/stesie/azure-cli-nix /etc/nixos/azure-cli-nix
 
