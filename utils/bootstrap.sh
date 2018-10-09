71fdd79de7ff5bb457aa40ba1316d6016ded1252
Cole Mickens <cole.mickens@gmail.com>
2018-10-09 03:19:51 -0700

    wip

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+set -euo pipefail
 
 device="${1:-"packet-kube"}"
 
@@ -18,11 +19,6 @@ cd /etc/nixpkgs
 [[ ! -d /etc/nixos/azure-cli-nix ]] && sudo git clone https://github.com/stesie/azure-cli-nix /etc/nixos/azure-cli-nix
 [[ ! -d /etc/nixos/nixpkgs-mozilla ]] && sudo git clone https://github.com/mozilla/nixpkgs-mozilla /etc/nixos/nixpkgs-mozilla
 
-sudo chown -R cole:cole /etc/nixcfg
-sudo chown -R cole:cole /etc/nixpkgs*
-sudo chown -R cole:cole /etc/nixos/nixpkgs-mozilla
-sudo chown -R cole:cole /etc/nixos/azure-cli-nix
-
 # change into the '${device}' configuration now
 export NIX_PATH=nixpkgs=/etc/nixpkgs:nixos-config=/etc/nixos/configuration.nix
 rb="$(nix-build --no-out-link --expr 'with import <nixpkgs/nixos> {}; config.system.build.nixos-rebuild')/bin/nixos-rebuild";
@@ -31,6 +27,10 @@ rb="$(nix-build --no-out-link --expr 'with import <nixpkgs/nixos> {}; config.sys
   --option extra-binary-caches "https://kixstorage.blob.core.windows.net/nixcache https://cache.nixos.org" \
   --option trusted-public-keys "nix-cache.cluster.lol-1:Pa4IudNcMNF+S/CjNt5GmD8vVJBDf8mJDktXfPb33Ak= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
+sudo chown -R cole:cole /etc/nixcfg
+sudo chown -R cole:cole /etc/nixpkgs*
+sudo chown -R cole:cole /etc/nixos/nixpkgs-mozilla
+sudo chown -R cole:cole /etc/nixos/azure-cli-nix
 
-sleep 120
 reboot
+
