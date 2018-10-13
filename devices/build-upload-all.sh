0c3c55bb4031609cdaa16615b9f3bb4aa41dfc6b
Cole Mickens <cole@kix.cluster.lol>
2018-10-13 09:51:01 +0000

    wip

--- devices/build-upload-all.sh
+++ devices/build-upload-all.sh
@@ -13,15 +13,17 @@ nixcfg="${DIR}/.."
 
 nixfiletobuild="${1:-"${nixcfg}/devices/all.nix"}"
 
+unset NIX_PATH
 results="$(\
   nix-build \
+    --no-out-link \
     --option build-cores 0 \
     --option extra-binary-caches "https://kixstorage.blob.core.windows.net/nixcache https://cache.nixos.org" \
     --option trusted-public-keys "nix-cache.cluster.lol-1:Pa4IudNcMNF+S/CjNt5GmD8vVJBDf8mJDktXfPb33Ak= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" \
     "${nixfiletobuild}")"
 
-echo "${result}" | while read -r closure; do
-  ${nixcfg}/utils/nix/ccc.sh "${closure}"
+echo "${results}" | while read -r closure; do
+  ${nixcfg}/utils/nix/cache-closure.sh "${closure}"
 done
 
 ${nixcfg}/utils/azure/upload-cache.sh
