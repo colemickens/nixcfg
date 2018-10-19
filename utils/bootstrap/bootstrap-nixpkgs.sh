d908585607702c0088429e7e9e24ffaa88d80740
System administrator <root@kix.cluster.lol>
2018-10-19 08:50:48 +0000

    fixup bs-nixpkgs

--- utils/bootstrap/bootstrap-nixpkgs.sh
+++ utils/bootstrap/bootstrap-nixpkgs.sh
@@ -3,13 +3,13 @@ set -x
 set -euo pipefail
 
 # clone nixpkgs
-if [[ ! -d /etc/nixpkgs ]]; then
+if [[ ! -d /etc/nixpkgs-raw ]]; then
   sudo git clone --bare https://github.com/colemickens/nixpkgs /etc/nixpkgs-raw
 fi
 
 # other nixpkgs branches we use
 cd /etc/nixpkgs-raw
-for b in sudo git ls-remote --heads origin  | sed 's?.*refs/heads/??' ; do
+for b in $(sudo git ls-remote --heads origin  | sed 's?.*refs/heads/??') ; do
   if [[ ! -d "/etc/nixpkgs-${b}" ]]; then
     sudo git worktree add "/etc/nixpkgs-${b}" "${b}"
   fi
