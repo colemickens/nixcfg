2d2e4d9307b8da0cd721e4e0ab51ecda98fab07b
System administrator <root@kix.cluster.lol>
2018-10-19 18:04:10 +0000

    fixup bootstrap

--- utils/bootstrap/bootstrap.sh
+++ utils/bootstrap/bootstrap.sh
@@ -10,8 +10,6 @@ if [[ ! -d "${nixcfg}" ]]; then
   sudo git clone https://github.com/colemickens/nixcfg /etc/nixcfg
 fi
 
-cd /etc/nixcfg/utils/bootstrap
-
 # link nixos config
 mv /etc/nixos/configuration.nix "/etc/nixos/configuration-old-$(date '+%s').nix" || true
 ln -s "${nixcfg}/modules/config-${device}.nix" /etc/nixos/configuration.nix
@@ -20,7 +18,7 @@ ln -s "${nixcfg}/modules/config-${device}.nix" /etc/nixos/configuration.nix
 sudo chown -R 1000:1000 "${nixcfg}"
 
 ## Bootstrap the nixpkgs branches, etc
-"${nixcfg}/bootstrap-nixpkgs.sh"
+"${nixcfg}/utils/bootstrap/bootstrap-nixpkgs.sh"
 
 # we still need to assume /etc/nixpkgs is the system config
 # for now bootstrap.sh is specific to the nixos device "pktkube" w/ nixpkgs branch "kata"
