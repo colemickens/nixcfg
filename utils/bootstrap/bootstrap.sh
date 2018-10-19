f01f9bf160b6e38d68ab1faec03247c2f5f60a0f
System administrator <root@kix.cluster.lol>
2018-10-19 08:57:55 +0000

    fixup

--- utils/bootstrap/bootstrap.sh
+++ utils/bootstrap/bootstrap.sh
@@ -24,7 +24,9 @@ sudo chown -R 1000:1000 "/etc/nixcfg"
 
 # we still need to assume /etc/nixpkgs is the system config
 # for now bootstrap.sh is specific to the nixos device "pktkube" w/ nixpkgs branch "kata"
-sudo ln -s /etc/nixpkgs-kata /etc/nixpkgs
+if [[ ! -e /etc/nixpkgs ]]; then
+  sudo ln -s /etc/nixpkgs-kata /etc/nixpkgs
+fi
 
 
 # change into the '${device}' configuration now
