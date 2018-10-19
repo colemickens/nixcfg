865f9232d67ff24cfcee2e914b1cc864eb0fabe0
Cole Mickens <cole.mickens@gmail.com>
2018-10-19 04:09:03 -0700

    wip

--- utils/bootstrap/bootstrap-nixpkgs.sh
+++ utils/bootstrap/bootstrap-nixpkgs.sh
@@ -3,17 +3,23 @@ set -x
 set -euo pipefail
 
 # clone nixpkgs
+pushd
 if [[ ! -d /etc/nixpkgs-raw ]]; then
+  sudo mkdir -p /etc/nixpkgs-raw
   sudo git clone --bare https://github.com/colemickens/nixpkgs /etc/nixpkgs-raw
+  cd /etc/nixpkgs-raw
+  sudo git config --local --add remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 fi
+popd
 
 # other nixpkgs branches we use
-cd /etc/nixpkgs-raw
+pushd /etc/nixpkgs-raw
 for b in $(sudo git ls-remote --heads origin  | sed 's?.*refs/heads/??') ; do
   if [[ ! -d "/etc/nixpkgs-${b}" ]]; then
     sudo git worktree add "/etc/nixpkgs-${b}" "${b}"
   fi
 done
+popd
 
 # make my normal user the owner
 sudo chown -R 1000:1000 /etc/nixpkgs*
