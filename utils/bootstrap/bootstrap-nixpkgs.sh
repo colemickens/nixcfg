0508c9e3c9c995174ed6b06873ebba82ab5cd542
Cole Mickens <cole.mickens@gmail.com>
2018-10-19 05:11:28 -0700

    wtf git, can't use bare checkout, so I guess 'blank' branch...

--- utils/bootstrap/bootstrap-nixpkgs.sh
+++ utils/bootstrap/bootstrap-nixpkgs.sh
@@ -5,16 +5,13 @@ set -euo pipefail
 # clone nixpkgs
 if [[ ! -d /etc/nixpkgs-raw ]]; then
   sudo mkdir -p /etc/nixpkgs-raw
-  sudo git clone --bare https://github.com/colemickens/nixpkgs /etc/nixpkgs-raw
+  sudo git clone https://github.com/colemickens/nixpkgs /etc/nixpkgs-raw -b blank
 fi
-
-pushd /etc/nixpkgs-raw
-sudo git config --local --add remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-popd
 
 # other nixpkgs branches we use
 pushd /etc/nixpkgs-raw
 for b in $(sudo git ls-remote --heads origin  | sed 's?.*refs/heads/??') ; do
+  if [[ "${b}" == "blank" ]]; then continue; fi
   if [[ ! -d "/etc/nixpkgs-${b}" ]]; then
     sudo git worktree add "/etc/nixpkgs-${b}" "${b}"
   fi
