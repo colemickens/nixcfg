e688a42eda95df0eab528034b4f1977d180beef6
Cole Mickens <cole.mickens@gmail.com>
2018-11-03 20:14:04 -0700

    bootstrap: patch up

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,31 +4,34 @@ set -euo pipefail
 
 device="${1:-"pktkube"}"
 branch="${2:-"kata"}"
-nixcfg="/etc/nixcfg"
+root="/etc/nixcfg"
 
-# clone nixcfg
-if [[ ! -d "${nixcfg}" ]]; then
-  sudo git clone https://github.com/colemickens/nixcfg /etc/nixcfg
-fi
+# clone root
+[[ ! -d "${root}" ]] && sudo \
+  git clone https://github.com/colemickens/nixcfg "${root}" /etc/root
 
 # link nixos config
-mv /etc/nixos/configuration.nix "/etc/nixos/configuration-old-$(date '+%s').nix" || true
-ln -s "${nixcfg}/modules/config-${device}.nix" /etc/nixos/configuration.nix
+mv /etc/nixos/{configuration.nix,configuration-old-$(date '+%s').nix} || true
+ln -s "${root}/modules/config-${device}.nix" /etc/nixos/configuration.nix
 
 # make my normal user the owner
-sudo chown -R 1000:1000 "${nixcfg}"
+sudo chown -R 1000:1000 "${root}"
 
 ## Bootstrap the nixpkgs branches, etc
-"${nixcfg}/utils/bootstrap/bootstrap-nixpkgs.sh"
+"${root}/utils/bootstrap/bootstrap-nixpkgs.sh"
 
-# we still need to assume /etc/nixpkgs is the system config
-# for now bootstrap.sh is specific to the nixos device "pktkube" w/ nixpkgs branch "kata"
-if [[ ! -e /etc/nixpkgs ]]; then
-  sudo ln -s "/etc/nixpkgs-${branch}" /etc/nixpkgs
-fi
+# make the right branch the active nixpkgs
+sudo ln -s "/etc/nixpkgs-${branch}" /etc/nixpkgs || true
 
-nix build "${nixcfg}/default.nix" -A "${device}"
+# activate our cache
+cachix use
+nix-env -iA cachix -f https://github.com/NixOS/nixpkgs/tarball/889c72032
+cachix use colemickens
 
+# build this devices configuration
+nix build "${root}/default.nix" -A "${device}"
+
+# set NIX_PATH, find nixos-rebuild, call it
 export NIX_PATH=nixpkgs=/etc/nixpkgs:nixos-config=/etc/nixos/configuration.nix
 rb="$(nix-build --no-out-link --expr 'with import <nixpkgs/nixos> {}; config.system.build.nixos-rebuild')/bin/nixos-rebuild";
 sudo -E "${rb}" boot
