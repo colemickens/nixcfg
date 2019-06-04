8b8d1cd81b5daf7a14f758ba33dba5be4e976b05
Cole Mickens <cole.mickens@gmail.com>
2019-06-04 10:19:26 -0700

    fix nixbuild.sh

--- nixbuild.sh
+++ nixbuild.sh
@@ -5,11 +5,7 @@
 # and we rebuild way more than we probably need to otherwise
 
 exec nix-build \
-  --option "extra-binary-caches" "https://cache.nixos.org" \
-  --option "extra-binary-caches" "https://colemickens.cachix.org" \
-  --option "extra-binary-caches" "https://nixpkgs-wayland.cachix.org" \
-  --option "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" \
-  --option "trusted-public-keys" "colemickens.cachix.org-1:oIGbn9aolUT2qKqC78scPcDL6nz7Npgotu644V4aGl4=" \
-  --option "trusted-public-keys" "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" \
+  --option "extra-binary-caches" "https://cache.nixos.org https://colemickens.cachix.org https://nixpkgs-wayland.cachix.org" \
+  --option "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= colemickens.cachix.org-1:oIGbn9aolUT2qKqC78scPcDL6nz7Npgotu644V4aGl4= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" \
   --option "build-cores" "0" \
   "${@}"
