c257af6c1de438e64dd994a7b6a4543cea6db432
Cole Mickens <cole.mickens@gmail.com>
2019-08-14 10:56:37 +0000

    cleanup plasma

--- modules/profile-plasma.nix
+++ modules/profile-plasma.nix
@@ -7,18 +7,7 @@ with lib;
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
 
-    nix = {
-      binaryCachePublicKeys = [ "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" ];
-      binaryCaches = [ "https://nixpkgs-wayland.cachix.org" ];
-      trustedUsers = [ "@wheel" "root" ];
-    };
-
     environment.systemPackages = with pkgs; [
-      obs-studio
-      wlrobs
-
-      ## nixpkgs-wayland README:
-      xwayland
     ];
   };
 }
