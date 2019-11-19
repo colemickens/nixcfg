3d2a29a43cbb8858031d179dcfe65750c179a47f
Cole Mickens <cole.mickens@gmail.com>
2019-11-18 22:27:49 -0800

    fixup loremipsum mnts

--- modules/loremipsum-media/rclone-mnt.nix
+++ modules/loremipsum-media/rclone-mnt.nix
@@ -10,10 +10,10 @@ let
       StartLimitInterval = "60s";
       StartLimitBurst = 3;
       ExecStartPre = [
-        "-${pkgs.fuse}/bin/fusermount -uz /mnt/${target}"
-        "${pkgs.coreutils}/bin/mkdir -p /mnt/${target}"
+        "-${pkgs.fuse}/bin/fusermount -uz /mnt/rclone/${target}"
+        "${pkgs.coreutils}/bin/mkdir -p /mnt/rclone/${target}"
       ];
-      ExecStart = "${c.rclone-lim-mount}/bin/rclone-lim-mount --allow-other ${target}: /mnt/${target}";
+      ExecStart = "${c.rclone-lim-mount}/bin/rclone-lim-mount --allow-other ${target}: /mnt/rclone/${target}";
       ExecStop = "${pkgs.fuse}/bin/fusermount -uz /mnt/rclone/${target}";
       Restart = "on-failure";
     };
