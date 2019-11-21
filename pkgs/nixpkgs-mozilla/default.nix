b2fd72bac7a46f88f2cc74a479ba793eac2eca4d
Cole Mickens <cole.mickens@gmail.com>
2019-11-21 02:24:59 -0800

    pkg import fixups

--- pkgs/nixpkgs-mozilla/default.nix
+++ pkgs/nixpkgs-mozilla/default.nix
@@ -4,4 +4,4 @@ in
 builtins.fetchTarball {
   url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${metadata.rev}.tar.gz";
   sha256 = metadata.sha256;
-};
+}
--- pkgs/nixpkgs-wayland/default.nix
+++ pkgs/nixpkgs-wayland/default.nix
@@ -4,4 +4,4 @@ in
 builtins.fetchTarball {
   url = "https://github.com/colemickens/nixpkgs-wayland/archive/${metadata.rev}.tar.gz";
   sha256 = metadata.sha256;
-};
+}
