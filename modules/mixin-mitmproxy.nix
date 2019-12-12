b3535816b6df9822f37730024e3829450cc51472
Cole Mickens <cole.mickens@gmail.com>
2019-12-12 09:00:03 +0000

    fixup mitmproxy path detection

--- modules/mixin-mitmproxy.nix
+++ modules/mixin-mitmproxy.nix
@@ -1,12 +1,13 @@
 { lib, pkgs, ... }:
 
 let
+  crtFilePath = "/home/cole/.mitmproxy/mitmproxy-ca-cert.pem";
   crtFile = pkgs.copyPathToStore "/home/cole/.mitmproxy/mitmproxy-ca-cert.pem";
 in
 {
   config = {
     security.pki.certificateFiles =
-      if (lib.pathExists "${crtFile}")
+      if (lib.pathExists "${crtFilePath}")
         then [ "${crtFile}" ]
         else [];
   };
