37bdb540e3f4dc3e2a57c4c4f902b263b4fc5624
Cole Mickens <cole.mickens@gmail.com>
2020-01-04 00:25:47 -0800

    xeep: reenable unifi

--- modules/mixin-unifi.nix
+++ modules/mixin-unifi.nix
@@ -12,15 +12,6 @@
       unifiPackage = pkgs.unifiStable;
 
       jrePackage = pkgs.jre8_headless;
-      #jrePackage = pkgs.jre8_headless.override {
-      #  swingSupport = false; # don't need swing things
-      #  guiSupport = false;   # don't need GUI things
-      #};
-
-      #mongodbPackage = pkgs.mongodb.override {
-      #  jsEngine = "none";    # can't cross compile mozjs
-      #  allocator = "system"; # can't cross compile gperftools
-      #};
     };
 
     networking.firewall.allowedTCPPorts = [ 8080 8443 ];
