27bebf627197c86ddb393e52a0e8a12c06b4026a
Cole Mickens <cole.mickens@gmail.com>
2020-06-29 19:03:20 -0700

    wip

--- .imports/home-manager/cmhm/metadata.nix
+++ .imports/home-manager/cmhm/metadata.nix
@@ -1,8 +1,8 @@
 {
   url = "https://github.com/colemickens/home-manager/archive/cmhm.tar.gz";
   repo_git = "https://github.com/colemickens/home-manager";
-  branch = "master";
-  rev = "7f7348b47049e8d25fb5b98db1d6215f8f643f0d";
+  branch = "cmhm";
+  rev = "4a51d908f0724318b7d7ca496e2968e969c9b40e";
   sha256 = "0snva5f9rw38hdppdavkijgn9ib9njwjr28rxk27qji98sy2f7al";
-  revdate = "2020-06-29 10:21:17 +0200";
+  revdate = "2020-06-28 18:27:56 -0700";
 }
