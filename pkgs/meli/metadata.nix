4b375fde5939763d1d871e444fb993cb0d87eb54
Cole Mickens <cole.mickens@gmail.com>
2020-12-01 14:03:52 -0800

    stash

--- pkgs/meli/metadata.nix
+++ pkgs/meli/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://git.meli.delivery/meli/meli";
   branch = "master";
-  rev = "7db32ff1b382bcdd562909855893dd4252d2e41e";
-  sha256 = "sha256-2ys19qjHW944INQXTCJxYnM4hRJ4U1KrNAYZe8AjG6A=";
-  cargoSha256 = "sha256-Sxbg8IzRJ21cSiMKRXQdbW7FY1MN08UONovUJd+YeXM=";
+  rev = "7efbe6d6926b4c2d4b50170cbb1fb8e16cb5154d";
+  sha256 = "sha256-rjhmJ3WfLWk6v1uldxDJiHfGGnD4un8xA8hC++J6vyI=";
+  cargoSha256 = "sha256-CFy7YJu4QdiDCV5d7Harbb4emeTnLNHqQ36pdil3raQ=";
 }
