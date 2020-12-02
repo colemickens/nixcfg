8dff82ee5cd4623ea7463945f6d833305f6e32ba
Cole Mickens <cole.mickens@gmail.com>
2020-12-01 19:58:24 -0800

    stash

--- pkgs/niche/metadata.nix
+++ pkgs/niche/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/colemickens/niche";
   branch = "master";
-  rev = "c50138017101ed3f96cb247073f95d2e3b068f95";
-  sha256 = "sha256-X1zPUqFkci74lLUGIeAgbPJ4uI0MPU/zfdRo5Ptqfok=";
+  rev = "16228faf4e6935a9cd4e9edc2621cca7cca0b725";
+  sha256 = "sha256-r5hmmjS3ZlrHEuHfBGKTmJI5B51DRgLKR2V4y1fQEfs=";
   vendorSha256 = "sha256-LDLwXmOOeGWUCov/cJt/d2VbFgF19LHCcF+UZxhVNiY=";
 }
