6b3e9c76eec9d4afca514d8897c4ac78cfc461de
Cole Mickens <cole.mickens@gmail.com>
2020-12-02 18:06:12 -0800

    stash

--- mixins/jellyfin.nix
+++ mixins/jellyfin.nix
@@ -1,9 +1,10 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   config = {
     services.jellyfin = {
       enable = true;
+      package = pkgs.jellyfin;
     };
     networking.firewall.allowedTCPPorts = [ 8096 ];
   };
