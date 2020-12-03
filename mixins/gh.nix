cdd37d52b6f5de71d3a1a524a483add96f6b8caa
Cole Mickens <cole.mickens@gmail.com>
2020-12-03 14:31:16 -0800

    stash

--- mixins/gh.nix
+++ mixins/gh.nix
@@ -2,17 +2,15 @@
 
 {
   config = {
-    sops.secrets."cachix.dhall" = {
+    sops.secrets."gh-hosts.yml" = {
       owner = "cole";
       group = "cole";
-      #path = "/home/cole/.config/cachix/cachix.dhall";
     };
     home-manager.users.cole = { pkgs, ... }: {
-      # TODO: finish implementing a `gh` module
       programs.gh = {
         enable = true;
         #hostsFile = secrets."gh-hosts.yml".path;
-        hostsFile = sops.secrets."cachix.dhall".path;
+        hostsFile = config.sops.secrets."cachix.dhall".path;
       };
     };
   };
