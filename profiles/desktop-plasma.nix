1070b09268f5004cbd1cbddfc6471dfdf9275c72
Cole Mickens <cole.mickens@gmail.com>
2020-12-04 16:57:34 -0800

    stash

--- profiles/desktop-plasma.nix
+++ profiles/desktop-plasma.nix
@@ -6,8 +6,8 @@
   ];
   config = {
     services.xserver.enable = true;
-    services.xserver.displayManager.plasma5.enable = true;
-    services.xserver.desktopManager.sddm.enable = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
 
     home-manager.users.cole = { pkgs, ... }: {
       home.sessionVariables = {
