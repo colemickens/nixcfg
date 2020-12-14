2741b111f8f6e85c28b912b040305e0cea846c28
Cole Mickens <cole.mickens@gmail.com>
2020-12-14 14:38:31 -0800

    stash

--- pkgs/neovim/metadata.nix
+++ pkgs/neovim/metadata.nix
@@ -2,7 +2,7 @@
   upattr = "neovim-unwrapped";
   repo_git = "https://github.com/neovim/neovim";
   branch = "nightly";
-  rev = "9c56d8e5f792cf770ebe88d9488549b4106b06a9";
-  sha256 = "sha256-h6WSxerI743ZaVbwyUHnFeRpH4R4q6nHuexL06NPEHo=";
+  rev = "82100a6bdb42cec30060d6c991ab78fd2331fa31";
+  sha256 = "sha256-VCAtCK9n7apZtBM3szqZLoTBC905DOxcKFVXgX84yqA=";
   revdate = "2020-09-14 15:03:02Z";
 }
