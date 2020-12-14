e7fd8eae53d6528731db8cc5f6b31c3391297ce9
Cole Mickens <cole.mickens@gmail.com>
2020-12-13 20:57:29 -0800

    stash

--- mixins/wlsunset.nix
+++ mixins/wlsunset.nix
@@ -4,7 +4,7 @@
   config = {
     home-manager.users.cole = { pkgs, ... }: {
       services.wlsunset = {
-        #enable = (pkgs.system == "x86_64-linux");
+        enable = (pkgs.system == "x86_64-linux");
         longitude = "-122.3321";
         latitude = "47.6062";
         temperature.day = 6500;
