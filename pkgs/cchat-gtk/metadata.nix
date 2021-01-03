6e9a8394ac0e3ce200624a94b90f92e82ce1d982
Cole Mickens <cole.mickens@gmail.com>
2021-01-03 14:48:20 -0800

    auto-update(manual): cchat-gtk: 1247f9d48f1f5b134090a43ef17db5021de1c73f => bcd2de2e494b254df6ad8ae52b56cfeee5717e8e

--- pkgs/cchat-gtk/metadata.nix
+++ pkgs/cchat-gtk/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/diamondburned/cchat-gtk";
   branch = "master";
-  rev = "1247f9d48f1f5b134090a43ef17db5021de1c73f";
-  sha256 = "sha256-ER96j1dGFLJRsLJPO2rfYzzNDsELMGvHCDBN1rAR88I=";
-  vendorSha256 = "sha256-xmwRDBbNKq4zjG94cihRtejbv8viFtThbKZrOljHrYc=";
+  rev = "bcd2de2e494b254df6ad8ae52b56cfeee5717e8e";
+  sha256 = "sha256-WU7HuV8I5OQKtHNdXclDiUmdw9bMOrEHBg3ln5jKTo0=";
+  vendorSha256 = "sha256-EW7LMUEmrTBMxNfmQlsiWS8HcyZDSUDBacWVRkKUV4s=";
 }
