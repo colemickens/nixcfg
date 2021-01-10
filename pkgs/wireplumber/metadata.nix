254df5d293e85d3dab9ad3b1d256957116259c90
Cole Mickens <cole.mickens@gmail.com>
2021-01-10 00:10:29 +0000

    auto-update(manual): wireplumber: 2f0799642d7be50686ed6cb768a13be67f68d265 => bd19c2ddef152d2b3421d8ff3a0f273e73cc38f4

--- pkgs/wireplumber/metadata.nix
+++ pkgs/wireplumber/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://gitlab.freedesktop.org/pipewire/wireplumber.git/";
   branch = "master";
-  rev = "2f0799642d7be50686ed6cb768a13be67f68d265";
-  sha256 = "sha256-W25PJRPPVi3h8671+cyZye/3M4X5kbZTL9gyrneRjgo=";
+  rev = "bd19c2ddef152d2b3421d8ff3a0f273e73cc38f4";
+  sha256 = "sha256-U95dy+GuYiGTTIVBqOWCQfMYxtm3joUetfHnXekDT6Y=";
 }
