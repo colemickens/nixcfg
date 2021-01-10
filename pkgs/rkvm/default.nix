b744dde866c0be5fcb178d64f9c2a92ddf7c18d3
Cole Mickens <cole.mickens@gmail.com>
2021-01-10 00:20:17 +0000

    rkvm: update from pr

--- pkgs/rkvm/default.nix
+++ pkgs/rkvm/default.nix
@@ -1,34 +1,35 @@
-{ stdenv, rustPlatform, fetchFromGitHub
-, pkg-config, libevdev, openssl, llvmPackages_latest, linuxHeaders }:
+{ stdenv, lib, rustPlatform, fetchFromGitHub
+, pkg-config, libevdev, openssl, llvmPackages, linuxHeaders
+}:
 
-let
-  metadata = import ./metadata.nix;
-in
 rustPlatform.buildRustPackage rec {
-  pname = "rkvm";
-  version = metadata.rev;
+  pname = "rkvm-unstable";
+  version = "ec404c69";
 
   src = fetchFromGitHub {
     owner = "htrefil";
-    repo = pname;
-    rev = metadata.rev;
-    sha256 = metadata.sha256;
+    repo = "rkvm";
+    rev = "ec404c69b38f7feff5103f898612734ee8d7ee95";
+    sha256 = "sha256-YZXHbZ71EsSyAtTenQ4rgp1fJbatkk7BW4Cmr/RpyXc=";
   };
 
-  cargoSha256 = metadata.cargoSha256;
+  cargoSha256 = "sha256-/VNnqKSPqAYXS312GaTtpl5j0uOHLfUX8u7LuEDhUTg=";
 
-  postPatch = ''
-    sed -i 's|.clang_arg("-I/usr/include/libevdev-1.0/")|.clang_arg("-I${libevdev}/include/libevdev-1.0").clang_arg("-I${linuxHeaders}/include")|g' ./input/build.rs
-  '';
-
-  nativeBuildInputs = [ pkg-config openssl llvmPackages_latest.libclang ];
-  LIBCLANG_PATH = "${llvmPackages_latest.libclang}/lib";
+  nativeBuildInputs = [ llvmPackages.clang pkg-config openssl ];
   buildInputs = [ libevdev openssl linuxHeaders ];
 
-  meta = with stdenv.lib; {
+  BINDGEN_EXTRA_CLANG_ARGS = "-I${lib.getDev libevdev}/include/libevdev-1.0";
+  LIBCLANG_PATH = "${lib.getLib llvmPackages.libclang}/lib";
+
+  # The libevdev bindings preserve comments from libev, some of which
+  # contain indentation which Cargo tries to interpret as doc tests.
+  doCheck = false;
+
+  meta = with lib; {
     description = "Virtual KVM switch for Linux machines";
     homepage = "https://github.com/htrefil/rkvm";
     license = licenses.mit;
     maintainers = [ maintainers.colemickens ];
   };
 }
+
