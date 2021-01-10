26fd978be79c4e9d81de4f9caa02ec8c87f55af8
Cole Mickens <cole.mickens@gmail.com>
2021-01-10 00:15:08 +0000

    undo cchat

--- pkgs/cchat-gtk/metadata.nix
+++ pkgs/cchat-gtk/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/diamondburned/cchat-gtk";
   branch = "master";
-  rev = "70517ecb88f5ec6c386b0dde600ab6fc67bb87e0";
-  sha256 = "0000000000000000000000000000000000000000000000000000";
+  rev = "70517ecb88f5ec6c386b0dde600ab6fc67bb8777";
+  sha256 = "sha256-EW7LMUEmrTBMxNfmQlsiWS8HcyZDSUDBacWVRkKKK4s=";
   vendorSha256 = "sha256-EW7LMUEmrTBMxNfmQlsiWS8HcyZDSUDBacWVRkKUV4s=";
 }
