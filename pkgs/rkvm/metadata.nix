e38099581a4a29d87f38c68f7508996d6fe2b174
Cole Mickens <cole.mickens@gmail.com>
2021-01-10 00:24:02 +0000

    auto-update(manual): rkvm: ec404c69b38f7feff5103f898612734ee8d7eeee => ec404c69b38f7feff5103f898612734ee8d7ee95

--- pkgs/rkvm/metadata.nix
+++ pkgs/rkvm/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "ec404c69b38f7feff5103f898612734ee8d7ee95";
   sha256 = "sha256-YZXHbZ71EsSyAtTenQ4rgp1fJbatkk7BW4Cmr/RpyXc=";
-  cargoSha256 = "sha256-1sxP74xt3DGCAhY6Dolaz2JB3Q6CTvtkgln08AMGmc0=";
+  cargoSha256 = "sha256-5uXHLC+f4idlBkOvWO3y1u24f/WSjv/DHfx1LaU8VGc=";
 }
