33380af4d1e13a88fe848faa17fe33ebd25f4daf
Cole Mickens <cole.mickens@gmail.com>
2021-01-14 14:57:55 -0800

    auto-update(manual): headscale: 70517ecb88f5ec6c386b0dde600ab6fc67bb87e0 => 92f33c15b74507ac8bae186714dd00ff9f4c1f0e

---
+++ pkgs/headscale/default.nix
@@ -0,0 +1,38 @@
+{ stdenv, buildGoModule, fetchFromGitHub
+, pkg-config, makeDesktopItem
+, gtk3, libhandy, gspell
+, wrapGAppsHook
+}:
+
+let metadata = import ./metadata.nix; in
+buildGoModule rec {
+  pname = "headscale";
+  version = metadata.rev;
+
+  src = fetchFromGitHub {
+    owner = "juanfont";
+    repo = pname;
+    rev = metadata.rev;
+    sha256 = metadata.sha256;
+  };
+
+  vendorSha256 = metadata.vendorSha256;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+  # buildInputs = [
+  #   gtk3 libhandy libhandy gspell
+  #   wrapGAppsHook
+  # ];
+
+  subPackages = [ "cmd/headscale" ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/juanfont/headscale";
+    description = "An open source implementation of the Tailscale coordination server";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ colemickens ];
+    platforms = platforms.linux;
+  };
+}
---
+++ pkgs/headscale/metadata.nix
@@ -0,0 +1,7 @@
+{
+  repo_git = "https://github.com/juanfont/headscale";
+  branch = "master";
+  rev = "92f33c15b74507ac8bae186714dd00ff9f4c1f0e";
+  sha256 = "sha256-glcQ3hwwNKe5AsSIZuuxdRyAjsxoyDaf8Q2docY2yoc=";
+  vendorSha256 = "sha256-77xwUcKd59cK1H6ROAgytPCR8zWcXoGhqyAXXLXknPc=";
+}
