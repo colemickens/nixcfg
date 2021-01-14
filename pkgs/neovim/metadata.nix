0b64e3bdab40a7cbfa0cce889a09b142fc2a91a8
Cole Mickens <cole.mickens@gmail.com>
2021-01-14 12:51:54 -0600

    auto-update(manual): neovim: 0af5a56e47b543c8497eaa71ca8ff6900059d062 => 2390a0d3c412060eeeec2c7076a2014e2834eb10

--- pkgs/neovim/metadata.nix
+++ pkgs/neovim/metadata.nix
@@ -2,6 +2,6 @@
   upattr = "neovim-unwrapped";
   repo_git = "https://github.com/neovim/neovim";
   branch = "master";
-  rev = "0af5a56e47b543c8497eaa71ca8ff6900059d062";
-  sha256 = "sha256-ejRVixcybmjGIh4Hy5uON1DVDLXP5WjEXGAJCjiiLQU=";
+  rev = "2390a0d3c412060eeeec2c7076a2014e2834eb10";
+  sha256 = "sha256-/kXgKQOfRd3Sn8LDqflFaqn87ORLUpqoVLxa4IRy5C4=";
 }
