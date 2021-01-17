8a98edd8e21ca4eae5318bae1ab808145fbda5e8
Cole Mickens <cole.mickens@gmail.com>
2021-01-17 17:21:56 -0600

    auto-update(manual): rkvm: b670f14e43f7ebbfae230a0f1f705aa2f3e0cc0a => bf133665eb446d9f128d02e4440cc67bce50f666

--- pkgs/rkvm/metadata.nix
+++ pkgs/rkvm/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/htrefil/rkvm";
   branch = "master";
-  rev = "b670f14e43f7ebbfae230a0f1f705aa2f3e0cc0a";
-  sha256 = "sha256-2ntj5hDBuB9y8JWLsUcUrvEL+eOPDPh1uI4ufcCLtug=";
-  cargoSha256 = "sha256-xKiH+0CXSaStkpyh81vC3CyPvVhv64E3xT1dX60fJ5g=";
+  rev = "bf133665eb446d9f128d02e4440cc67bce50f666";
+  sha256 = "sha256-naWoLo3pPETkYuW4HATkrfjGcEHSGAAXixgp1HOlIcg=";
+  cargoSha256 = "sha256-+DMuYyt7baPrI6kiQxObwHHPsLhPBqXxouAF5xXXihA=";
 }
