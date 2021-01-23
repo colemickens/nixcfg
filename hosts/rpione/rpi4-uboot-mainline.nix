1f3a7b5619c4c3e0922d0a1621f184b276faef02
Cole Mickens <cole.mickens@gmail.com>
2021-01-23 00:07:33 -0600

    stash

--- hosts/rpione/rpi4-uboot-mainline.nix
+++ hosts/rpione/rpi4-uboot-mainline.nix
@@ -3,8 +3,8 @@
 let
   moduleList = [
     # cargo culted:
-    "pcie_brcmstb" "bcm_phy_lib" "broadcom" "mdio_bcm_unimac" "genet"
-    "vc4" "bcm2835_dma" "i2c_bcm2835" # Allows early (earlier) modesetting for the Raspberry Pi
+    #"pcie_brcmstb" "bcm_phy_lib" "broadcom" "mdio_bcm_unimac" "genet"
+    "vc4" "bcm2835_dma" "i2c_bcm2835"
 
     # try enable usb storage?
     "xhci_pci" "usb_storage"
@@ -19,23 +19,22 @@ in {
       loader.grub.enable = false;
       loader.raspberryPi.enable = true;
       loader.raspberryPi.version = 4;
-      #kernelPackages = pkgs.lib.mkForce pkgs.linuxPackages_rpi4;
+      kernelPackages = pkgs.lib.mkForce pkgs.linuxPackages_latest;
       loader.raspberryPi.uboot.enable = true;
       loader.raspberryPi.firmwareConfig = ''
-        dtoverlay=vc4-fkms-v3d
-        dtoverlay=disable-bt
+        disable_overscan=1
       '';
-      kernelPackages = pkgs.lib.mkForce pkgs.linuxPackages_latest;
       supportedFilesystems = [ "zfs" ];
       initrd.availableKernelModules = moduleList;
       kernelModules = moduleList;
 
       kernelParams = [
-        "cma=64M"
-        "earlycon=pl011,mmio32,0xfe201000"
-        "console=ttyS0,115200n8"
-        "console=ttyAMA0,115200n8"
-        "console=tty0"
+        #"cma=64M"
+        #"8250.nr_uarts=1"
+        #"earlycon=pl011,mmio32,0xfe201000"
+        #"console=ttyAMA0,115200n8"
+        #"console=tty1"
+        "console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"
       ];
 
       consoleLogLevel = lib.mkDefault 7;
