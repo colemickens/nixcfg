2630b0db6de8cf383fa7c9a1a4acbf1c15be9ac4
Cole Mickens <cole.mickens@gmail.com>
2021-01-24 19:40:47 -0600

    auto-update(manual): mirage-im: 844321974c726af22153afd2f7df01c29add265c => 71785911a01678cc4ef215f75b136bbeef2b6b23

--- pkgs/mirage-im/metadata.nix
+++ pkgs/mirage-im/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/mirukana/mirage";
   branch = "master";
-  rev = "844321974c726af22153afd2f7df01c29add265c";
-  sha256 = "sha256-J+siFuvoKCtw3X98S2bXN/o+8ZUQxdmS8JoFO9lhrl0=";
+  rev = "71785911a01678cc4ef215f75b136bbeef2b6b23";
+  sha256 = "sha256-rJIeVsYwrlkwEbtg0rDW3Oh6ce7ZBzUMZJdpLcLEXbk=";
   revdate = "2020-09-01 17:38:30Z";
 }
