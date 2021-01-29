b6807db8ec1013f360c9830aacf3551557203bd8
Cole Mickens <cole.mickens@gmail.com>
2021-01-29 01:44:33 +0000

    common: configRev

--- mixins/common.nix
+++ mixins/common.nix
@@ -16,7 +16,7 @@ with lib;
       then inputs.self.rev
       else throw "Refusing to build from a dirty Git tree!";
 
-    system.nixos.label = "${config.system.nixos.version}-${pkgs.lib.substring 0 8 config.system.configurationRevision}";
+    system.nixos.label = "${config.networking.hostName}-${config.system.nixos.version}-${pkgs.lib.substring 0 8 config.system.configurationRevision}";
 
     boot = {
       cleanTmpDir = true;
