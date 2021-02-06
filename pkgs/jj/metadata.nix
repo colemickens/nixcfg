8948331babb0ca221a5311d007a30f6a1637cce6
Cole Mickens <cole.mickens@gmail.com>
2021-02-06 11:34:56 +0000

    auto-update(manual): jj: 4ecbd893789aa3ecc96aa2fab275685c11538888 => 4ecbd893789aa3ecc96aa2fab275685c115388b0

--- pkgs/jj/metadata.nix
+++ pkgs/jj/metadata.nix
@@ -3,5 +3,5 @@
   branch = "main";
   rev = "4ecbd893789aa3ecc96aa2fab275685c115388b0";
   sha256 = "sha256-LVvybuD2629g5L/sGEiIxbzQJD/0hsFu6SqLnMGjuqE=";
-  cargoSha256 = "sha256-w5djQgRdcoP9CQzRt22v5EB0xAj1yOihKpX1G/TJcOc=";
+  cargoSha256 = "sha256-EUPPFt0SVdFdKh0dazCXZQd3qXTb1oU2dxRMMHdlQbc=";
 }
