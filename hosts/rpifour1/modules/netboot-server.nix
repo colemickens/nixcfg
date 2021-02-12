f5d7d4cbd1047454bbd1b5c8641e4e13d36ef567
Cole Mickens <cole.mickens@gmail.com>
2021-02-12 15:24:04 -0800

    rpifour1/2: netboot wip

--- hosts/rpifour1/modules/netboot-server.nix
+++ hosts/rpifour1/modules/netboot-server.nix
@@ -77,7 +77,8 @@ let
   '';
 
   earlycon = "earlycon=uart8250,mmio32,0xfe215040";
-  console = "console=ttyS0,115200";
+  #console = "console=ttyS0,115200";
+  console = "";
   cmdline = pkgs.writeText "cmdline.txt" ''
     ${lib.optionalString (earlycon!="") earlycon} ${lib.optionalString (console!="") console} root=/dev/nfs nfsroot=192.168.1.2:/rpifour2,vers=4.1,proto=tcp ro ip=dhcp rootwait elevator=deadline init=${rpifour2_system.config.system.build.toplevel}/init isolcpus=3
   '';
