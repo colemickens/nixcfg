078ba5cf5117ac2ff632d2b8c55d1d06eaa4ee03
Cole Mickens <cole.mickens@gmail.com>
2021-02-18 18:41:40 -0800

    rpifour1: wip netboot

--- hosts/rpifour1/modules/netboot-server.nix
+++ hosts/rpifour1/modules/netboot-server.nix
@@ -180,7 +180,7 @@ in
     services.nfs.server = {
       enable = true;
       exports = ''
-        /export             192.168.1.0/24(fsid=0,rw,sync,no_subtree_check)
+        /export             192.168.1.0/24(fsid=0,ro)
         /export/rpifour2    192.168.1.0/24(ro,nohide,no_root_squash,insecure,no_subtree_check)
       '';
     };
