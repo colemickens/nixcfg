c1ab7e3bd360e39bc9d334b169ebae8e9a056483
Cole Mickens <cole.mickens@gmail.com>
2021-02-20 21:44:09 -0800

    rpifour1: wip netboot

--- hosts/rpifour1/modules/netboot-server.nix
+++ hosts/rpifour1/modules/netboot-server.nix
@@ -5,8 +5,8 @@ let
   rpifour2_mac = "dc-a6-32-59-d6-f8";
 
   # control whether we boot armv7l (32bit) or aarch64 (64bit) (the config.txt is updated automatically)
-  netbootSystem = "aarch64-linux";
-  #netbootSystem = "armv7l-linux";
+  #netbootSystem = "aarch64-linux";
+  netbootSystem = "armv7l-linux";
   # except armv7l doesn't work yet...
 
   rpifour2_config = ({ config, lib, pkgs, modulesPath, inputs, ... }: {
