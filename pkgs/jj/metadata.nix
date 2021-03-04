458da9936c62406551b8870e404754790abd53eb
Cole Mickens <cole.mickens@gmail.com>
2021-03-03 16:15:22 -0800

    jj: lock due to nightly issue

--- pkgs/jj/metadata.nix
+++ pkgs/jj/metadata.nix
@@ -4,4 +4,5 @@
   rev = "2a531832d6633e38147bb03af579d91e3bbd0c30";
   sha256 = "sha256-iUvShyOBDP7LuR1QYmUGV9yFyO8n+dGtSGZGiHtsJHo=";
   cargoSha256 = "sha256-xJDhmvatA5DjMnQ3jS6mDFasS/jQIhUjklOwMUyVhpU=";
+  skip = true;
 }
