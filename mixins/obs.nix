fe88c0c7c831de8866774c830f4ae64e29ab4b9c
Cole Mickens <cole.mickens@gmail.com>
2021-03-06 04:14:44 -0800

    fix obs?

--- mixins/obs.nix
+++ mixins/obs.nix
@@ -8,7 +8,7 @@
     environment.systemPackages = with pkgs; [
       v4l-utils
 
-      (pkgs.writeScript "obs-v4l2loopback-setup" ''
+      (pkgs.writeScriptBin "obs-v4l2loopback-setup.sh" ''
         set -x
         sudo modprobe v4l2loopback devices=1 video_nr=13 card_label="''${CAMERA_NAME:-"OBS Virtual Camera"}" exclusive_caps=1
       '')
