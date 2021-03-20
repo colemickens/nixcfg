22d4d6bb32d2fd4e3a40307013cb5cd40e5cdb6d
Cole Mickens <cole.mickens@gmail.com>
2021-03-19 22:56:48 -0700

    github ci wip

--- hosts/slynux/hydra.nix
+++ hosts/slynux/hydra.nix
@@ -2,7 +2,7 @@
 
 let
   hydraHostname = "hydra.${config.networking.hostName}.ts.r10e.tech";
-  aarch64_host_key="NDA5NiBTSEEyNTY6cFVEeDF1U3dBWHFYVm1IeDYzd3Q1aHZlYkRvQ1UrU01GYUoxeEI0SndyUSBhYXJjaDY0Lm5peG9zLmNvbW11bml0eSAoUlNBKQoyNTYgU0hBMjU2OmlQTlBGK3dkSmV6bjloejl0bjAyT05wd0FMZW10ZVR2cTVOejV4ZHNOYTggYWFyY2g2NC5uaXhvcy5jb21tdW5pdHkgKEVEMjU1MTkpCg==";
+  aarch64_host_key = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU1VVHo1aTl1NUgyRkhOQW1aSnlvSmZJR3lVbS9IZkdoZnduYzE0MkwzZHMK";
   machinesFile = pkgs.writeText "machines.txt" ''
     localhost x86_64-linux - 4 1 kvm,nixos-test,big-parallel,benchmark 
     colemickens@aarch64.nixos.community aarch64-linux - 4 1 kvm,nixos-test,big-parallel,benchmark - ${aarch64_host_key}
@@ -14,7 +14,7 @@ in {
     system.activationScripts.hydra-queue-runner = {
       text = ''
         mkdir -p /var/lib/hydra/queue-runner/.ssh
-        ln -sf /home/cole/.ssh/config /var/lib/hydra/queue-runner/.ssh/config
+        cp --remove-destination /home/cole/.ssh/config /var/lib/hydra/queue-runner/.ssh/config
       '';
       deps = [];
     };
