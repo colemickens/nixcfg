39515f587838c938bba5d7d303863d2997d202ab
Cole Mickens <cole.mickens@gmail.com>
2021-03-20 21:48:03 -0700

    more hydra

--- hosts/slynux/hydra.nix
+++ hosts/slynux/hydra.nix
@@ -21,29 +21,26 @@ let
     # 1. don't want to actually use aarch64 box for this
     # 2. I think there's version skew issues regarding using
     #    nixUnstable with hydra and aarch64.nixos using nixStable
-    # { 
-    #   hostName = "aarch64.nixos.community";
-    #   sshHostKeyBase64 = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU1VVHo1aTl1NUgyRkhOQW1aSnlvSmZJR3lVbS9IZkdoZnduYzE0MkwzZHMK";
-    #   sshUser = "colemickens";
-    #   #sshKey = # it's just in the /var/lib/hydra/queue-runner/.ssh/id_rsa
-    #   system = "aarch64-linux";
-    #   systems = [ "aarch64-linux" ];
-    #   mandatoryFeatures = [];
-    #   supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
-    #   speedFactor = 1;
-    #   maxJobs = 4;
-    # }
-    {
-      hostName = "rpifour1.ts.r10e.tech";
-      sshUser = "cole";
-      # ❯ echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOocMaAv2g1YK6SBFUYl4azZ0dGRid07D9CN8TQ2CCAa" | base64 -w0
-      sshHostKeyBase64 = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU9vY01hQXYyZzFZSzZTQkZVWWw0YXpaMGRHUmlkMDdEOUNOOFRRMkNDQWEK";
+    { 
+      hostName = "aarch64.nixos.community";
+      sshHostKeyBase64 = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU1VVHo1aTl1NUgyRkhOQW1aSnlvSmZJR3lVbS9IZkdoZnduYzE0MkwzZHMK";
+      sshUser = "colemickens";
       systems = [ "aarch64-linux" ];
-      mandatoryFeatures = [];
-      supportedFeatures = [];
+      supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
       speedFactor = 1;
       maxJobs = 4;
     }
+    # {
+    #   hostName = "rpifour1.ts.r10e.tech";
+    #   sshUser = "cole";
+    #   # ❯ echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOocMaAv2g1YK6SBFUYl4azZ0dGRid07D9CN8TQ2CCAa" | base64 -w0
+    #   sshHostKeyBase64 = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU9vY01hQXYyZzFZSzZTQkZVWWw0YXpaMGRHUmlkMDdEOUNOOFRRMkNDQWEK";
+    #   systems = [ "aarch64-linux" ];
+    #   mandatoryFeatures = [];
+    #   supportedFeatures = [];
+    #   speedFactor = 1;
+    #   maxJobs = 4;
+    # }
   ];
   builder = (import ./hydra-machinestxt-builder.nix { inherit lib; });
   machinesFileText = (builder machinesConfig);
