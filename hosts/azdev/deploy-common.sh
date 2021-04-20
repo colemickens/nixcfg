d927c9961b13ff5ed2bed614df315524af3c3b05
Cole Mickens <cole.mickens@gmail.com>
2021-04-20 15:07:52 -0700

    azdev: deploy: default ssh key

--- hosts/azdev/deploy-common.sh
+++ hosts/azdev/deploy-common.sh
@@ -31,6 +31,8 @@ image_id="$(az image show -g "${IMAGE_GROUP}" -n "${IMAGE_NAME}" -o tsv --query 
 
 az group create -n "${MACHINE_GROUP}" -l "${AZURE_LOCATION}"
 
+AZURE_SSH="${AZURE_SSH:-"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9YAN+P0umXeSP/Cgd5ZvoD5gpmkdcrOjmHdonvBbptbMUbI/Zm0WahBDK0jO5vfJ/C6A1ci4quMGCRh98LRoFKFRoWdwlGFcFYcLkuG/AbE8ObNLHUxAwqrdNfIV6z0+zYi3XwVjxrEqyJ/auZRZ4JDDBha2y6Wpru8v9yg41ogeKDPgHwKOf/CKX77gCVnvkXiG5ltcEZAamEitSS8Mv8Rg/JfsUUwULb6yYGh+H6RECKriUAl9M+V11SOfv8MAdkXlYRrcqqwuDAheKxNGHEoGLBk+Fm+orRChckW1QcP89x6ioxpjN9VbJV0JARF+GgHObvvV+dGHZZL1N3jr8WtpHeJWxHPdBgTupDIA5HeL0OCoxgSyyfJncMl8odCyUqE+lqXVz+oURGeRxnIbgJ07dNnX6rFWRgQKrmdV4lt1i1F5Uux9IooYs/42sKKMUQZuBLTN4UzipPQM/DyDO01F0pdcaPEcIO+tp2U6gVytjHhZqEeqAMaUbq7a6ucAuYzczGZvkApc85nIo9jjW+4cfKZqV8BQfJM1YnflhAAplIq6b4Tzayvw1DLXd2c5rae+GlVCsVgpmOFyT6bftSon/HfxwBE4wKFYF7fo7/j6UbAeXwLafDhX+S5zSNR6so1epYlwcMLshXqyJePJNhtsRhpGLd9M3UqyGDAFoOQ== cardno:000607126708"}"
+
 args=(
   --name "${MACHINE_NAME}"
   --resource-group "${MACHINE_GROUP}"
@@ -39,7 +41,7 @@ args=(
   --attach-data-disks "${DATA_DISK_ID}"
   --admin-username "azureuser"
   --location "${AZURE_LOCATION}"
-  --ssh-key-values "$(ssh-add -L | head -1)"
+  --ssh-key-values "${AZURE_SSH}"
   --os-disk-size-gb "${AZURE_VM_OS_DISK_SIZE}"
   --storage-sku "${AZURE_STORAGE_SKU}"
   --public-ip-address-dns-name "${MACHINE_NAME}"
