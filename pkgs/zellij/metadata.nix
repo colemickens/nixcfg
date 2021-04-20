593c734c76db3a8892ab7b6d0b5a5af73cbd8bd4
Cole Mickens <cole.mickens@gmail.com>
2021-04-20 20:36:00 +0000

    zellij: fix branch

--- pkgs/zellij/metadata.nix
+++ pkgs/zellij/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/zellij-org/zellij";
-  branch = "master";
+  branch = "main";
   rev = "74cc48fc500f7f81a867394e8d6550562500d14f";
   sha256 = "sha256-xtlUOTQ0uD1q3nJvn4zj3czS3/bpDeu4GZ8elC/AHVY=";
   cargoSha256 = "sha256-wzkAM7awlnhjGS1SeWSQjHNuUoDCisWfWX6Rfjw6s9I=";
