369edb350824b0133c5639c2385a5abe47d2dd3d
Cole Mickens <cole.mickens@gmail.com>
2021-04-24 23:30:51 +0000

    azure cleanup

--- hosts/azdev/deploy.sh
+++ hosts/azdev/deploy.sh
@@ -7,18 +7,20 @@ export AZURE_IMAGE_NAME="azdevimg.vhd"
 export AZURE_MACHINE_GROUP="azdev2020vm1"
 export AZURE_MACHINE_NAME="azdev2020vm1"
 
+if [[ "${1}" == "small" ]]; then
+  export AZURE_VM_SIZE="Standard_D4as_v4"
+  export AZURE_VM_OS_DISK_SIZE="128" # this can be larger than the sku allows, since we use a non-ephemeral disk
+  export AZURE_STORAGE_SKU="Premium_LRS"
+  export AZURE_EPHEMERAL_DISK="false"
+  export AZURE_ACCEL_NIC="false"
+elif [[ "${1}" == "large" ]]; then
+  ## big size, boost, expensive, for intense dev seshs brah (ew, forgive me)
+  export AZURE_VM_SIZE="Standard_D16as_v4"
+  export AZURE_EPHEMERAL_DISK="true"
+  export AZURE_ACCEL_NIC="true"
+fi
 
-## small size, cheap, long-running
-export AZURE_VM_SIZE="Standard_D4as_v4"
-export AZURE_VM_OS_DISK_SIZE="128" # this can be larger than the sku allows, since we use a non-ephemeral disk
-export AZURE_STORAGE_SKU="Premium_LRS"
-export AZURE_EPHEMERAL_DISK="false"
-export AZURE_ACCEL_NIC="false"
+export AZURE_SSH="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9YAN+P0umXeSP/Cgd5ZvoD5gpmkdcrOjmHdonvBbptbMUbI/Zm0WahBDK0jO5vfJ/C6A1ci4quMGCRh98LRoFKFRoWdwlGFcFYcLkuG/AbE8ObNLHUxAwqrdNfIV6z0+zYi3XwVjxrEqyJ/auZRZ4JDDBha2y6Wpru8v9yg41ogeKDPgHwKOf/CKX77gCVnvkXiG5ltcEZAamEitSS8Mv8Rg/JfsUUwULb6yYGh+H6RECKriUAl9M+V11SOfv8MAdkXlYRrcqqwuDAheKxNGHEoGLBk+Fm+orRChckW1QcP89x6ioxpjN9VbJV0JARF+GgHObvvV+dGHZZL1N3jr8WtpHeJWxHPdBgTupDIA5HeL0OCoxgSyyfJncMl8odCyUqE+lqXVz+oURGeRxnIbgJ07dNnX6rFWRgQKrmdV4lt1i1F5Uux9IooYs/42sKKMUQZuBLTN4UzipPQM/DyDO01F0pdcaPEcIO+tp2U6gVytjHhZqEeqAMaUbq7a6ucAuYzczGZvkApc85nIo9jjW+4cfKZqV8BQfJM1YnflhAAplIq6b4Tzayvw1DLXd2c5rae+GlVCsVgpmOFyT6bftSon/HfxwBE4wKFYF7fo7/j6UbAeXwLafDhX+S5zSNR6so1epYlwcMLshXqyJePJNhtsRhpGLd9M3UqyGDAFoOQ== cardno:000607126708"
 
-## big size, boost, expensive, for intense dev seshs brah (ew, forgive me)
-export AZURE_VM_SIZE="Standard_D16as_v4"
-export AZURE_EPHEMERAL_DISK="true"
-export AZURE_ACCEL_NIC="true"
-
-../azdev/deploy-common.sh
-
+NIXOS_AZURE="/home/cole/code/nixos-azure"
+nix shell "${NIXOS_AZURE}#azutil" -c "deploy-vm"
