87af31e6fb924bf4260e2e793e53268eba41171f
Cole Mickens <cole.mickens@gmail.com>
2021-04-24 22:31:23 +0000

    azdev deploy

--- hosts/azdev/deploy-common.sh
+++ hosts/azdev/deploy-common.sh
@@ -29,6 +29,10 @@ fi
 
 image_id="$(az image show -g "${IMAGE_GROUP}" -n "${IMAGE_NAME}" -o tsv --query '[id]')"
 
+if [[ "${AZURE_IMAGE_ONLY:-""}" == "true" ]]; then
+  exit 0
+fi
+
 az group create -n "${MACHINE_GROUP}" -l "${AZURE_LOCATION}"
 
 AZURE_SSH="${AZURE_SSH:-"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9YAN+P0umXeSP/Cgd5ZvoD5gpmkdcrOjmHdonvBbptbMUbI/Zm0WahBDK0jO5vfJ/C6A1ci4quMGCRh98LRoFKFRoWdwlGFcFYcLkuG/AbE8ObNLHUxAwqrdNfIV6z0+zYi3XwVjxrEqyJ/auZRZ4JDDBha2y6Wpru8v9yg41ogeKDPgHwKOf/CKX77gCVnvkXiG5ltcEZAamEitSS8Mv8Rg/JfsUUwULb6yYGh+H6RECKriUAl9M+V11SOfv8MAdkXlYRrcqqwuDAheKxNGHEoGLBk+Fm+orRChckW1QcP89x6ioxpjN9VbJV0JARF+GgHObvvV+dGHZZL1N3jr8WtpHeJWxHPdBgTupDIA5HeL0OCoxgSyyfJncMl8odCyUqE+lqXVz+oURGeRxnIbgJ07dNnX6rFWRgQKrmdV4lt1i1F5Uux9IooYs/42sKKMUQZuBLTN4UzipPQM/DyDO01F0pdcaPEcIO+tp2U6gVytjHhZqEeqAMaUbq7a6ucAuYzczGZvkApc85nIo9jjW+4cfKZqV8BQfJM1YnflhAAplIq6b4Tzayvw1DLXd2c5rae+GlVCsVgpmOFyT6bftSon/HfxwBE4wKFYF7fo7/j6UbAeXwLafDhX+S5zSNR6so1epYlwcMLshXqyJePJNhtsRhpGLd9M3UqyGDAFoOQ== cardno:000607126708"}"
