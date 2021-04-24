8ae0c5e2ef948a91742258d7111f1e3d3e347dc6
Cole Mickens <cole.mickens@gmail.com>
2021-04-24 15:29:26 -0700

    azdev: deploy

--- hosts/azdev/deploy.sh
+++ hosts/azdev/deploy.sh
@@ -7,16 +7,20 @@ export IMAGE_NAME="azdevimg.vhd"
 export MACHINE_GROUP="azdev2020vm1"
 export MACHINE_NAME="azdev2020vm1"
 
-export AZURE_STORAGE_SKU="Premium_LRS"
 
 ## small size, cheap, long-running
 export AZURE_VM_SIZE="Standard_D4as_v4"
-export AZURE_VM_OS_DISK_SIZE="128";
+export AZURE_VM_OS_DISK_SIZE="128" # this can be larger than the sku allows, since we use a non-ephemeral disk
+export AZURE_STORAGE_SKU="Premium_LRS"
 export AZURE_EPHEMERAL_DISK="false"
+export AZURE_ACCEL_NIC="false"
 
 ## big size, boost, expensive, for intense dev seshs brah (ew, forgive me)
 export AZURE_VM_SIZE="Standard_D16as_v4"
-#export AZURE_VM_OS_DISK_SIZE="128";  # leave unset
+export AZURE_VM_OS_DISK_SIZE="128";
+export AZURE_STORAGE_SKU="Standard_LRS" # must use standard with ephemeral os disk (it's faster tho)
 export AZURE_EPHEMERAL_DISK="true"
+export AZURE_ACCEL_NIC="true"
 
 ../azdev/deploy-common.sh
+
