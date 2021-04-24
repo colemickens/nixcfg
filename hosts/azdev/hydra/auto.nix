b0d3a8901764726982b56faadb51a85f6d3b4890
Cole Mickens <cole.mickens@gmail.com>
2021-04-24 22:29:05 +0000

    hydra-auto wip

--- hosts/azdev/hydra/auto.nix
+++ hosts/azdev/hydra/auto.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, inputs, modulesPath, ... }:
 
-{
+let hydraAuto = pkgs.writeShellScript "hydra-auto.sh" ''
+  
+'';
+in {
   config = {
     services.hydra-autoproj = {
       admins = {
@@ -9,14 +12,11 @@
         email = "cole@hydra";
       };
       projects = {
-        foo = {
-          displayName = "friendlyFoo";
-          # TODO: can this itself be a flake ref? I thought so?
-          # enabled = "1"; # default
-          # visible = "1"; # default
-          # decltype = "git";
-          # declvalue = "https://github.com/colemickens/nixcfg main";
-          # declfile = "spec.json";
+        "nixcfg" = {
+          displayName = "nixcfg";
+          decltype = "git";
+          declvalue = "https://github.com/colemickens/nixcfg main";
+          declfile = "spec.json";
         };
       };
     };
--- modules/hydra-autoproj.nix
+++ modules/hydra-autoproj.nix
@@ -1,30 +1,21 @@
-# module to auto-populate hydra projects based on flake->hydraSpecs
 { config, lib, pkgs }:
 
 # TODO: This is like 30% half-fleshed out, never run/tested, etc, needs a lot of love (or for hydra to be replaced)
 
 let
-  adminScript = email: username: password:  pkgs.writeShellScript "hydra-autoproj.sh" ''
-    sudo -u hydra -- ${pkgs.hydra-dev}/bin/hydra-create-user "${username}" --full-name "${username}" --email-address "${email}" --password "${password}" --role admin
-  '';
+  cfg = config.hydra-auto;
 
-  # { 
-  #   "admin" = { password = "admin"; role = "admin"; };
-  #   "viewer" = { password = "viewer"; };
-  # }
+  initScript = pkgs.writeShellScript "hydra-auto.sh" ''
 
-  newProject = {
-    displayName,
-    visible ? true,
-    decltype ? "git",
-    declvalue ? "",
-    declfile ? "spec.json",
-  }@args: toJSON args; # TODO: escape it?
+    # for each in ${cfg.admins}
+    sudo -u hydra -- ${pkgs.hydra-dev}/bin/hydra-create-user \
+      "${username}" --full-name "${username}" \
+      --email-address "${email}" --password "${password}" \
+      --role admin
 
-  projectScript = projectName: p: pkgs.writeShellScript "hydra-autoproj.sh" ''
+    # for each in ${cfg.projects}
     LOGIN="{\"username\":\"cole\", \"password\": \"cole\"}"
     ${pkgs.curl}/bin/curl -b /tmp/cookie -c /tmp/cookie -d "${LOGIN}" -X 'POST' -H 'Content-Type: application/json' --referer 'http://localhost:3000/' http://localhost:3000/login
-
     JSON="${newProject p}"
     ${pkgs.curl}/bin/curl -b /tmp/cookie -c /tmp/cookie -d "${JSON}" -X 'PUT' -H 'Content-Type: application/json' --referer 'http://localhost:3000/' http://localhost:3000/project/${projectName}
   '';
@@ -33,12 +24,19 @@ let
     # option for autoAdmin
     # option for autoProjects
     # option for the endpoint
+    admins = module{};
+    projects = module{};
   };
+
+in {
+  inherit options;
 
   config = {
     systemd.services."hydra-autoproj" = {
       # todo: auto start after hydra
+      serviceConfig = {
+        WantedAfter = "hydra-init.service";
+      };
     };
   };
-in
-{ inherit options config; }
+}
