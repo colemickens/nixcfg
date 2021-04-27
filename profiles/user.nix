5d5da97108747a949e7df938d77457b3623f1859
Cole Mickens <cole.mickens@gmail.com>
2021-04-27 20:57:10 +0000

    stash

--- profiles/user.nix
+++ profiles/user.nix
@@ -13,7 +13,12 @@
       #mkpasswd -m sha-512
       hashedPassword = "$6$k.vT0coFt3$BbZN9jqp6Yw75v9H/wgFs9MZfd5Ycsfthzt3Jdw8G93YhaiFjkmpY5vCvJ.HYtw0PZOye6N9tBjNS698tM3i/1";
       shell = pkgs.zsh;
-      extraGroups = [ "wheel" "networkmanager" "kvm" "libvirtd" "docker" "transmission" "audio" "video" "sway" "sound" "pulse" "input" "render" "dialout" "keys" "ipfs" "plugdev" ];
+      extraGroups = [
+        "wheel"
+        "networkmanager" "kvm" "libvirtd" "docker" "transmission" "audio" "video" "sway" "sound"
+        "pulse" "input" "render" "dialout" "keys" "ipfs" "plugdev"
+        "scard"
+      ];
       uid = 1000;
       group = "cole";
     };
