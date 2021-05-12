9ac6bf678c4aaaf14c9139794ba806582b49ccba
Cole Mickens <cole.mickens@gmail.com>
2021-05-12 13:29:35 -0700

    auto-update(manual): conduit: ad5e51dc251480ed9ca584ed7ceb82fe305122c7 => f3e6311680fc0b73d68571a5c0d73d7823e2f4c4

--- pkgs/conduit/metadata.nix
+++ pkgs/conduit/metadata.nix
@@ -3,6 +3,6 @@
   branch = "master";
   rev = "f3e6311680fc0b73d68571a5c0d73d7823e2f4c4";
   sha256 = "sha256-3KUee8EoZRCNHBbLwITD4jKl97bseOJTOY7/9/ddOBs=";
-  cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+  cargoSha256 = "sha256-nfaE2HIu0dY0fUZdFddx0te0kgw/yjVu2gl0ImCQutw=";
   revdate = "2020-08-31 06:57:21Z";
 }
