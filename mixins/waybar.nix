d916ea91dc8c1edbb2d3a5e18a57c56bbc3b57ac
Cole Mickens <cole.mickens@gmail.com>
2021-05-14 16:42:47 -0700

    waybar: update

--- mixins/waybar.nix
+++ mixins/waybar.nix
@@ -1,28 +1,28 @@
 { pkgs, config, ... }:
 
 let
-  jobpath = "/run/user/1000/srht/jobs";
-  jobs = {
-    "niche" = "niche";
-    "n-w" = "nixpkgs-wayland";
-    "f-f-n" = "flake-firefox-nightly";
-  };
+  # jobpath = "/run/user/1000/srht/jobs";
+  # jobs = {
+  #   "niche" = "niche";
+  #   "n-w" = "nixpkgs-wayland";
+  #   "f-f-n" = "flake-firefox-nightly";
+  # };
 
-  suffix = pkgs.lib.mapAttrsToList (k: v: ''
-    status="$("${pkgs.jq}/bin/jq" -r '[.results[] | select(.tags=="${v}" and .status!="running" and .status!="cancelled")][0] | .status' "${jobpath}/data")"
-    echo "{\"text\":\"''${status}\", \"class\":\"srht-''${status}\"}" > "${jobpath}/${v}-json"
-  '') jobs;
+  # suffix = pkgs.lib.mapAttrsToList (k: v: ''
+  #   status="$("${pkgs.jq}/bin/jq" -r '[.results[] | select(.tags=="${v}" and .status!="running" and .status!="cancelled")][0] | .status' "${jobpath}/data")"
+  #   echo "{\"text\":\"''${status}\", \"class\":\"srht-''${status}\"}" > "${jobpath}/${v}-json"
+  # '') jobs;
 
-  jobsScript = pkgs.writeShellScriptBin "jobs.sh" (pkgs.lib.concatStrings (
-    [''
-      TOKEN=$(cat ${config.sops.secrets."srht-pat".path})
-      BUILD_HOST="https://builds.sr.ht"
-      "${pkgs.coreutils}/bin/mkdir" -p "${jobpath}"
-      "${pkgs.curl}/bin/curl" \
-        -H "Authorization:token ''${TOKEN}" \
-        -H "Content-Type: application/json" -X GET \
-        "''${BUILD_HOST}/api/jobs" > "${jobpath}/data"
-    ''] ++ suffix ));
+  # jobsScript = pkgs.writeShellScriptBin "jobs.sh" (pkgs.lib.concatStrings (
+  #   [''
+  #     TOKEN=$(cat ${config.sops.secrets."srht-pat".path})
+  #     BUILD_HOST="https://builds.sr.ht"
+  #     "${pkgs.coreutils}/bin/mkdir" -p "${jobpath}"
+  #     "${pkgs.curl}/bin/curl" \
+  #       -H "Authorization:token ''${TOKEN}" \
+  #       -H "Content-Type: application/json" -X GET \
+  #       "''${BUILD_HOST}/api/jobs" > "${jobpath}/data"
+  #   ''] ++ suffix ));
 in
 {
   config = {
@@ -32,23 +32,23 @@ in
     };
 
     home-manager.users.cole = { pkgs, ... }: {
-      systemd.user.services."srht-jobs-status" = {
-        Unit.Description = "check srht-jobs status";
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${jobsScript}/bin/jobs.sh";
-        };
-      };
-      systemd.user.timers."srht-jobs-status" = {
-        Unit.Description = "check srht jobs status";
-        Timer = { OnBootSec = "1m"; OnUnitInactiveSec = "1m"; Unit = "srht-jobs-status.service"; };
-        Install.WantedBy = [ "default.target" ];
-        # {
-        #     wantedBy = [ "timers.target" ];
-        #     partOf = [ "srht-${repo}.service" ];
-        #     timerConfig.OnCalendar = "hourly";
-        # }
-      };
+      # systemd.user.services."srht-jobs-status" = {
+      #   Unit.Description = "check srht-jobs status";
+      #   Service = {
+      #     Type = "oneshot";
+      #     ExecStart = "${jobsScript}/bin/jobs.sh";
+      #   };
+      # };
+      # systemd.user.timers."srht-jobs-status" = {
+      #   Unit.Description = "check srht jobs status";
+      #   Timer = { OnBootSec = "1m"; OnUnitInactiveSec = "1m"; Unit = "srht-jobs-status.service"; };
+      #   Install.WantedBy = [ "default.target" ];
+      #   # {
+      #   #     wantedBy = [ "timers.target" ];
+      #   #     partOf = [ "srht-${repo}.service" ];
+      #   #     timerConfig.OnCalendar = "hourly";
+      #   # }
+      # };
       programs.waybar = {
         enable = true;
         style = pkgs.lib.readFile ./waybar.css;
@@ -65,8 +65,8 @@ in
           ];
           modules-center = [];
           modules-right =
-            (pkgs.lib.mapAttrsToList (k: v: "custom/srht-${k}") jobs)
-            ++ [
+            #(pkgs.lib.mapAttrsToList (k: v: "custom/srht-${k}") jobs) ++
+            [
               "idle_inhibitor"
               "tray"
               "pulseaudio"
@@ -78,13 +78,14 @@ in
               "battery"
             ];
 
-          modules = (pkgs.lib.mapAttrs' (k: v: 
-            pkgs.lib.nameValuePair "custom/srht-${k}" {
-              format = "${k}: {}";
-              interval = 10;
-              exec = "${pkgs.coreutils}/bin/cat ${jobpath}/${v}-json";
-              return-type = "json";
-            }) jobs) // {
+          modules = #(pkgs.lib.mapAttrs' (k: v: 
+            # pkgs.lib.nameValuePair "custom/srht-${k}" {
+            #   format = "${k}: {}";
+            #   interval = 10;
+            #   exec = "${pkgs.coreutils}/bin/cat ${jobpath}/${v}-json";
+            #   return-type = "json";
+            # }) jobs) /
+          {
             "sway/workspaces" = {
               all-outputs = true;
               disable-scroll-wraparound = true;
@@ -98,8 +99,8 @@ in
               on-click-middle = "${pkgs.sway}/bin/swaymsg exec \"${pkgs.alacritty}/bin/alacritty -e pulsemixer\"";
             };
             network = {
-              format-wifi = "{ifname} ssid({essid} {signalStrength}%) up({bandwidthUpBits}) down({bandwidthDownBits})";
-              format-ethernet = "{ifname} up({bandwidthUpBits}) down({bandwidthDownBits})";
+              format-wifi = "{ifname} '{essid}' {signalStrength}% {bandwidthUpBits}/{bandwidthDownBits}";
+              format-ethernet = "{ifname} eth {bandwidthUpBits}/{bandwidthDownBits}";
             };
             cpu.interval = 2;
             cpu.format = "cpu load({load}%) use({usage}%)";
