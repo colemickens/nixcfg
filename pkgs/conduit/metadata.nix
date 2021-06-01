5992cbf43f3a890535735cac1f54e1b30b8bf3a7
Cole Mickens <cole.mickens@gmail.com>
2021-06-01 14:46:54 -0700

    conduit ver fix

--- pkgs/conduit/metadata.nix
+++ pkgs/conduit/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://gitlab.com/famedly/conduit.git/";
   branch = "master";
-  rev = "ceaeab6816476de29f64c66ccd8eddbd8498a644";
-  sha256 = "sha256-VrIp8mRrNH0jsk2P5hldX6sepuSqw5hH815KZkycT44=";
-  cargoSha256 = "0000000000000000000000000000000000000000000000000012";
+  rev = "1ab209736aa93cebc2987595431eb24f5df830c1";
+  sha256 = "sha256-BdUz0t9dESRTAvLMviUecEG7WkD8slqyYGsRTI1UsMU=";
+  cargoSha256 = "sha256-CqpoUIpMieJ0vJ09fkp5JPtmc7yDWLlMkRgkZWn0aZY=";
   revdate = "2020-08-31 06:57:21Z";
 }
