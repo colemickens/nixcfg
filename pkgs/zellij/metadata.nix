ed4b97aa5a0c7b90f7064a6a596c7df72a45f74e
Cole Mickens <cole.mickens@gmail.com>
2021-06-16 15:43:43 -0700

    auto-update(manual): zellij: 193331062655c4a0b79cf9433157ec8526c4b29f => 5c69ce9aad9b7f2e97576106a292c211db073129

--- pkgs/zellij/metadata.nix
+++ pkgs/zellij/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/zellij-org/zellij";
   branch = "main";
-  rev = "193331062655c4a0b79cf9433157ec8526c4b29f";
-  sha256 = "sha256-O7q1tNEiBglt948d0cegyHzK71x0hbjAp1mB2bPcHsE=";
-  cargoSha256 = "sha256-1rGsBy1xCsqxCd8h4O6sKSmAU5oCTpPY+m+VnbTRfkI=";
+  rev = "5c69ce9aad9b7f2e97576106a292c211db073129";
+  sha256 = "sha256-SoV7laKI1b/qN01oCOsTK8/escPjt/XjfnThpzZgDdE=";
+  cargoSha256 = "sha256-3r/UDYziisHPsvoDxCI/+7IN8yAcogARLw/GSvbD3f8=";
 }
