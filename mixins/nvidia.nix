8a47ae3e91e3c9305e0fe703d223bd2814182963
Cole Mickens <cole.mickens@gmail.com>
2021-07-14 16:04:42 -0700

    gnome/nvidia updates

--- mixins/nvidia.nix
+++ mixins/nvidia.nix
@@ -1,14 +1,17 @@
 { config, pkgs, lib, ... }:
+
 {
   config = {
     hardware.nvidia.modesetting.enable = true;
-      # hardware.nvidia.powerManagement.enable = true; # TODO: test
-      services.xserver = {
-        enable = true;
-        videoDriver = "nvidia";
-        # autoSuspend = false; # nvidia doesn't wake up, others seem to notice this too?
-        displayManager.gdm.nvidiaWayland =
-          (config.services.xserver.displayManager.gdm.enable == true);
-      };
+    hardware.nvidia.package = pkgs.nvidiaBeta;
+  
+    # hardware.nvidia.powerManagement.enable = true; # TODO: test
+
+    services.xserver = {
+      enable = true;
+      # autoSuspend = false; # nvidia doesn't wake up, others seem to notice this too?
+      displayManager.gdm.nvidiaWayland =
+        (config.services.xserver.displayManager.gdm.enable == true);
+    };
   };
 }
--- profiles/desktop-gnome.nix
+++ profiles/desktop-gnome.nix
@@ -7,7 +7,7 @@
   config = {
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome3.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
 
     home-manager.users.cole = { pkgs, ... }: {
       home.sessionVariables = {
