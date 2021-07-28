d665d697be5dd8afa47cdfc4223af3821762765f
Cole Mickens <cole.mickens@gmail.com>
2021-07-27 18:35:16 -0700

    stash

--- pkgs/zellij/metadata.nix
+++ pkgs/zellij/metadata.nix
@@ -3,5 +3,5 @@
   branch = "main";
   rev = "3df0210647e65170576f1ea97328cce26c211a43";
   sha256 = "029pfz01iqr1ginxahg4qrc75f6lg4343sapscip66f6rm62ixf1";
-  cargoSha256 = "12dp7abs8jg07d5qvbk2saxyw0ig1qwjdwrdw6y5y5j039c8lxcd";
+  cargoSha256 = "0g70nm9iscppkqic25sby4kvn8vx4jdmpvd15hsy4m12rqz20s6v";
 }
