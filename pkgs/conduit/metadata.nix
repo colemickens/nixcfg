f24bebe48203d12b973e04f5abc4b926e2d893e0
Cole Mickens <cole.mickens@gmail.com>
2021-08-01 21:08:02 -0700

    auto-update(manual): conduit: 6666aae5a51eb530ad7bc586e30134d9564012e3 => 8174b16c386b6ab0c471367ef95d3354403e0e4b

--- pkgs/conduit/metadata.nix
+++ pkgs/conduit/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://gitlab.com/famedly/conduit.git/";
   branch = "master";
-  rev = "6666aae5a51eb530ad7bc586e30134d9564012e3";
-  sha256 = "sha256-1npKmpJqm1CA1mCPXuBt2+qZJdaGHXuoq1WJ34DklSg=";
-  cargoSha256 = "sha256-BXB6YIC8y7gS0EnDtUhMAfNgTIof0FcHscFbAx+sMiw=";
+  rev = "8174b16c386b6ab0c471367ef95d3354403e0e4b";
+  sha256 = "sha256-MmZn8PBTRzoAAr9EljRuA9Avx/yuzNbFTSINkMxEDX4=";
+  cargoSha256 = "sha256-GHFRbCMawHQw0+r9ouewFLO0fq6t+bgaVGpYYiuiQs4=";
   revdate = "2020-08-31 06:57:21Z";
 }
