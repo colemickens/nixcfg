9d40bc38116edcdbf42a78b3ec8d01763a2d8d9b
Cole Mickens <cole.mickens@gmail.com>
2021-08-03 21:08:19 -0700

    stash

--- mixins/syncthing.nix
+++ mixins/syncthing.nix
@@ -2,6 +2,9 @@
 
 {
   config = {
+    networking.firewall.allowedTCPPorts = [ 22000 ];
+    networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+    
     home-manager.users.cole = { pkgs, ... }: {
       services.syncthing = {
         enable = true;
