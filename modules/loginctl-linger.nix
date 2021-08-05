e191446cbf385e5b80ad9030bc3088a189afcdd3
Cole Mickens <cole.mickens@gmail.com>
2021-08-04 23:22:11 -0700

    stash

--- modules/loginctl-linger.nix
+++ modules/loginctl-linger.nix
@@ -18,7 +18,6 @@ let
     (concatStrings (map (s: "${s}\n")
       (sort (a: b: a < b) lingeringUsers))); # this sorting is important for `comm` to work correctly
 
-  #updateLingering = pkgs.writeScript "update-lingering" ''
   updateLingering = ''
     if [ -e ${dataDir} ] ; then
       ls ${dataDir} | sort | comm -3 -1 ${lingeringUsersFile} - | xargs -r ${pkgs.systemd}/bin/loginctl disable-linger
