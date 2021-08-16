8489fb7026764f7f8607a892d5a09a65ffd805f1
Cole Mickens <cole.mickens@gmail.com>
2021-08-16 15:57:04 -0700

    auto-update(manual): headscale: 5b1b40ce93ffb5f2540449e94562cb9dc98e3267 => 47b61c0cea27a43a451decbcbcb17a9fcdf69993

--- pkgs/headscale/metadata.nix
+++ pkgs/headscale/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/juanfont/headscale";
   branch = "main";
-  rev = "5b1b40ce93ffb5f2540449e94562cb9dc98e3267";
-  sha256 = "sha256-+0eBXBcEyD6doKWbik5xxi2uQAwiFR5n9ndat2x1vhI=";
-  vendorSha256 = "sha256-ducYkfgwnyvvZ//2ghA35Oyei9bQIN+LBYViDw8RHVs=";
+  rev = "47b61c0cea27a43a451decbcbcb17a9fcdf69993";
+  sha256 = "sha256-pzsbCxJ3N30XpjF//02SV6URhA6f6Wz8a6HvGxsK7M4=";
+  vendorSha256 = "sha256-ususDOF/LznhK4EInHE7J/ItMjziGfP9Gn8/Q5wd78g=";
 }
