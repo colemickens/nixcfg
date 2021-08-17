ce82a5b58d3caa3831d37f87b0de558628a914e3
Cole Mickens <cole.mickens@gmail.com>
2021-08-17 12:17:27 -0700

    stash

--- mixins/nushell.nix
+++ mixins/nushell.nix
@@ -18,6 +18,30 @@
             startup = [ "alias e [msg] { echo $msg }" ];
             no_auto_pivot = true;
           }
+
+          {
+            color_config = {
+              primitive_int = "green";
+              primitive_decimal = "red";
+              primitive_filesize = "ur";
+              primitive_string = "pb";
+              primitive_line = "yellow";
+              primitive_columnpath = "cyan";
+              primitive_pattern = "white";
+              primitive_boolean = "green";
+              primitive_date = "ru";
+              primitive_duration = "blue";
+              primitive_range = "purple";
+              primitive_path = "yellow";
+              primitive_binary = "cyan";
+              separator_color = "purple";
+              header_align = "l"; # left|l, right|r, center|c;
+              header_color = "c"; # green|g, red|r, blue|u, black|b, yellow|y, purple|p, cyan|c, white|w;
+              header_bold = true;
+              index_color = "rd";
+              leading_trailing_space_bg = "white";
+            };
+          }
         ];
       };
     };
