1db99027224328422188a98ac4fcd32c0e3616a7
Cole Mickens <cole.mickens@gmail.com>
2021-08-20 21:28:21 -0700

    stash

--- mixins/nvidia.nix
+++ mixins/nvidia.nix
@@ -8,8 +8,7 @@ in
     boot.blacklistedKernelModules = [ "nouveau" ];
 
     hardware.nvidia.modesetting.enable = true;
-    #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
     hardware.nvidia.powerManagement.enable = false;
     
     services.xserver = {
