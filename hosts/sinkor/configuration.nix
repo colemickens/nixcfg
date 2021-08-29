409a88b98439aed46e8bff58b676aefbae93db70
Cole Mickens <cole.mickens@gmail.com>
2021-08-29 15:25:32 -0700

    stash

--- hosts/sinkor/configuration.nix
+++ hosts/sinkor/configuration.nix
@@ -90,6 +90,14 @@ in
       };
     };
 
+    boot.initrd.network.enable = true;
+    boot.initrd.network.ssh = {
+      enable = true;
+      port = 22;
+      authorizedKeys = (import ../../data/sshkeys.nix);
+      hostKeys = [ ./ssh_host_ed25519_key ];
+    };
+
     fileSystems = {
       # on the tow-boot SD card
       "/boot" = {
