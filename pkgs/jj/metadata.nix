6a67f26c953a2c219fb67d4be9d05aa607f731ec
Cole Mickens <cole.mickens@gmail.com>
2021-09-02 19:19:32 -0700

    auto-update(manual): jj: ecbbac23be8d01d7eea8904fec5f2799acaa3532 => ccdd65195313dd721e09a7a1373ba378f6b96ec1

--- pkgs/jj/metadata.nix
+++ pkgs/jj/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/martinvonz/jj";
   branch = "main";
-  rev = "ecbbac23be8d01d7eea8904fec5f2799acaa3532";
-  sha256 = "sha256-doIa4rQAF3jVYOOyNV4x6Lb2+Qe6nVZKOux+ALNoF1U=";
-  cargoSha256 = "sha256-RChU+rmEc/GoodQAlFtmKViaqGqy2WH0qX47fbLntac=";
+  rev = "ccdd65195313dd721e09a7a1373ba378f6b96ec1";
+  sha256 = "sha256-6x0OuEFthGetDSOuW2okYZLMTCcjDcfXgGAXB1mRfuw=";
+  cargoSha256 = "sha256-tCsRDdA1zT4+GwKsPh22+xWdizQcFlB8IeMMPk4912M=";
 }
