e20a78d897f5ad8f34e8f8d3fc6332073f11a315
Cole Mickens <cole.mickens@gmail.com>
2021-09-12 00:12:40 -0700

    auto-update(manual): neochat: 5c86692fb529000f2c995ae0caf7722348d1d96f => af2992579847e2170dab5a70bad84839b96a5872

--- pkgs/neochat/metadata.nix
+++ pkgs/neochat/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/KDE/neochat";
   branch = "master";
-  rev = "5c86692fb529000f2c995ae0caf7722348d1d96f";
-  sha256 = "sha256-9amSeAOwEa3xzkhiJS+UiAZhkmL9ILCjuKDua2/UkRk=";
+  rev = "af2992579847e2170dab5a70bad84839b96a5872";
+  sha256 = "sha256-npSN6JlXbYppRQmk1oEjZSkrOHXcKbhrnglsUu85O0k=";
 }
