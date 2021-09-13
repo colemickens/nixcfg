f2bc21f4c599bc27cf76fb000df6d6dcb001deac
Cole Mickens <cole.mickens@gmail.com>
2021-09-13 14:56:16 -0700

    stash

--- hosts/sinkor/configuration.nix
+++ hosts/sinkor/configuration.nix
@@ -28,7 +28,7 @@ in
         # "/etc/NetworkManager/system-connections"
       ];
       files = [
-        # "/etc/machine-id"
+        "/etc/machine-id"
         "/etc/ssh/ssh_host_ed25519_key"
         "/etc/ssh/ssh_host_ed25519_key.pub"
         "/etc/ssh/ssh_host_rsa_key"
