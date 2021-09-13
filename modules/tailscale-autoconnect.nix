e00b7ab0335530f1ff3878e0cd45c55b3e600776
Cole Mickens <cole.mickens@gmail.com>
2021-09-12 23:27:17 -0700

    stash

--- modules/tailscale-autoconnect.nix
+++ modules/tailscale-autoconnect.nix
@@ -17,31 +17,30 @@ in {
     };
   };
 
-  config = if cfg.enable then {
+  config = {
     # adapted from: https://tailscale.com/blog/nixos-minecraft/
 
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-      after = [ "network-pre.target" "tailscale.service" ];
-      wants = [ "network-pre.target" "tailscale.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-      };
-      script = with pkgs; ''
-        # TODO: there's a better way to do this: tailscaled settle
-        sleep 2
+    # systemd.services.tailscale-autoconnect = {
+    #   description = "Automatic connection to Tailscale";
+    #   after = [ "network-pre.target" "tailscale.service" ];
+    #   wants = [ "network-pre.target" "tailscale.service" ];
+    #   wantedBy = [ "multi-user.target" ];
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #   };
+    #   script = with pkgs; ''
+    #     # TODO: there's a better way to do this: tailscaled settle
+    #     sleep 2
 
-        # TODO: does this actually check for auth'd?
-        # check if we are already authenticated to tailscale
-        status="$(${pkgs.tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
+    #     # TODO: does this actually check for auth'd?
+    #     # check if we are already authenticated to tailscale
+    #     status="$(${pkgs.tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+    #     if [ $status = "Running" ]; then # if so, then do nothing
+    #       exit 0
+    #     fi
 
-        # otherwise authenticate with tailscale
-        cat "${cfg.tokenFile}" | ${pkgs.tailscale}/bin/tailscale up -authkey -
-      '';
-    };
-  } else {};
+    #     # otherwise authenticate with tailscale
+    #     cat "${cfg.tokenFile}" | ${pkgs.tailscale}/bin/tailscale up -authkey -
+    #   '';
+  };
 }
