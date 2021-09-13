7a58bdbb534a507ba2761454421377409f31b27d
Cole Mickens <cole.mickens@gmail.com>
2021-09-13 14:12:09 -0700

    stash

--- mixins/wayland-tweaks.nix
+++ mixins/wayland-tweaks.nix
@@ -16,41 +16,41 @@
 
     nixpkgs.overlays = [
       (final: prev: {
-        vscodium = (prev.runCommandNoCC "codium"
-          { buildInputs = with pkgs; [ makeWrapper ]; }
-          ''
-            makeWrapper ${prev.vscodium}/bin/codium $out/bin/codium \
-              --add-flags "--enable-features=UseOzonePlatform" \
-              --add-flags "--ozone-platform=wayland"
+        # vscodium = (prev.runCommandNoCC "codium"
+        #   { buildInputs = with pkgs; [ makeWrapper ]; }
+        #   ''
+        #     makeWrapper ${prev.vscodium}/bin/codium $out/bin/codium \
+        #       --add-flags "--enable-features=UseOzonePlatform" \
+        #       --add-flags "--ozone-platform=wayland"
 
-            ln -sf ${prev.vscodium}/share $out/share
-          ''
-          );
+        #     ln -sf ${prev.vscodium}/share $out/share
+        #   ''
+        #   );
 
-        element-desktop = (prev.runCommandNoCC "element"
-            { buildInputs = with prev; [ makeWrapper ]; }
-            ''
-              makeWrapper ${prev.element-desktop}/bin/element-desktop $out/bin/element-desktop \
-                --add-flags "--enable-features=UseOzonePlatform" \
-                --add-flags "--ozone-platform=wayland"
+        # element-desktop = (prev.runCommandNoCC "element"
+        #     { buildInputs = with prev; [ makeWrapper ]; }
+        #     ''
+        #       makeWrapper ${prev.element-desktop}/bin/element-desktop $out/bin/element-desktop \
+        #         --add-flags "--enable-features=UseOzonePlatform" \
+        #         --add-flags "--ozone-platform=wayland"
 
-              ln -sf ${prev.element-desktop}/share $out/share
-            ''
-          );
+        #       ln -sf ${prev.element-desktop}/share $out/share
+        #     ''
+        #   );
 
-        ungoogled-chromium = (let
-          #c = inputs.nixos-unstable.legacyPackages.${prev.system}.ungoogled-chromium;
-          c = prev.ungoogled-chromium;
-          in prev.runCommandNoCC "wrap-chromium"
-            { buildInputs = with pkgs; [ makeWrapper ]; }
-            ''
-              makeWrapper ${c}/bin/chromium $out/bin/chromium \
-                --add-flags "--enable-features=UseOzonePlatform" \
-                --add-flags "--ozone-platform=wayland"
+        # ungoogled-chromium = (let
+        #   #c = inputs.nixos-unstable.legacyPackages.${prev.system}.ungoogled-chromium;
+        #   c = prev.ungoogled-chromium;
+        #   in prev.runCommandNoCC "wrap-chromium"
+        #     { buildInputs = with pkgs; [ makeWrapper ]; }
+        #     ''
+        #       makeWrapper ${c}/bin/chromium $out/bin/chromium \
+        #         --add-flags "--enable-features=UseOzonePlatform" \
+        #         --add-flags "--ozone-platform=wayland"
 
-              ln -sf ${c}/share $out/share
-            ''
-            );
+        #       ln -sf ${c}/share $out/share
+        #     ''
+        #     );
       })
     ];
   };
