2968f10cb40ec7d0b4fe99d294e58e64a35d4f2d
Cole Mickens <cole.mickens@gmail.com>
2021-09-13 17:55:53 -0700

    stash

--- hosts/pinebook/configuration.nix
+++ hosts/pinebook/configuration.nix
@@ -4,6 +4,7 @@ let
 
   pinebook-fix-sound = (pkgs.writeShellScriptBin "pinebook-fix-sound" ''
     export NIX_PATH="nixpkgs=${toString inputs.nixpkgs}"
+    export PATH="${makeBinPath [ pkgs.nix ]}:''$PATH}"
     ${toString inputs.wip-pinebook-pro}/sound/reset-sound.rb
   '');
 in
