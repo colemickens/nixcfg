1f3a7fe14e329aa01b000a59d7c8be184667c446
Cole Mickens <cole.mickens@gmail.com>
2021-09-15 15:28:32 -0700

    stash

--- hosts/rpifour1/modules/unifi.nix
+++ hosts/rpifour1/modules/unifi.nix
@@ -1,31 +1,20 @@
 { pkgs, ... }:
 
-let
-  # pinnedNixpkgs = let
-  #   channelRelease =
-  #     "nixos-19.09pre190687.3f4144c30a6"; # last known working mongo
-  #   channelName = "unstable";
-  #   url =
-  #     "https://releases.nixos.org/nixos/${channelName}/${channelRelease}/nixexprs.tar.xz";
-  #   sha256 = "040f16afph387s0a4cc476q3j0z8ik2p5bjyg9w2kkahss1d0pzm";
-  # in import (builtins.fetchTarball { inherit url sha256; }) {
-  #   system = pkgs.system; # TODO: ? is this the best way?
-  #   # TODO: inherit overlays too?
-  # };
-  # mdbp = pinnedNixpkgs.mongodb;
-in {
+{
   config = {
     nixpkgs.config = {
       allowUnfree = true;
-      oraclejdk.accept_license = true;
+      #oraclejdk.accept_license = true;
     };
+
+    users.users.unifi.group = "unifi";
+    users.groups.unifi = {};
 
     services.unifi = {
       enable = true;
       unifiPackage = pkgs.unifiStable;
-      #mongodbPackage = mdbp;
       mongodbPackage = pkgs.mongodb;
-      jrePackage = pkgs.jre8_headless;
+      #jrePackage = pkgs.jre8_headless;
       maximumJavaHeapSize = 256;
     };
 
