8506a4e0daa7a55d5573c13bbdeb44dbef457b19
Cole Mickens <cole.mickens@gmail.com>
2021-09-20 21:33:55 -0700

    gha

--- .github/secrets.nix
+++ .github/secrets.nix
@@ -2,10 +2,24 @@
 
 let
   pkgs = import nixpkgs { inherit system; };
+
+  secretdata = import ../secrets/secretdata.nix {lib=pkgs.lib;};
+  secrets = pkgs.lib.mapAttrs (k: v: v //
+    {
+      owner = "runner";
+      group = "runner";
+    }
+  ) secretdata;
+
   fakeSystem = (nixpkgs.lib.nixosSystem {
     inherit system;
     modules = [
-      ../secrets
+      inputs.sops-nix.nixosModules.sops
+
+      ({config,lib,pkgs,...}:
+      {
+        config.sops.secrets = secrets;
+      })
     ];
     specialArgs = { inherit inputs; };
   });
