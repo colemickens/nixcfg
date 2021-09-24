60ce6175abcc63f0e3fbf8e7927067894e1e3223
Cole Mickens <cole.mickens@gmail.com>
2021-09-23 18:17:38 -0700

    stash

--- profiles/interactive.nix
+++ profiles/interactive.nix
@@ -30,7 +30,8 @@ in
 
   config = {
     # HM: ca.desrt.dconf error:
-    sops.secrets."nixup-secrets".owner = config.users.users.cole;
+    # TODO: sops.secrets."nixup-secrets".owner = config.users.users.cole;
+    sops.secrets."nixup-secrets".owner = "cole";
 
     services.dbus.packages = with pkgs; [ gnome3.dconf ];
     nix.extraOptions = ''
