82b17a7c648cfad6407fcc026c8b29fb51db8691
Cole Mickens <cole.mickens@gmail.com>
2021-09-23 18:15:20 -0700

    stash

--- mixins/tailscale.nix
+++ mixins/tailscale.nix
@@ -9,10 +9,12 @@
     services.tailscale.enable = true;
     services.tailscale-autoconnect.enable = false;
     services.tailscale-autoconnect.tokenFile = "# use sops";
-  } // (if config.networking.hostName != "jeffhyper" then {} else {
-    systemd.services.tailscale.serviceConfig.Environment = [
-      "PORT=${config.services.tailscale.port}"
-      "FLAGS=--exit-node=192.168.1.200"
-    ];
-  });
+  }
+  #  // (if config.networking.hostName != "jeffhyper" then {} else {
+  #   systemd.services.tailscale.serviceConfig.Environment = [
+  #     "PORT=${config.services.tailscale.port}"
+  #     "FLAGS=--exit-node=192.168.1.200"
+  #   ];
+  # })
+  ;
 }
