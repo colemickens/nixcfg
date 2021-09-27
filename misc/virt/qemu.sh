980e4f7ca40b9d5f9814838d8a03e370250cfe84
Cole Mickens <cole.mickens@gmail.com>
2021-09-27 00:05:45 -0700

    gha

--- misc/virt/qemu.sh
+++ misc/virt/qemu.sh
@@ -5,22 +5,29 @@ args=()
 
 # echo "remote-viewer spice+unix:///tmp/vmspice.socket"
 #  -virtfs local,path=/run/media/cole,mount_tag=share,security_model=passthrough \
-sudo rm -rf /tmp/vmspice-tails.socket
+sudo rm -rf /tmp/qemu.socket
 
 source ../../secrets/unencrypted/qemu-profile-$1
 
 [[ "${QEMU_ISO:-""}" != "" ]] && args=("${args[@]}" -cdrom "${QEMU_ISO}")
-[[ "${QEMU_VIRTIO_GPU:-""}" != "" ]]   && args=("${args[@]}"
-  -vga none \
-  -device virtio-gpu-pci,virgl=on \
-  -spice gl=on,unix=on,addr=/tmp/vmspice-tails.socket,disable-ticketing=on \
-  -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-  -chardev spicevmc,id=spicechannel0,name=vdagent \
-  -device virtserialport,chardev=charchannel1,id=channel1,name=org.spice-space.stream.0 \
-  -chardev spiceport,name=org.spice-space.stream.0,id=charchannel1 \
+[[ "${QEMU_VIRTIO_GPU:-""}" == "gl" ]]   && args=("${args[@]}"
+  -vga none
+  -device virtio-gpu-pci,virgl=on
+  -spice gl=on,unix=on,addr=/tmp/qemu.socket,disable-ticketing=on
+  -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0
+  -chardev spicevmc,id=spicechannel0,name=vdagent
+  -device virtserialport,chardev=charchannel1,id=channel1,name=org.spice-space.stream.0
+  -chardev spiceport,name=org.spice-space.stream.0,id=charchannel1
+)
+[[ "${QEMU_VIRTIO_GPU:-""}" == "win" ]]   && args=("${args[@]}"
+  -vga none -vga qxl
+  -spice unix=on,addr=/tmp/qemu.socket,disable-ticketing=on
+  -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0
+  -chardev spicevmc,id=spicechannel0,name=vdagent
+  -device virtserialport,chardev=charchannel1,id=channel1,name=org.spice-space.stream.0
+  -chardev spiceport,name=org.spice-space.stream.0,id=charchannel1
 )
 [[ "${QEMU_DRIVE_ARGS:-""}" != "" ]] && args=("${args[@]}" "${QEMU_DRIVE_ARGS[@]}" )
-
 
 sudo qemu-system-x86_64 \
   -machine pc,accel=kvm \
@@ -33,18 +40,18 @@ sudo qemu-system-x86_64 \
   -device intel-hda -device hda-duplex \
   -device virtio-serial \
   -device vhost-vsock-pci,id=vhost-vsock-pci0,guest-cid=3 \
-  "${args[@]}" "${QEMU_EXTRA[@]}"
-&
+  "${args[@]}" "${QEMU_EXTRA[@]}" &
+set -x
 pid=$!
 sleep 1
-sudo chown cole:cole /tmp/vmspice-tails.socket
+sudo chown cole:cole /tmp/qemu.socket
 trap 'sudo kill $pid' EXIT
-printf "\n\n**\nremote-viewer spice+unix:///tmp/vmspice-tails.socket\n**\n\n"
+remote-viewer spice+unix:///tmp/qemu.socket &
 wait $pid
 
 
 # if [[ "${1}" == "disabled_tails" ]]; then
-#   sudo rm -rf /tmp/vmspice-tails.socket
+#   sudo rm -rf /tmp/qemu.socket
 #   sudo qemu-system-x86_64 \
 #     -drive file=/dev/nvme0n1p5,if=virtio \
 #     -boot d -cdrom $ISO \
@@ -65,7 +72,7 @@ wait $pid
 # elif [[ "${1}" == "xtails" ]]; then
 
 # elif [[ "${1}" == "smb" ]]; then
-#   sudo rm -rf /tmp/vmspice-tails.socket
+#   sudo rm -rf /tmp/qemu.socket
 #   sudo qemu-system-x86_64 \
 #     -drive file=/dev/nvme0n1p5,if=virtio \
 #     -boot d -cdrom $ISO \
@@ -75,7 +82,7 @@ wait $pid
 #     -audiodev pa,id=snd0,server=/run/user/1000/pulse/native \
 #     -device AC97,audiodev=snd0 \
 #     -vga qxl -device virtio-serial-pci \
-#     -spice unix=on,addr=/tmp/vmspice-tails.socket,disable-ticketing=on \
+#     -spice unix=on,addr=/tmp/qemu.socket,disable-ticketing=on \
 #     -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
 #     -chardev spicevmc,id=spicechannel0,name=vdagent
 # elif [[ "${VM}" == "win" ]]; then
