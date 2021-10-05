dba40ebdeb16ee16c742d6a2e4f76b7790913620
Cole Mickens <cole.mickens@gmail.com>
2021-10-04 23:41:31 -0700

    wip

--- hosts/oracular/build.sh
+++ hosts/oracular/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+set -euo pipefail
+set -x
 
 R="../.."
 h="opc@132.226.31.59"
 nix copy --to "ssh-ng://${h}" "${R}#toplevels.oracular_kexec"
 nix build --store "ssh-ng://${h}" "${R}#images.oracular_kexec"
+
+# update oracle:
+# curl "https://nixos-nix-install-tests.cachix.org/serve/j087w6xqqspfs363449x750x9r0kn31s/install" > install
+# chmod +x install
+# ./install --tarball-url-prefix https://nixos-nix-install-tests.cachix.org/serve
