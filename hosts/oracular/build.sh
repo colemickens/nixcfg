88627637add726b779603deed26600ebcd4ab584
Cole Mickens <cole.mickens@gmail.com>
2021-10-04 23:49:37 -0700

    wip

--- hosts/oracular/build.sh
+++ hosts/oracular/build.sh
@@ -5,7 +5,9 @@ set -x
 R="../.."
 h="opc@132.226.31.59"
 nix copy --no-check-sigs --to "ssh-ng://${h}" "${R}#toplevels.oracular_kexec"
-nix build --store "ssh-ng://${h}" "${R}#images.oracular_kexec"
+nix build --eval-store "auto" --store "ssh-ng://${h}" "${R}#images.oracular_kexec"
+
+# TODO: what does nix build do in regards to --out-link when store is remote?
 
 # update oracle:
 # curl "https://nixos-nix-install-tests.cachix.org/serve/j087w6xqqspfs363449x750x9r0kn31s/install" > install
