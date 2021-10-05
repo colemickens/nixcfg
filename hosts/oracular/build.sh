465a1a257e77cb20683446aa0d5505bbb987b8a9
Cole Mickens <cole.mickens@gmail.com>
2021-10-04 23:43:04 -0700

    wip

--- hosts/oracular/build.sh
+++ hosts/oracular/build.sh
@@ -4,7 +4,7 @@ set -x
 
 R="../.."
 h="opc@132.226.31.59"
-nix copy --to "ssh-ng://${h}" "${R}#toplevels.oracular_kexec"
+nix copy --no-check-sigs --to "ssh-ng://${h}" "${R}#toplevels.oracular_kexec"
 nix build --store "ssh-ng://${h}" "${R}#images.oracular_kexec"
 
 # update oracle:
