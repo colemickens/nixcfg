c94c997d57d24159509e905eeaba3086b65f247c
Cole Mickens <cole.mickens@gmail.com>
2021-10-10 21:45:26 -0700

    wip

--- hosts/oracular/build.sh
+++ hosts/oracular/build.sh
@@ -6,8 +6,11 @@ R="../.."
 h="cole@$(tailscale ip --6 oracular)"
 nix copy --no-check-sigs --to "ssh-ng://${h}" "${R}#toplevels.oracular_kexec"
 nix build --eval-store "auto" --store "ssh-ng://${h}" "${R}#nixosConfigurations.oracular_kexec.config.system.build.kexec_script"
+out="$(nix eval --raw "${R}#nixosConfigurations.oracular_kexec.config.system.build.kexec_script")"
 
-nix eval --raw "${R}#nixosConfigurations.oracular_kexec.config.system.build.kexec_script"
+
+ssh "${h}" \
+    
 
 # TODO: what does nix build do in regards to --out-link when store is remote?
 
