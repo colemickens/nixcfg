e1241fa10c447cf10416fb19d2d56ca161ce4189
Cole Mickens <cole.mickens@gmail.com>
2021-10-17 15:34:55 -0700

    wip

--- hosts/bluephone/build.sh
+++ hosts/bluephone/build.sh
@@ -5,12 +5,12 @@ set -x
 
 thing="images.bluephone"
 result="$(nix eval --raw "/home/cole/code/nixcfg#${thing}")"
-out="$(echo "${thing}" | sha256sum | cut -d' ' -f1)"
+out="colemickens-$(echo "${thing}" | sha256sum | cut -d' ' -f1)"
 
 git -C /home/cole/code/nixcfg commit . -m "wip" || true; git -C /home/cole/code/nixcfg push origin HEAD
 ssh "colemickens@aarch64.nixos.community" "git -C /home/colemickens/code/nixcfg pull"
 ssh "colemickens@aarch64.nixos.community" "nix build -L /home/colemickens/code/nixcfg#${thing} --out-link /tmp/${out}"
 
-#nix copy --no-check-sigs --from "ssh-ng://colemickens@aarch64.nixos.community" "${result}"
+nix copy --no-check-sigs --from "ssh-ng://colemickens@aarch64.nixos.community" "${result}"
 ssh "colemickens@aarch64.nixos.community" "nix path-info -r $result"
-#ssh "colemickens@aarch64.nixos.community" "rm -f /tmp/${out}"
+ssh "colemickens@aarch64.nixos.community" "rm -f /tmp/${out}"
