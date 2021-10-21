48482d6ca25b3acc7653416241bb62f6123c728f
Cole Mickens <cole.mickens@gmail.com>
2021-10-21 15:25:46 -0700

    wip

---
+++ hosts/bluephone/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+thing="images.bluephone"
+result="$(nix eval --raw "/home/cole/code/nixcfg#${thing}")"
+out="colemickens-$(echo "${thing}" | sha256sum | cut -d' ' -f1)"
+
+git -C /home/cole/code/nixcfg commit . -m "wip" || true; git -C /home/cole/code/nixcfg push origin HEAD
+ssh "colemickens@aarch64.nixos.community" "git -C /home/colemickens/code/nixcfg remote update \
+  && git -C /home/colemickens/code/nixcfg reset --hard origin/main \
+  && nix build -L /home/colemickens/code/nixcfg#${thing} --out-link /tmp/${out}"
+
+nix copy --no-check-sigs --from "ssh-ng://colemickens@aarch64.nixos.community" "${result}"
+ssh "colemickens@aarch64.nixos.community" "nix path-info -r $result"
+ssh "colemickens@aarch64.nixos.community" "rm -f /tmp/${out}"
