3709a23a1bf036a370e0162ec351c1ff5793bde7
Cole Mickens <cole.mickens@gmail.com>
2021-10-22 22:08:28 -0700

    wip

--- misc/agcache.sh
+++ misc/agcache.sh
@@ -14,6 +14,6 @@ while read p; do
     | grep -v partition \
     | grep -v PARTITION \
     | grep -v firmware \
-    | grep -v "-bundle" \
+    | grep -v -- "-bundle" \
     | cachix push "colemickens"
 done </tmp/drvs
