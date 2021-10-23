abffa090ff4a3502b7647f06debe923d7c568f80
Cole Mickens <cole.mickens@gmail.com>
2021-10-22 22:03:37 -0700

    wip

--- misc/agcache.sh
+++ misc/agcache.sh
@@ -2,10 +2,17 @@
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 thing="${1}"
 
+set -x
+
 nix path-info --derivation -r "${DIR}/..#${thing}" > /tmp/drvs
 
-cat /tmp/drvs | nix-build -j0 \
+while read p; do
+  (echo "${p}" | grep "partition") && continue
+  (echo "${p}" | grep "fastboot") && continue
+
+  nix-build -j0 "${p}"\
     | grep -v fastboot \
     | grep -v partition \
     | grep -v PARTITION \
     | cachix push "colemickens"
+done </tmp/drvs
