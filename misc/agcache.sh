79e1ef7bb2bc5f3714edc69152fca8781bcd8951
Cole Mickens <cole.mickens@gmail.com>
2021-10-22 22:01:10 -0700

    wip

--- misc/agcache.sh
+++ misc/agcache.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 thing="${1}"
 
-nix path-info --derivation -r "${thing}" > /tmp/drvs
+nix path-info --derivation -r "${DIR}/..#${thing}" > /tmp/drvs
 
 cat /tmp/drvs | nix-build -j0 \
     | grep -v fastboot \
