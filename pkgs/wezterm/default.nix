def5188f10a3a45db32f7efd2c873f6f571ebcb0
Cole Mickens <cole.mickens@gmail.com>
2021-11-06 16:55:30 -0700

    auto-update(manual): wezterm: 24875004f66640e220bb1e4369afc93e5417e0c0 => f56a6aa276b7c4c5ed67d68cf033474fa057de29

---
+++ pkgs/wezterm/default.nix
@@ -0,0 +1,36 @@
+args_@{ lib
+, fetchFromGitHub
+, wezterm
+, zlib
+, ... }:
+
+let
+  metadata = import ./metadata.nix;
+  extraNativeBuildInputs = [
+    # "extra-cmake-modules" "pkg-config"
+  ];
+  extraBuildInputs = [
+    # "qqc2-desktop-style" "sonnet" "kio"
+    "zlib"
+  ];
+  ignore = [ "wezterm" "fetchFromGithub" ] ++ extraBuildInputs;
+  args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
+  newsrc = fetchFromGitHub {
+    owner = "wez";
+    repo = "wezterm";
+    inherit (metadata) rev sha256;
+  };
+in
+(wezterm.override args).overrideAttrs(old: {
+  pname = "wezterm";
+  version = "${metadata.rev}";
+  src = newsrc;
+  
+  cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+    src = newsrc;
+    outputHash = metadata.cargoSha256;
+  });
+
+  buildInputs = old.buildInputs ++ (map (n: args_.${n}) extraBuildInputs);
+  nativeBuildInputs = old.nativeBuildInputs ++ (map (n: args_.${n}) extraNativeBuildInputs);
+})
---
+++ pkgs/wezterm/metadata.nix
@@ -0,0 +1,7 @@
+{
+  repo_git = "https://github.com/wez/wezterm";
+  branch = "main";
+  rev = "f56a6aa276b7c4c5ed67d68cf033474fa057de29";
+  sha256 = "sha256-Fq/y6eePCv78Kdvjog0dsbuD6gUyKZzzEkgnXDJNIDE=";
+  cargoSha256 = "sha256-LAz9Fh3nUyikSaBf8p4lkQPiMuw2o7bAZla79hQSZAo=";
+}
