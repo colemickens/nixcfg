44f5d8df6fdf8e63c2994f95eb41ec34b398866c
Cole Mickens <cole.mickens@gmail.com>
2021-12-02 15:20:16 -0800

    auto-update(manual): meli: 330a2b20ed492f6b6ea86c196d43d67430487faa => d8e9a00563c023abb0ff75aaa4ba3fa92626c5ce

--- pkgs/meli/metadata.nix
+++ pkgs/meli/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://git.meli.delivery/meli/meli";
   branch = "master";
-  rev = "330a2b20ed492f6b6ea86c196d43d67430487faa";
-  sha256 = "sha256-qSa8sJEXlbwB901wmxO/xhNRvTtODFdH0BFAInj2Ze8=";
-  cargoSha256 = "sha256-d+vfBH/pdY6aFnMxssKEHuDkBiv3JC1sOcdUoA/LVmw=";
+  rev = "d8e9a00563c023abb0ff75aaa4ba3fa92626c5ce";
+  sha256 = "sha256-1vRcfcB9hchCZkMv/ViEKkYRmEwRoFxCJmbJQ2xEPPE=";
+  cargoSha256 = "sha256-u3I41GQJIJrMCfmbuAXWn/ki9E9xW/FqsWMfUPtc65A=";
 }
