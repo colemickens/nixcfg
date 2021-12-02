323c3e268ec7e2191496894d83abbcf503523cd6
Cole Mickens <cole.mickens@gmail.com>
2021-12-02 15:20:21 -0800

    auto-update(manual): poweralertd: d0e576a3a19c4257eb0dccdff15fc5d0a5d3d2c6 => 3c0c7c727db9c7fc03e790946541e2b2d0146cd2

--- pkgs/poweralertd/metadata.nix
+++ pkgs/poweralertd/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~kennylevinsen/poweralertd";
   branch = "master";
-  rev = "d0e576a3a19c4257eb0dccdff15fc5d0a5d3d2c6";
-  sha256 = "sha256-d7dRF5jBS/Bg41whYAmVnJf+Z3T0OfmrNYVjdglOPKc=";
+  rev = "3c0c7c727db9c7fc03e790946541e2b2d0146cd2";
+  sha256 = "sha256-1VS0WZR9zxkFoD0VFjiQBxUFsWTABBZfvDOPO8bfPyo=";
 }
