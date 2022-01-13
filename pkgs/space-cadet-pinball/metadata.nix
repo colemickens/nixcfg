134217f7a0d54985e9b17bb90f6d377f640bb635
Cole Mickens <cole.mickens@gmail.com>
2022-01-13 15:33:42 -0800

    auto-update(manual): space-cadet-pinball: 43af97127b3ae6cb8fb4e9aaebc0585aa2427dca => 8f34829b1e4756472ce6889f31eb875ce343fb05

--- pkgs/space-cadet-pinball/metadata.nix
+++ pkgs/space-cadet-pinball/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/k4zmu2a/SpaceCadetPinball";
   branch = "master";
-  rev = "de13d4e326b2dfa8e6dfb59815c0a8b9657f942d";
-  sha256 = "sha256-/nk4kNsmL1z2rKgV1dh9gcVjp8xJwovhE6/u2aNl/fA=";
+  rev = "8f34829b1e4756472ce6889f31eb875ce343fb05";
+  sha256 = "sha256-Rjrh06WyE1Wxih+cGsAXY3smKqZrRXQLOoEd+NgL0aU=";
 }
