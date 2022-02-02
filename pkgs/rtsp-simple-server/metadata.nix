0616e21e3dca9950461524ecd6055249cc135950
Cole Mickens <cole.mickens@gmail.com>
2022-02-02 14:36:57 -0800

    auto-update(manual): rtsp-simple-server: 9c38b42b4c95664603d2eebd0bb1edbc3ba58c01 => 7c1d329d613786515702dab7dbd079db1d8ad7a5

--- pkgs/rtsp-simple-server/metadata.nix
+++ pkgs/rtsp-simple-server/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/aler9/rtsp-simple-server";
   branch = "main";
-  rev = "9c38b42b4c95664603d2eebd0bb1edbc3ba58c01";
-  sha256 = "sha256-rfwuggENwSODZMsIt+NUbCAnXUNhmoAd+2FGx9bj7xE=";
-  vendorSha256 = "sha256-HWmktfSie2HvahMQG5+vYueHSeoJkzRSd0ozJldrTxg=";
+  rev = "7c1d329d613786515702dab7dbd079db1d8ad7a5";
+  sha256 = "sha256-EM43vkTl4nylzPUMvdPLPEHmseEfQh6cWGy9MtdX+90=";
+  vendorSha256 = "sha256-WmuCMaHQqvQIUDnkIZtgt1ZQrZJtUvE6SftzDUKRR6c=";
 }
