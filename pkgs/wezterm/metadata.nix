dc0158da11ba070d9e888bd93014a6ee95a92456
Cole Mickens <cole.mickens@gmail.com>
2022-02-09 13:39:37 -0800

    auto-update(manual): wezterm: 7a5956d3b8d8181ae81a5c71c32d08a4a870bdc9 => dbf190d414877cd025a234f11e83256e727527f8

--- pkgs/wezterm/metadata.nix
+++ pkgs/wezterm/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/wez/wezterm";
   branch = "main";
-  rev = "7a5956d3b8d8181ae81a5c71c32d08a4a870bdc9";
-  sha256 = "sha256-EoZt8Mw6S/iEDN0grGreaeZ+EhpgS7fRjcq20AXDTgs=";
+  rev = "dbf190d414877cd025a234f11e83256e727527f8";
+  sha256 = "sha256-2LPnBEW9AM+hHP/mKw7yCSSU520i/shEWV30w3qYJJs=";
   cargoSha256 = "sha256-iuMgP3lGY8vNe7+axImrrqjzoB0367yc6SL58Z5mZ34=";
 }
