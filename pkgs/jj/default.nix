14195e2abd8369fb081387a9200795a2f72b4da8
Cole Mickens <cole.mickens@gmail.com>
2022-02-14 17:28:44 -0800

    auto-update(manual): jj: cea3c1537a802b94e1e28f3f2eb27f328c3435f9 => 03e6b8c0e6a44d12494a7467f626108ebba2fbec

--- pkgs/jj/default.nix
+++ pkgs/jj/default.nix
@@ -11,8 +11,15 @@
 , makeWrapper
 }:
 
-let metadata = import ./metadata.nix; in
-rustPlatform.buildRustPackage rec {
+let metadata = {
+  repo_git = "https://github.com/martinvonz/jj";
+  branch = "main";
+  rev = "03e6b8c0e6a44d12494a7467f626108ebba2fbec";
+  sha256 = "sha256-mhK2fF54IjQjQpb9fzvjuLK/pzmAxWxOt6cDpjpFeT0=";
+  cargoSha256 = "sha256-lp42XmY7G+4ECtAfHOrBkWZmE6HBza5y3U17u0DWt40=";
+  # skip = true;
+};
+in rustPlatform.buildRustPackage rec {
   pname = "jujutsu";
   version = metadata.rev;
 
@@ -28,6 +35,7 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [ openssl dbus sqlite ];
 
   meta = with lib; {
+    verinfo = metadata;
     description = "Jujutsu (an experimental VCS)";
     homepage = "https://github.com/martinvonz/jj";
     license = licenses.asl20;
--- pkgs/jj/metadata.nix
+++
@@ -1,8 +0,0 @@
-{
-  repo_git = "https://github.com/martinvonz/jj";
-  branch = "main";
-  rev = "cea3c1537a802b94e1e28f3f2eb27f328c3435f9";
-  sha256 = "sha256-OJYEXLUS2jFb1D8c1O+OpKpSfpPV9VIPdKzHytPhzUE=";
-  cargoSha256 = "sha256-EuWrPEQyv1hiKHp5EuFwgWbb90qu4E6AwySRJMtzBCU=";
-  skip = true;
-}
