c166432762116c2cab2d41928444d859527282d9
Cole Mickens <cole.mickens@gmail.com>
2022-02-23 00:48:45 -0800

    auto-update(manual): onionbalance: c2b50f7f2de7fe4d1b596cfa61393f27715508ea => 1e4bae3409f14a1075365037d63a47c0329bab0c

--- pkgs/onionbalance/default.nix
+++ pkgs/onionbalance/default.nix
@@ -1,31 +1,52 @@
 { lib
-, fetchFromGitLab, buildPythonApplication
+#, fetchFromGitLab
+, fetchFromGitHub
+, buildPythonApplication
 , setuptools, setproctitle, stem, future, pyyaml, cryptography, pycrypto
 , pexpect, mock, pytest, pytest-mock, tox
+, pycryptodomex
+, coveralls, flake8, pylint
 }:
 
 let
   metadata = {
-    repo_git = "https://gitlab.torproject.org/tpo/core/onionbalance";
+    #repo_git = "https://gitlab.torproject.org/tpo/core/onionbalance";
+    repo_git = "https://github.com/colemickens/onionbalance";
     branch = "main";
-    rev = "c2b50f7f2de7fe4d1b596cfa61393f27715508ea";
-    sha256 = "sha256-21LqMaWGeGaDzpDQ+SwfrEGxMFKvnEd7F0OmqclQRE8=";
+    rev = "1e4bae3409f14a1075365037d63a47c0329bab0c";
+    sha256 = "sha256-keuV1Tjqd1c7+8o2AAcyO8kvCUoRdpd/WoiJL4DCtHc=";
   };
   version = builtins.substring 0 10 metadata.rev;
 in buildPythonApplication rec {
   pname = "onionbalance";
   inherit version;
 
-  src = fetchFromGitLab {
-    domain = "gitlab.torproject.org";
-    owner = "tpo";
-    repo = "core/onionbalance";
+  # src = fetchFromGitLab {
+  #   domain = "gitlab.torproject.org";
+  #   owner = "tpo";
+  #   repo = "core/onionbalance";
+  #   inherit (metadata) rev sha256;
+  # };
+  src = fetchFromGitHub {
+    owner = "colemickens";
+    repo = "onionbalance";
     inherit (metadata) rev sha256;
   };
 
-  propagatedBuildInputs = [ setuptools setproctitle stem pyyaml cryptography pycrypto future ];
+  propagatedBuildInputs = [
+    setuptools setproctitle stem pyyaml cryptography pycrypto future
+    pycryptodomex
+  ];
 
-  checkInputs = [ pexpect mock pytest pytest-mock tox ];
+  preCheck = ''
+    rm -rf test/functional/v2
+    rm -rf test/v2
+  '';
+
+  checkInputs = [
+    pexpect mock pytest pytest-mock tox
+    coveralls flake8 pylint
+  ];
 
   meta = with lib; {
     verinfo = metadata;
