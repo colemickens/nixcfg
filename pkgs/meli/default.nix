b7311ef3296850b9a4f1f587c2caf9c96e3cbc24
Cole Mickens <cole.mickens@gmail.com>
2022-02-25 23:38:02 -0800

    auto-update(manual): meli: e9aaa7b067903040acd7f3d7c685de94b3b98450 => d0de04854ec4770b54e4d8303a9b8ab9eb5d68b0

--- pkgs/meli/default.nix
+++ pkgs/meli/default.nix
@@ -18,9 +18,9 @@ let
   metadata ={
     repo_git = "https://git.meli.delivery/meli/meli";
     branch = "master";
-    rev = "e9aaa7b067903040acd7f3d7c685de94b3b98450";
-    sha256 = "sha256-TmpMAxLCPZ5gFxi1+jmLCwyaaaAXjpw8A3jgjlM4sHU=";
-    cargoSha256 = "sha256-m5GRmEJ5i/g8F8iShfOe0O+2YOjfr7DQrfcWrNL65I8=";
+    rev = "d0de04854ec4770b54e4d8303a9b8ab9eb5d68b0";
+    sha256 = "sha256-WUc6j1fxe6S1MmFPP4/vbSBl81+qhIngJ2M6IwBWVow=";
+    cargoSha256 = "sha256-XBDhBtxEpNC2AqSgLPe83ncthBS68kpWHyA8MzuWPLw=";
   };
 in rustPlatform.buildRustPackage rec {
   pname = "meli";
