15fd2112cd5ef279555ed687df1a92f09cbb6d06
Cole Mickens <cole.mickens@gmail.com>
2022-03-10 15:10:07 -0800

    auto-update(manual): wezterm: ce3dd004441a04fc6a43c2cac6b8b4f11d0de1ad => d5391fa520f8c780f587060b529189a12b382115

--- pkgs/wezterm/default.nix
+++ pkgs/wezterm/default.nix
@@ -11,8 +11,8 @@ let
   metadata = rec {
     repo_git = "https://github.com/wez/wezterm";
     branch = "main";
-    rev = "ce3dd004441a04fc6a43c2cac6b8b4f11d0de1ad";
-    sha256 = "sha256-AmRUrFq7hfay5O3xvRHGX4zI45kb3RSWKOUdkpCFTq4=";
+    rev = "d5391fa520f8c780f587060b529189a12b382115";
+    sha256 = "sha256-NAkrf+9By0Z5uTv+XR1erN8Gq4TlflfyAVtahUEe9HE=";
     cargoSha256 = "sha256-0tnzsSLcLHWuolZV2D7fq5bQiffs34l/jz6wCNqGOf0=";
   };
   extraNativeBuildInputs = [
@@ -32,7 +32,7 @@ let
     fetchSubmodules = true;
   };
 in
-(wezterm.override args).overrideAttrs(old: {
+(wezterm.override args).overrideAttrs(old: rec {
   pname = "wezterm";
   version = "${metadata.rev}";
   src = newsrc;
@@ -41,6 +41,9 @@ in
     src = newsrc;
     outputHash = metadata.cargoSha256;
   });
+  prePatch = ''
+    echo 
+  '';
 
   buildInputs = old.buildInputs ++ (map (n: args_.${n}) extraBuildInputs);
   nativeBuildInputs = old.nativeBuildInputs ++ (map (n: args_.${n}) extraNativeBuildInputs);
