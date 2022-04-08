6a8f603cb05c03b0109cfb17fb40f416cf356158
Cole Mickens <cole.mickens@gmail.com>
2022-04-08 12:58:12 -0700

    auto-update(manual): rtsp-simple-server: 0605a2f3692d1da4f38cf027516bc92c7ce842d8 => 2c485f918b43f0245f5ca0afb868c03b949131f3

--- pkgs/rtsp-simple-server/default.nix
+++ pkgs/rtsp-simple-server/default.nix
@@ -4,9 +4,9 @@ let
   metadata = {
     repo_git = "https://github.com/aler9/rtsp-simple-server";
     branch = "main";
-    rev = "0605a2f3692d1da4f38cf027516bc92c7ce842d8";
-    sha256 = "sha256-Lj+MUCZJoluj438dm42nj4jix2XmThQ6yeJOxkF6KYc=";
-    vendorSha256 = "sha256-YaHCJXyVfy+GIhCa+9CxZWzXGM/tOqTy9V2DDchFcyU=";
+    rev = "2c485f918b43f0245f5ca0afb868c03b949131f3";
+    sha256 = "sha256-UVvETWtAIXjVPY/fZo+dLuh7CT4cu6EP098k4TEij4g=";
+    vendorSha256 = "sha256-Gdhefux8/UF80ckTUkvY0Fgv3dUf/FyvWoTLvebSEmw=";
   };
 in buildGoModule rec {
   pname = "rtsp-simple-server";
