24a4220cc18cc86c25cdd2d72f9083354143d4c1
Cole Mickens <cole.mickens@gmail.com>
2022-06-24 23:03:13 -0700

    auto-update(manual): anodium: bf216d7e45fc2595302c6978d192ddd412d26f96 => eb4431ca4713490c6206c3c847b002100f3d65d8

--- pkgs/anodium/default.nix
+++ pkgs/anodium/default.nix
@@ -1,35 +1,66 @@
-{ stdenv, lib, fetchFromGitHub, rustPlatform
-, pkg-config, makeWrapper
-# , glib, cairo, pango, atk
-, mesa, udev, xorg
-, libinput, libseat
+{ stdenv
+, lib
+, fetchFromGitHub
+, rustPlatform
+, pkg-config
+, makeWrapper
+, glib
+, cairo
+, pango
+, atk
+, gdk-pixbuf
+, gtk3
+, gtk-layer-shell
+, mesa
+, udev
+, xorg
+, libinput
+, libseat
 }:
 
 let
-  metadata = import ./metadata.nix;
+  verinfo =
+    {
+      repo_git = "https://github.com/colemickens/Anodium";
+      branch = "master";
+      rev = "eb4431ca4713490c6206c3c847b002100f3d65d8";
+      sha256 = "sha256-v7jAI5/VaFfBl/IupAOPdJacA2sDP954W33rUO2EOGw=";
+      cargoSha256 = "sha256-4b1QDqdmrihnEb4u8wWE/hH65phTXV2dXQSUEgNW/bg=";
+    };
 in
 rustPlatform.buildRustPackage rec {
   name = "anodium";
-  version = metadata.rev;
+  version = verinfo.rev;
 
   src = fetchFromGitHub {
-    owner = "PolyMeilex";
+    owner = "colemickens";
     repo = "Anodium";
-    rev = version;
-    sha256 = metadata.sha256;
+    inherit (verinfo) rev sha256;
   };
 
-  cargoSha256 = metadata.cargoSha256;
+  cargoSha256 = verinfo.cargoSha256;
 
   nativeBuildInputs = [
     pkg-config
     makeWrapper
   ];
   buildInputs = [
-    udev mesa libinput libseat
-    xorg.libXcursor xorg.libXrandr xorg.libXi
+    udev
+    mesa
+    libinput
+    libseat
+    glib
+    cairo
+    pango
+    atk
+    gdk-pixbuf
+    gtk3
+    gtk-layer-shell
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
   ];
-  
+
   # TODO: makeWrapper for EGL?
 
   strictDeps = true; #?
@@ -37,7 +68,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "WIP Wayland Compositor";
     homepage = "https://github.com/PolyMeilex/Anodium";
-    verinfo = metadata;
+    verinfo = verinfo;
     # license = licenses.gpl3;
     maintainers = with maintainers; [ colemickens ];
   };
