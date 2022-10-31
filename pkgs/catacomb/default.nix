66201aaca2a1bdc0628a1469fe23704aa93c084b
Cole Mickens <cole.mickens@gmail.com>
2022-10-30 17:04:07 -0700

    auto-update(manual): catacomb: 64d7971fea8eb23513c41d1807c7e27beaeaf985 => dd6a02fd4c690242115628f800c168954009d70c

--- pkgs/catacomb/default.nix
+++ pkgs/catacomb/default.nix
@@ -26,8 +26,8 @@ let
     {
       repo_git = "https://github.com/${owner}/catacomb";
       branch = "master";
-      rev = "64d7971fea8eb23513c41d1807c7e27beaeaf985";
-      sha256 = "sha256-r0Xk4QyAGzkBimt9iqSLLnqyLqG8jyn70nu2YB7ETzo=";
+      rev = "dd6a02fd4c690242115628f800c168954009d70c";
+      sha256 = "sha256-2uMrmP4+4DecACcnd8DJU3ual+F5jIJYeg0TM4her44=";
       cargoSha256 = "sha256-2ftEk4pseNZ4K5DBY5sqVjTS4Jyx2rThdyaqjC2IsGg=";
     };
 in
