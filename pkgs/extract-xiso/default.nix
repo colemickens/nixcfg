294798320bef73e76dc3246dbcdf02bf960c58ec
Cole Mickens <cole.mickens@gmail.com>
2022-11-28 14:27:23 -0800

    auto-update: extract-xiso: d8646e90ae15bb918ec6fb7e1dc1b5ab0547bfee => 4488c39d7aa0bd0c371929a3fdeb456123aa46b3

--- pkgs/extract-xiso/default.nix
+++ pkgs/extract-xiso/default.nix
@@ -20,9 +20,9 @@ let
     };
     repo_git = "https://github.com/${github.owner}/${github.repo}";
     branch = "master";
-    rev = "d8646e90ae15bb918ec6fb7e1dc1b5ab0547bfb5";
-    sha256 = "sha256-rkxwQy29FfXbG78cAdm5en6eRzkg6Q5UOZTItlbbxwk=";
-    cargoSha256 = "sha256-5q5is9joQu6KHhQugUO1jt+/78bZtt/ciBSMxhS2IYg=";
+    rev = "4488c39d7aa0bd0c371929a3fdeb456123aa46b3";
+    sha256 = "";
+    cargoSha256 = "";
   };
 in
 stdenv.mkDerivation rec {
