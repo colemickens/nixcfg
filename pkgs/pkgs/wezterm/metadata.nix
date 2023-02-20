5cc130131d825945a1c3166cd07736ff7c61c9b3
Cole Mickens <cole.mickens@gmail.com>
2023-02-20 09:04:47 -0800

    auto-update: wezterm: 3b39aa551da19ba4c0138cd3b19a4bffcc219cf1 => 23211fc8a5df8ee3c491450f0fab54655c431ca9

--- pkgs/pkgs/wezterm/metadata.nix
+++ pkgs/pkgs/wezterm/metadata.nix
@@ -4,7 +4,7 @@ rec {
   repo = "wezterm";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "3b39aa551da19ba4c0138cd3b19a4bffcc219cf1";
-  sha256 = "sha256-68ykmwFxrQBa+EZD3GBbz6zi9EL0g6SYHIhjZuTS/O0=";
-  cargoSha256 = "sha256-qgB5m3vHI7uIQkTWHnIMn+ohSZa+5GMy02OLQH9e7pU=";
+  rev = "23211fc8a5df8ee3c491450f0fab54655c431ca9";
+  sha256 = "sha256-JvwXCz4DT5g+gKqgs/Hltj4ThVDDrT9JbzgUODMeyoc=";
+  cargoSha256 = "sha256-fXchqbVd0YrMLC/yj7JbUD7YN+8m2RKQgdFoB5Lqgc4=";
 }
