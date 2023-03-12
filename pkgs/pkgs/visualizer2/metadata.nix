bc4ddf3101e67e9c97e5aa2694a6e259bcc7aca3
Cole Mickens <cole.mickens@gmail.com>
2023-03-11 21:18:49 -0500

    auto-update: visualizer2: fa8da76c3a9432654922c65b3f6642d54fb9ba86 => 512e02ebe092ea2e87655fe22f925d0f27e5f0c3

--- pkgs/pkgs/visualizer2/metadata.nix
+++ pkgs/pkgs/visualizer2/metadata.nix
@@ -4,8 +4,8 @@ rec {
     repo = "visualizer2";
   };
   repo_git = "https://github.com/${github.owner}/${github.repo}";
-  branch = "master";
-  rev = "d8646e90ae15bb918ec6fb7e1dc1b5ab0547bfb5";
-  sha256 = "sha256-rkxwQy29FfXbG78cAdm5en6eRzkg6Q5UOZTItlbbxwk=";
-  cargoSha256 = "sha256-5q5is9joQu6KHhQugUO1jt+/78bZtt/ciBSMxhS2IYg=";
+  branch = "canon";
+  rev = "512e02ebe092ea2e87655fe22f925d0f27e5f0c3";
+  sha256 = "sha256-nQ4N9t/0UdZscj6ngP1XL3TronmAF/k07xCz5O3653E=";
+  cargoSha256 = "sha256-QozWKcGte/DV2JJVjyVa4f1t1xDlgbR+nGs36/pM+cI=";
 }
