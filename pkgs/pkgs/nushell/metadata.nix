c30dca375efa57eefb2cb5b0c4c1f7658ef1fdeb
Cole Mickens <cole.mickens@gmail.com>
2023-03-14 07:34:53 -0700

    auto-update: nushell: 86faf753bd864dcef54911d1ccba5c6785c12bdb => c7583ecdb71e4cd641020dbe78fb814c18de389e

--- pkgs/pkgs/nushell/metadata.nix
+++ pkgs/pkgs/nushell/metadata.nix
@@ -4,7 +4,7 @@ rec {
   repo = "nushell";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "86faf753bd864dcef54911d1ccba5c6785c12bdb";
-  sha256 = "sha256-dNRmSnbHOGyAWBsONrTMVOG/j+0rp0wX3CFS//hLMtE=";
-  cargoSha256 = "sha256-CBU2yERojb1KDr+CazEa9nsSpCuyKst77tJDU3fCCz8=";
+  rev = "c7583ecdb71e4cd641020dbe78fb814c18de389e";
+  sha256 = "sha256-6FnQoTZfAzLUSu2J+e6KQeon4IYUp6HXSmbl/8qkQeo=";
+  cargoSha256 = "sha256-ZjN5gbDLPrFYjt7cW8WmTf1au7i4hWs9cjMtI8GP29k=";
 }
