1922cdf35ba49a5619534913223f11a4c3c612c4
Cole Mickens <cole.mickens@gmail.com>
2023-04-17 10:41:31 -0500

    profiles/gui: fix pass-secret-service PASSWORD_STORE_LOCATION

--- profiles/gui.nix
+++ profiles/gui.nix
@@ -62,6 +62,8 @@ in
       services = {
         pass-secret-service = {
           enable = true;
+          # copied from profiles/interactive -> PASSWORD_STORE_DIR
+          storePath = "${hm.config.xdg.dataHome}/password-store";
         };
       };
 
