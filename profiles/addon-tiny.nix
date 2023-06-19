3c79c4e4e594b6e39ef683f9328cbf0ac82380f5
Cole Mickens <cole.mickens@gmail.com>
2023-06-18 23:04:09 -0600

    tiny: override openconnect to avoid gtk+3

---
+++ profiles/addon-tiny.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ... }:
+
+{
+  config = {
+    networking.networkmanager.plugins = lib.mkForce [];
+
+    services.fwupd.enable = lib.mkForce false;
+    services.udisks2.enable = lib.mkForce false;
+    hardware.usbWwan.enable = lib.mkForce false;
+
+    documentation.enable = false;
+    documentation.doc.enable = false;
+    documentation.info.enable = false;
+    documentation.nixos.enable = false;
+
+    # TODO: not sure about this one
+    fonts.fontconfig.enable = false;
+    
+    nixpkgs.overlays = [
+      (final: prev: {
+        gnupg23 = prev.gnupg23.override { openldap = null; };
+        openconnect = null;
+        # openfortivpn = null;
+        # networkmanager-fortisslvpn = null;
+      })
+    ];
+  };
+}
