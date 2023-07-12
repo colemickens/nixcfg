f44bfdce30cc03155df6b1084b1211bd72c49c44
Cole Mickens <cole.mickens@gmail.com>
2023-07-12 15:06:24 -0400

    wezterm: unstable-2023-07-11 -> unstable-2023-07-12
    Diff: https://github.com/wez/wezterm/compare/d07ec3e8bb14cf784f9ca74ed69f5ab03d685650...cc567ab4645bf76f3934d016a9adfea03b5e72f0

--- pkgs/wezterm/default.nix
+++ pkgs/wezterm/default.nix
@@ -37,15 +37,15 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "wezterm";
-  version = "unstable-2023-07-11";
-  rev = "d07ec3e8bb14cf784f9ca74ed69f5ab03d685650";
+  version = "unstable-2023-07-12";
+  rev = "cc567ab4645bf76f3934d016a9adfea03b5e72f0";
 
   src = fetchFromGitHub {
     owner = owner;
     repo = pname;
     rev = rev;
     fetchSubmodules = true;
-    sha256 = "sha256-5SEFwE8pbUrkp/Zme6goAPAOtl6QLErgndDYxxWpDsA=";
+    sha256 = "sha256-URW3imD6otF6AFVHNCOtq35XfCw59uoFGJYnguAZgGc=";
   };
 
   postPatch = ''
