a682d61fedfb6d7a4872116153751fef33013b2d
Cole Mickens <cole.mickens@gmail.com>
2023-09-01 11:25:56 -0500

    rio: unstable-2023-09-01 -> unstable-2023-09-01
    Diff: https://github.com/raphamorim/rio/compare/e827cd936f4de5317327b1ff7dbc6cb41e3a1f5e...0a0ceedcf784c600ffb45cb8681628036bb4880f
    
    Changelog: https://github.com/raphamorim/rio/blob/vunstable-2023-09-01/CHANGELOG.md

---
+++ pkgs/rio/default.nix
@@ -0,0 +1,106 @@
+{ lib
+, fetchFromGitHub
+, rustPlatform
+, nixosTests
+, nix-update-script
+
+, autoPatchelfHook
+, ncurses
+, pkg-config
+
+, gcc-unwrapped
+, fontconfig
+, libGL
+, vulkan-loader
+, libxkbcommon
+
+, withX11 ? true
+, libX11
+, libXcursor
+, libXi
+, libXrandr
+, libxcb
+
+, withWayland ? true
+, wayland
+}:
+let
+  rlinkLibs = [
+    (lib.getLib gcc-unwrapped)
+    fontconfig
+    libGL
+    libxkbcommon
+    vulkan-loader
+  ] ++ lib.optionals withX11 [
+    libX11
+    libXcursor
+    libXi
+    libXrandr
+    libxcb
+  ] ++ lib.optionals withWayland [
+    wayland
+  ];
+in
+rustPlatform.buildRustPackage rec {
+  pname = "rio";
+  version = "unstable-2023-09-01";
+
+  src = fetchFromGitHub {
+    owner = "raphamorim";
+    repo = "rio";
+    rev = "0a0ceedcf784c600ffb45cb8681628036bb4880f";
+    hash = "sha256-Kq9uhL15PEdQw+fauqxuMiquzXlz223M7SpPNyiHPpY=";
+  };
+
+  # cargoHash = "sha256-jev4fMwCygVzvrkI1nSE5tx59nGqNKQUHCNHvkr9R90=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    ncurses
+    pkg-config
+  ];
+
+  runtimeDependencies = rlinkLibs;
+
+  buildInputs = rlinkLibs;
+
+  outputs = [ "out" "terminfo" ];
+
+  buildNoDefaultFeatures = true;
+  buildFeatures = [
+    (lib.optionalString withX11 "x11")
+    (lib.optionalString withWayland "wayland")
+  ];
+
+  checkFlags = [
+    # Fail to run in sandbox environment.
+    "--skip=screen::context::test"
+  ];
+
+  postInstall = ''
+    install -dm 755 "$terminfo/share/terminfo/r/"
+    tic -xe rio,rio-direct -o "$terminfo/share/terminfo" misc/rio.terminfo
+    mkdir -p $out/nix-support
+    echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
+  '';
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [ "--version-regex" "v([0-9.]+)" ];
+    };
+
+    tests.test = nixosTests.terminal-emulators.rio;
+  };
+
+  meta = {
+    description = "A hardware-accelerated GPU terminal emulator powered by WebGPU";
+    homepage = "https://raphamorim.io/rio";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ otavio oluceps ];
+    platforms = lib.platforms.unix;
+    changelog = "https://github.com/raphamorim/rio/blob/v${version}/CHANGELOG.md";
+  };
+}
