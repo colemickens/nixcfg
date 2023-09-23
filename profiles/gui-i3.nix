66abfcf49c2cb7f8a4d10874c8ff5a737e2793d1
Cole Mickens <cole.mickens@gmail.com>
2023-09-23 23:31:20 +0200

    gui-i3: add to test steam

---
+++ profiles/gui-i3.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  imports = [
+    ./gui.nix
+  ];
+  config = {
+    environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
+    services.xserver = {
+      enable = true;
+
+      desktopManager = {
+        xterm.enable = false;
+      };
+
+      displayManager = {
+        defaultSession = "none+i3";
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu #application launcher most people use
+          i3status # gives you the default i3 status bar
+          i3lock #default i3 screen locker
+          i3blocks #if you are planning on using i3blocks over i3status
+        ];
+      };
+    };
+  };
+}
