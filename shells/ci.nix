e2ce0115a626a943069ad768813a6ce2f9f4bca9
Cole Mickens <cole.mickens@gmail.com>
2023-09-27 00:57:01 +0200

    shells/ci: add nix-fast-build

--- shells/ci.nix
+++ shells/ci.nix
@@ -25,6 +25,7 @@ minimalMkShell {
     ]) ++ [
       inputs.nix-eval-jobs.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
       inputs.nix-update.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
+      inputs.nix-fast-build.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
     ]
   );
 }
