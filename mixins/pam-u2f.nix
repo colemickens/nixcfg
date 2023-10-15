4bb12947a8abefdb0dc6d567dc8403b00ad470ab
Cole Mickens <cole.mickens@gmail.com>
2023-10-15 23:09:22 +0200

    mixins/pam-u2f.nix: add cole's mapping

--- mixins/pam-u2f.nix
+++ mixins/pam-u2f.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, inputs, ... }:
 
 let
+  mapping = "cole:ffKBkSoiV7KcwUcdpjagrP9P4Gj4SFLxnZHRp3gy/jZbOD3J5xGCyidt9ruyA9ZT+DkV/lKd78Wy4RAEM8qodQ==,DfllypvIlTkhzZKEdVdRQb7iYev7K0TvpzaREkT2CFlhz4j/3vwWHzGBpQToxA2YOR1/Mwbm0cb4VxDJXs4k9g==,es256,+presence";
 in
 {
   config = {
@@ -8,6 +9,10 @@ in
       u2f = {
         enable = true;
       };
+    };
+
+    home-manager.users.cole = { pkgs, ... }: {
+      xdg.configFile."Yubico/u2f_keys".text = mapping;
     };
   };
 }
