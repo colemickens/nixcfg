9d4f8338b7f5e87959665f832c8694cc8bd802a9
Cole Mickens <cole.mickens@gmail.com>
2023-11-01 21:34:02 -0500

    shells/ci.nix: include fast-flake-update

--- shells/ci.nix
+++ shells/ci.nix
@@ -24,6 +24,7 @@ minimalMkShell {
       inputs.nix-eval-jobs.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
       inputs.nix-update.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
       inputs.nix-fast-build.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
+      inputs.fast-flake-update.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
     ]
   );
 }
