d2777d09a2c05374785fa02e628e32e95393dc9f
Cole Mickens <cole.mickens@gmail.com>
2023-11-04 13:43:13 -0500

    mixins/pam-u2f.nix: re-enable lock with yubikey removal

--- mixins/pam-u2f.nix
+++ mixins/pam-u2f.nix
@@ -5,13 +5,23 @@ let
 
   # TODO: imagine how to make this be smarter, multi-user, multi-waylands?
   # or just simply not WAYLAND_DISPLAY=1?
+  # userSwayLock = pkgs.writeShellScript "ykswaylock.sh" ''
+  #   export WAYLAND_DISPLAY=wayland-1
+  #   export XDG_RUNTIME_DIR=/run/user/1000
+  #   date '+%s'
+  #   ${pkgs.swaylock}/bin/swaylock --debug --daemonize
+  # '';
+  # ykDisconnect = pkgs.writeShellScript "yk-disconnect.sh" ''
+  #   echo "$1 - ykdisconnect"
+  #   env |& sed  "s/^/[$1] /" >> /tmp/ykdisconnect.env
+  #   echo "[$1]---------------" >>/tmp/ykdisconnect.env
+  #   /run/current-system/sw/bin/sudo -u cole ${userSwayLock} $1 \
+  #     |& sed  "s/^/[$1] /" >>/tmp/ykdisconnect.log & 2>&1
+  #   echo "done $1" >>/tmp/ykdisconnect.log
+  # '';
+
   ykDisconnect = pkgs.writeShellScript "yk-disconnect.sh" ''
-    /run/current-system/sw/bin/sudo -u cole \
-      env \
-        WAYLAND_DISPLAY=wayland-1 \
-        XDG_RUNTIME_DIR=/run/user/1000 \
-          ${pkgs.swaylock}/bin/swaylock --debug --daemonize \
-            |& sed  "s/^/[$1] /" >> /tmp/ykdisconnect.log &
+    ${pkgs.procps}/bin/pkill -USR1 swayidle
   '';
 in
 {
@@ -30,9 +40,9 @@ in
     # --- we end up getting a log that shows BOTH attempts to start swaylock fail, supposedly taking the lock
     # --- even though the lock screen does render for a good 1000-ish-ms.
     # --- log: https://gist.github.com/colemickens/ee0da4f9ebcabc3049c69027aecdafa6
-    # services.udev.extraRules = ''
-    #   ACTION=="remove", SUBSYSTEM=="usb", ENV{PRODUCT}=="1050/406/543", RUN+="${ykDisconnect} '%E{SEQNUM}'"
-    # '';
+    services.udev.extraRules = ''
+      ACTION=="remove", SUBSYSTEM=="usb", ENV{PRODUCT}=="1050/406/543", RUN+="${ykDisconnect} '%E{SEQNUM}'"
+    '';
 
     home-manager.users.cole = { pkgs, ... }: {
       xdg.configFile."Yubico/u2f_keys".text = mapping;
