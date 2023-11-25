adbddd16aaa6d2fa15a75ce3dd67599065475e64
Cole Mickens <cole.mickens@gmail.com>
2023-11-24 20:45:21 -0500

    flake.nix: test specific rev of nfb

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
     };
     # nix-fast-build = { url = "github:Mic92/nix-fast-build"; };
     nix-fast-build = {
-      url = "github:colemickens/nix-fast-build";
+      url = "github:colemickens/nix-fast-build/e7c7050412040a2fe49a0011ed5e06d4b25ab863";
       inputs."nixpkgs".follows = "cmpkgs";
     };
     fast-flake-update = {
