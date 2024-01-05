60e317a8222b0d1b42a3383c9f531addc2681de9
Cole Mickens <cole.mickens@gmail.com>
2024-01-04 21:18:48 -0600

    mixins/iwd-networks: add GoonNet

--- mixins/iwd-networks.nix
+++ mixins/iwd-networks.nix
@@ -14,12 +14,17 @@ in
         sopsFile = ../secrets/encrypted/iwd_Mickey.psk;
         format = "binary";
       };
+      "iwd_GoonNet.psk" = {
+        sopsFile = ../secrets/encrypted/iwd_GoonNet.psk;
+        format = "binary";
+      };
     };
 
     systemd.tmpfiles.rules = [
       # "C /var/lib/iwd/chimera-iot.psk 0400 root root - /run/secrets/iwd_network_chimera-iot.psk"
       "C /var/lib/iwd/chimera-iot.psk 0600 root root - ${n1}"
-      "C /var/lib/iwd/Mickey.psk 0600 root root - ${config.sops.secrets."iwd_Mickey.psk".path}"
+      (let h = "Mickey"; in "C /var/lib/iwd/${h}.psk 0600 root root - ${config.sops.secrets."iwd_${h}.psk".path}")
+      (let h = "GoonNet"; in "C /var/lib/iwd/${h}.psk 0600 root root - ${config.sops.secrets."iwd_${h}.psk".path}")
     ];
   };
 }
