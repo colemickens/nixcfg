f4706d17c8f96c9526073da0b2e3a144b649487f
Cole Mickens <cole.mickens@gmail.com>
2024-01-12 19:52:15 -0600

    profiles/addon-clouddev.nix: openvscode-server experiments

---
+++ profiles/addon-clouddev.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, inputs, ... }:
+
+# these are dev tools that we want available
+# system wide on my dev machine(s)
+
+{
+  config = {
+    networking.firewall.interfaces."tailscale0".allowedTCPPorts = [
+      3000
+    ];
+    services = {
+      openvscode-server = {
+        enable = true;
+        user = "cole";
+        group = "cole";
+        host = "0.0.0.0"; # firewall protects us, we only allow in tailscale0
+        port = 7777;
+      };
+    };
+  };
+}
