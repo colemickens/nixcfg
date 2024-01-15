ec4d4f142c9526a548f70d465c19706e7e470a1f
Cole Mickens <cole.mickens@gmail.com>
2024-01-15 17:31:44 -0600

    wp6: script, modernize further, default on

---
+++ hosts/xeep/services/home-assistant/esphome/wp6_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+esphome run --no-logs wp6_sw102.yaml --device 192.168.1.166; sleep 60
+esphome run --no-logs wp6_sw103.yaml --device 192.168.1.229; sleep 60
+esphome run --no-logs wp6_sw104.yaml --device 192.168.1.193; sleep 60
+esphome run --no-logs wp6_sw105.yaml --device 192.168.1.195; sleep 60
+esphome run --no-logs wp6_sw106.yaml --device 192.168.1.176; sleep 60
+esphome run --no-logs wp6_sw107.yaml --device 192.168.1.197; sleep 60
+esphome run --no-logs wp6_sw108.yaml --device 192.168.1.198; sleep 60
+esphome run --no-logs wp6_sw109.yaml --device 192.168.1.217; sleep 60
