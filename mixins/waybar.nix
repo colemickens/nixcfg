1082f061af0d731e622926299a92bed73074c568
Cole Mickens <cole.mickens@gmail.com>
2024-01-15 21:08:14 -0600

    mixins/waybar: fixup powerprofilesctl workaround

--- mixins/waybar.nix
+++ mixins/waybar.nix
@@ -15,7 +15,7 @@ let
 
   pppScript = pkgs.writeShellScript "waybar-ppp.sh" ''
     set -euo pipefail
-    val="$(timeout 5 ${pkgs.power-profiles-daemon}/bin/powerprofilesctl get)"
+    val="$(${pkgs.coreutils}/bin/timeout 5 ${pkgs.power-profiles-daemon}/bin/powerprofilesctl get)"
     if [[ "''${val}" == "performance" ]]; then
       echo $'{"text": "ppp prf"}';
     elif [[ "''${val}" == "balanced" ]]; then
