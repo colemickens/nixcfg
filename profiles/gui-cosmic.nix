83291800a660c161740b24a45ddd462fc498e7b9
Cole Mickens <cole.mickens@gmail.com>
2024-01-20 10:34:46 -0600

    profiles/gui-cosmic: init new profile for cosmic

---
+++ profiles/gui-cosmic.nix
@@ -0,0 +1,50 @@
+{ pkgs, lib, config, inputs, ... }:
+
+let
+  prefs = import ../mixins/_preferences.nix { inherit inputs config lib pkgs; };
+in
+{
+  imports = [
+    ./gui-wayland.nix
+  ];
+
+  config = {
+    services.xserver.desktopManager.cosmic = {
+      enable = true;
+    };
+
+    home-manager.users.cole = { pkgs, config, ... }@hm:
+      {
+        home.packages = with pkgs; [  ];
+
+        home.sessionVariables = {
+          # WLR_RENDERER = "vulkan";
+          # XDG_CURRENT_DESKTOP = "sway";
+        };
+
+        # xdg.portal = {
+        #   enable = true;
+        #   extraPortals = with pkgs; [
+        #     xdg-desktop-portal-gtk
+        #     xdg-desktop-portal-wlr
+        #   ];
+        #   config = {
+        #     common = {
+        #       default = [ "gtk" ];
+        #     };
+        #     sway = {
+        #       default = [ "gtk" ];
+        #       "org.freedesktop.impl.portal.Screencast" = [ "wlr" ];
+        #       "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
+        #     };
+        #   };
+        #   #   extraPortal = with pkgs; [
+        #   #     xdg-desktop-portal-wlr
+        #   #     (xdg-desktop-portal-gtk.override {
+        #   #       buildPortalsInGnome = false;
+        #   #     })
+        #   #   ];
+        # };
+    };
+  };
+}
