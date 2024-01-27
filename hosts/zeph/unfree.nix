daa40578e8deb2a5ac7c6b4cf151086c48c50044
Cole Mickens <cole.mickens@gmail.com>
2024-01-27 11:49:19 -0600

    hosts/zeph: add newer trezor to unfree

--- hosts/zeph/unfree.nix
+++ hosts/zeph/unfree.nix
@@ -10,6 +10,7 @@
 
       # trezor-suite... is unfree?
       "trezor-suite-23.12.3" # why's the version inside?
+      "trezor-suite-24.1.2" # why's the version inside?
 
       "ngrok" # ugh, dev for work
 
