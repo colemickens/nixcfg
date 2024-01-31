84c42fd60f5afef9031339b80eb5be0c15afd217
Cole Mickens <cole.mickens@gmail.com>
2024-01-31 10:42:49 -0600

    mixins/common.nix: re-enable no-amd-hdr just in case

--- mixins/common.nix
+++ mixins/common.nix
@@ -134,13 +134,13 @@ in
           boot.initrd.luks.devices."nixos-luksroot".fallbackToPassword = true;
         };
       };
-      ## we've had no issues, let's get rid of an extra big eval
-      # "no-amd-hdr" = lib.mkIf cfg.kernelPatchHDR {
-      #   inheritParentConfig = true;
-      #   configuration = {
-      #     nixcfg.common.kernelPatchHDR = lib.mkForce false;
-      #   };
-      # };
+      # we've had no issues, let's get rid of an extra big eval
+       "no-amd-hdr" = lib.mkIf cfg.kernelPatchHDR {
+         inheritParentConfig = true;
+         configuration = {
+           nixcfg.common.kernelPatchHDR = lib.mkForce false;
+         };
+       };
     };
 
     boot.kernelPatches = lib.mkIf (cfg.kernelPatchHDR) [
