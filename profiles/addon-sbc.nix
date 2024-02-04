d2ed53dd3304c902661953d2c76ea0c18719530d
Cole Mickens <cole.mickens@gmail.com>
2024-02-03 20:58:06 -0600

    profiles/addon-sbc: add gstreamer

--- profiles/addon-sbc.nix
+++ profiles/addon-sbc.nix
@@ -3,5 +3,17 @@
 {
   config = {
     networking.wireless.iwd.enable = true;
+
+    # include gstreamer
+    environment.systemPackages = with pkgs; [
+      # gst-launch-1
+      # extra plugins, etc
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-libav
+    ];
   };
 }
