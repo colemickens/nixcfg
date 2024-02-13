0fb85123e68fd05347598953ed1857b6244e68dc
Cole Mickens <cole.mickens@gmail.com>
2024-02-12 22:12:14 -0600

    mixins/jj: config changes

---
+++ docs/jj-notes.md
@@ -0,0 +1,5 @@
+# misc jj notes
+
+`jj git fetch --all-remotes`
+
+`jj git clone git@github.com:foo/bar` (not colocated, can only use `jj` commands)
--- mixins/jj.nix
+++ mixins/jj.nix
@@ -18,15 +18,34 @@
               name = "Cole Mickens";
               email = "cole.mickens@gmail.com";
             };
-            ui = {
-              color = "always";
-            };
             core = {
               fsmonitor = "watchman";
             };
             git = {
               # abandon-unreachable-commits = true; # ? TODO: not sure if better to do manually
               push-branch-prefix = "colemickens/push-";
+            };
+            template-aliases = {
+              one = ''
+                if(root,
+                  builtin_log_root(change_id, commit_id),
+                  label(if(current_working_copy, "working_copy"),
+                    concat(
+                      separate(" ",
+                        builtin_change_id_with_hidden_and_divergent_info,
+                        if(conflict, label("conflict", "conflict")),
+                        if(empty, label("empty", "(empty)")),
+                        if(description, description.first_line(), description_placeholder),
+                        format_short_commit_id(commit_id),
+                        git_head,
+                        branches,
+                        tags,
+                        working_copies,
+                      ) ++ "\n",
+                    ),
+                  )
+                )
+              '';
             };
           };
         };
