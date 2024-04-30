2c772f64b635bf1ce91b1efea257444a54fa1812
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-04-30 12:20:17 -0700

    git-repo-manager: 0.7.15-unstable-2024-02-01 -> 0.7.16-unstable-2024-04-27
    Diff: https://github.com/hakoerber/git-repo-manager/compare/ea7299a14788556536e00bf0311bd5f650ad2600...3a433a1acdf99e4f0bd7e6726ea6a2814f82b26d

--- pkgs/git-repo-manager/default.nix
+++ pkgs/git-repo-manager/default.nix
@@ -38,15 +38,15 @@ in
   # ]);
 rustPlatform.buildRustPackage (
   let
-    version = "0.7.15-unstable-2024-02-01";
-    rev = "ea7299a14788556536e00bf0311bd5f650ad2600";
+    version = "0.7.16-unstable-2024-04-27";
+    rev = "3a433a1acdf99e4f0bd7e6726ea6a2814f82b26d";
     owner = "hakoerber";
     pname = "git-repo-manager";
     src = fetchFromGitHub {
       owner = owner;
       repo = pname;
       rev = rev;
-      hash = "sha256-pd7MZqQngyRuOLW67lx7a22pZrQjgTvNCDxzzHjxT8g=";
+      hash = "sha256-OFB7X8jIo2cSkOfk5t7W/WI/BIwZARRanfawHtjNz14=";
     };
   in
   {
