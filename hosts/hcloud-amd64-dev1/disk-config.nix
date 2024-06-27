905507248b37c782a69d7fbd1ab1509e750b743b
Cole Mickens <cole.mickens@gmail.com>
2024-06-26 19:31:33 -0700

    hcloud: disko: try to use persistent disk for home+nix

--- hosts/hcloud-amd64-dev1/disk-config.nix
+++ hosts/hcloud-amd64-dev1/disk-config.nix
@@ -2,33 +2,34 @@
 { lib, ... }:
 {
   disko.devices = {
-    # disk.disk2 = {
-    #   device = lib.mkDefault "/dev/disk/by-path/pci-0000:06:00.0-scsi-0:0:0:2";
-    #   type = "disk";
-    #   content = {
-    #     type = "gpt";
-    #     partitions = {
-    #       home = {
-    #         name = "home";
-    #         size = "24G";
-    #         content = {
-    #           type = "filesystem";
-    #           format = "ext4";
-    #           mountpoint = "/home";
-    #         };
-    #       };
-    #       nix = {
-    #         name = "home";
-    #         size = "74G";
-    #         content = {
-    #           type = "filesystem";
-    #           format = "ext4";
-    #           mountpoint = "/nix";
-    #         };
-    #       };
-    #     };
-    #   };
-    # };
+    # uncommenting this breaks nixos-anywhere deployment
+    disk.disk2 = {
+      device = lib.mkDefault "/dev/disk/by-path/pci-0000:06:00.0-scsi-0:0:0:2";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          home = {
+            name = "home";
+            size = "24G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
+          };
+          nix = {
+            name = "home";
+            size = "74G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/nix";
+            };
+          };
+        };
+      };
+    };
     disk.disk1 = {
       device = lib.mkDefault "/dev/disk/by-path/pci-0000:06:00.0-scsi-0:0:0:1";
       type = "disk";
