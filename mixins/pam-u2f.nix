378df6a0c8f0341ade0bbe72687f65bb294b0dbc
Cole Mickens <cole.mickens@gmail.com>
2024-07-29 17:45:11 -0700

    mixins/pam-u2f.nix: roll usb-a key binding

--- mixins/pam-u2f.nix
+++ mixins/pam-u2f.nix
@@ -24,7 +24,7 @@ in
     # (NOTE: "Yubico" is just a questionable default, apparently, see "authFile" option)
     home-manager.users.cole =
       let
-        mapping_usba = "ffKBkSoiV7KcwUcdpjagrP9P4Gj4SFLxnZHRp3gy/jZbOD3J5xGCyidt9ruyA9ZT+DkV/lKd78Wy4RAEM8qodQ==,DfllypvIlTkhzZKEdVdRQb7iYev7K0TvpzaREkT2CFlhz4j/3vwWHzGBpQToxA2YOR1/Mwbm0cb4VxDJXs4k9g==,es256,+presence";
+        mapping_usba = "UE3cSMtyI9d4yxI7Bghg+FLiZgLaT+bjluUPuBO1HUMzzgIlIKxxGRpasmMDH+cmIaq5iobCaEu2xT1YpikU8g==,8Bgw3GYK8m9ecPaPpFkneNsREQL7RlZL7hTfEGAfm4u9Dmk22Xn3pf/+TwzpsyG1c1RBOEJkBvSvZ70P8tAgVA==,es256,+presence+pin";
         mapping_usbc = "GxHJs340o/neC4/4jQKPwUM9TodnS7IEZ/onrzzY8UTLagT7dpi2lzZ8vANmXI9O4JeaK68XlhfBuFJbSPmVjg==,6/v6kyT7Oj8RfcBQYPuncn7PPM6/CHYSYSmeTfWJkdZoal0Bk3hqkGeA3QwK9PYqSrZgKMIDR4pmQRlKDl/SQg==,es256,+presence";
       in
       ({ pkgs, ... }:
