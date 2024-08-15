b619a36ce0200b54bcae1315faba5ffc3836216b
Cole Mickens <cole.mickens@gmail.com>
2024-08-15 09:45:10 -0700

    mixins/pam2uf: roll fido unlocks again...

--- mixins/pam-u2f.nix
+++ mixins/pam-u2f.nix
@@ -24,8 +24,9 @@ in
     # (NOTE: "Yubico" is just a questionable default, apparently, see "authFile" option)
     home-manager.users.cole =
       let
-        mapping_usba = "UE3cSMtyI9d4yxI7Bghg+FLiZgLaT+bjluUPuBO1HUMzzgIlIKxxGRpasmMDH+cmIaq5iobCaEu2xT1YpikU8g==,8Bgw3GYK8m9ecPaPpFkneNsREQL7RlZL7hTfEGAfm4u9Dmk22Xn3pf/+TwzpsyG1c1RBOEJkBvSvZ70P8tAgVA==,es256,+presence+pin";
-        mapping_usbc = "GxHJs340o/neC4/4jQKPwUM9TodnS7IEZ/onrzzY8UTLagT7dpi2lzZ8vANmXI9O4JeaK68XlhfBuFJbSPmVjg==,6/v6kyT7Oj8RfcBQYPuncn7PPM6/CHYSYSmeTfWJkdZoal0Bk3hqkGeA3QwK9PYqSrZgKMIDR4pmQRlKDl/SQg==,es256,+presence";
+        # `pam_u2f` (don't use -N, pam ends up burning fido retries and locking it out!)
+        mapping_usba = "2SuopB53dlt3MXGqlC9s1bNu8QlLXWvP0vI50J34d9yyWHhckMF2wb44a3TBAtSuDJmlGEDica02Je6u+2DW1g==,96hoe48WSy6+CasvLxSvi4k0Cqbjf+68MJvO23RLJD9HgtoFlj8vcTPwkV/I664By8Rw07WT/Cj8j86CicyMUA==,es256,+presence";
+        mapping_usbc = "Lzrc/jEJLZyBus5Cq8ufhtarA14RBs9WByxwhhWumK5lLOWu4kSn9ptkcJFlqbLldBUwMRV/JYzIKcXdPBXSaQ==,10K/2WFm1fb2ue9Bw2NxNIL2BRvab8TBYuM4J56yjo3bf1H8HB93580Ci4AY9QdaPEOK2LlqClG/exxF7L+02A==,es256,+presence";
       in
       ({ pkgs, ... }:
       {
