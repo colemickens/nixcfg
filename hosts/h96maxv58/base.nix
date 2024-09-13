5a92163ea1abc25b4260784e9e4d9b572192747c
Cole Mickens <cole.mickens@gmail.com>
2024-09-13 11:22:51 -0700

    hosts/h96maxv58: use upstream armbian-firmware

--- hosts/h96maxv58/base.nix
+++ hosts/h96maxv58/base.nix
@@ -17,19 +17,17 @@ in
     inputs.h96.outputs.nixosModules.kernel-config
     inputs.h96.outputs.nixosModules.device-tree
 
+    inputs.disko.nixosModules.disko
+
     ../../profiles/core.nix
     ../../profiles/user-cole.nix
     ../../profiles/user-jeff.nix
-
-    # ../../profiles/gui-sway-auto.nix
 
     ../../mixins/common.nix
     ../../mixins/iwd-networks.nix
     ../../mixins/sshd.nix
     ../../mixins/tailscale.nix
     ../../mixins/unifi.nix
-
-    inputs.disko.nixosModules.disko
   ];
 
   config = {
@@ -37,6 +35,7 @@ in
 
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
       "armbian-firmware"
+      "armbian-firmware-unstable"
       "unifi-controller"
       "mongodb"
     ];
@@ -52,67 +51,37 @@ in
         rm $disk
       )
     '';
-    disko.devices = {
-      disk = {
-        disk0 = {
-          type = "disk";
-          imageSize = "4G";
-          content = {
-            type = "gpt";
-            partitions = {
-              firmware = {
-                start = "64";
-                alignment = 1;
-                end = "61440";
-              };
-              ESP = {
-                start = "64M";
-                end = "512M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
-              };
-              rootfs = {
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
-              };
+    disko.devices.disk.disk0 = {
+      type = "disk";
+      imageSize = "4G";
+      content = {
+        type = "gpt";
+        partitions = {
+          firmware = {
+            start = "64";
+            alignment = 1;
+            end = "61440";
+          };
+          ESP = {
+            start = "64M";
+            end = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          rootfs = {
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
       };
     };
-
-    hardware.firmware = [
-      (pkgs.armbian-firmware.overrideAttrs {
-        src = pkgs.fetchFromGitHub {
-          owner = "armbian";
-          repo = "firmware";
-          rev = "6c1532bccd4f99608d7f09a0f115214a7867fb0a";
-          hash = "sha256-DlRKCLOGW15FNfuzB/Ua2r1peMn/xHBuhOEv+e3VvTk=";
-        };
-        compressFirmware = false;
-        installPhase = ''
-                  runHook preInstall
-
-                  mkdir -p $out/lib/firmware
-                  cp -a * $out/lib/firmware/
-
-          #        ln -sf $out/lib/firmware/ap6275p/fw_bcm43752a2_pcie_ag.bin $out/lib/firmware/brcm/brcmfmac43752-pcie.bin
-          #        ln -sf $out/lib/firmware/ap6275p/fw_bcm43752a2_pcie_ag.bin $out/lib/firmware/brcm/brcmfmac43752-pcie.rockchip,rk3588.bin
-          #        ln -sf $out/lib/firmware/ap6275p/clm_bcm43752a2_pcie_ag.blob $out/lib/firmware/brcm/brcmfmac43752-pcie.clm_blob
-          #        ln -sf $out/lib/firmware/ap6275p/nvram_AP6275P.txt $out/lib/firmware/brcm/brcmfmac43752-pcie.txt
-          #        ln -sf $out/lib/firmware/ap6275p/config.txt $out/lib/firmware/brcm/brcmfmac43752-pcie-more-config.txt
-
-                  runHook postInstall
-        '';
-      })
-    ];
 
     nixpkgs.overlays = [
       (final: super: {
@@ -129,6 +98,8 @@ in
       bottom
     ];
 
+    hardware.firmware = [ pkgs.armbian-firmware ];
+
     services.pipewire.enable = true;
     services.pipewire.pulse.enable = true;
 
@@ -138,8 +109,6 @@ in
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.systemd-boot.installDeviceTree = true;
-
-    # add hardware device tree
 
     networking.wireless.enable = lib.mkForce false;
     networking.wireless.iwd.enable = true;
