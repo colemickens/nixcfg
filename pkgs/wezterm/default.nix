c229401518bdebf718cc49cb472e37ea6cbc3e49
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-09-29 21:56:41 -0700

    wezterm: 20240203-110809-5046fc22-unstable-2024-09-18 -> 20240203-110809-5046fc22-unstable-2024-09-29
    Diff: https://github.com/wez/wezterm/compare/832c533c5f9e8d1c3315f2fbf96c125927a232f7...a2f2c07a29f5c98f6736cde0c86b24887f9fd48a

--- pkgs/wezterm/default.nix
+++ pkgs/wezterm/default.nix
@@ -37,15 +37,15 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "wezterm";
-  version = "20240203-110809-5046fc22-unstable-2024-09-18";
-  rev = "832c533c5f9e8d1c3315f2fbf96c125927a232f7";
+  version = "20240203-110809-5046fc22-unstable-2024-09-29";
+  rev = "a2f2c07a29f5c98f6736cde0c86b24887f9fd48a";
 
   src = fetchFromGitHub {
     owner = owner;
     repo = pname;
     rev = rev;
     fetchSubmodules = true;
-    sha256 = "sha256-clrhggSQ0E/U3LGUU2ojNUZHsPKqlYVP971kF9LCRs4=";
+    sha256 = "sha256-KY/9DpHPLKv68a57CFvtRhnhxsXtJ1UmYMfeo3QH9qs=";
   };
 
   postPatch = ''
