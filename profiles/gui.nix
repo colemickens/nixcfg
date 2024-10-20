bbaaa7ef070f28fe2e600ca7e91bb4ff64bec178
Cole Mickens <cole.mickens@gmail.com>
2024-10-20 07:28:01 -0700

    profiles/gui: power on bluetooth at boot

--- profiles/gui.nix
+++ profiles/gui.nix
@@ -39,7 +39,7 @@ in
 
     hardware.bluetooth = {
       enable = true;
-      powerOnBoot = false;
+      powerOnBoot = true;
       settings = {
         General = {
           Experimental = true;
