3ee95f79ecd3ea23db5cd487e29084c9f5d0da9b
Cole Mickens <cole.mickens@gmail.com>
2024-10-29 08:52:12 -0700

    profiles/asus: remove supergfxctl

--- profiles/addon-asus.nix
+++ profiles/addon-asus.nix
@@ -15,19 +15,19 @@
         enable = true;
         enableUserService = true;
       };
-      supergfxd = {
-        enable = true;
-        settings = {
-          always_reboot = false;
-          no_logind = true;
-          mode = "Integrated";
-          # mode = "Hybrid";
-          vfio_enable = false;
-          vfio_save = false;
-          logout_timeout_s = 180;
-          hotplug_type = "None";
-        };
-      };
+      # supergfxd = {
+      #   enable = true;
+      #   settings = {
+      #     always_reboot = false;
+      #     no_logind = true;
+      #     mode = "Integrated";
+      #     # mode = "Hybrid";
+      #     vfio_enable = false;
+      #     vfio_save = false;
+      #     logout_timeout_s = 180;
+      #     hotplug_type = "None";
+      #   };
+      # };
     };
   };
 }
