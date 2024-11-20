d1da2c53b1d31561a213a983baa6d40d6c202da6
Cole Mickens <cole.mickens@gmail.com>
2024-11-19 16:56:58 -0800

    profiles/clouddev: add SSL for ds-ws-colemickens; self-signed only

--- profiles/addon-clouddev.nix
+++ profiles/addon-clouddev.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   config,
   inputs,
   ...
@@ -10,7 +11,10 @@
 
 {
   config = {
-    networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 7777 ];
+    networking.firewall.interfaces."tailscale0".allowedTCPPorts = [
+      7777
+      7778
+    ];
     environment.systemPackages = [
       # (pkgs.writeShellScriptBin "start-openvscode-server" ''
       #   set -x
@@ -31,6 +35,42 @@
         #   NIX_PATH = "nixpkgs=/home/cole/code/nixpkgs/cmpkgs";
         # };
       };
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+    };
+
+    services.nginx.virtualHosts."ds-ws-colemickens.mickens.us" = {
+        listen = [{port = 7778;  addr="0.0.0.0"; ssl = true;}];
+
+        addSSL = true;
+        enableACME = true;
+        #root = "/var/www/ds-ws-colemickens.mickens.us";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:7777/";
+          proxyWebsockets = true;
+        };
+    };
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "foo@bar.com";
+      defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+    };
+    # block actually attempts to get cert, leaving us with whatever minica self-signed
+    systemd.services."acme-ds-ws-colemickens.mickens.us" = {
+      enable = false;
+      wantedBy = lib.mkForce [ ];
+    };
+    # acme-ds-ws-colemickens.mickens.us.timer
+    systemd.timers."acme-ds-ws-colemickens.mickens.us" = {
+      enable = false;
+      timerConfig = lib.mkForce {};
+      wantedBy = lib.mkForce [];
     };
   };
 }
