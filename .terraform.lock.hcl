040efef3290cb85a24e825207b85fbeeec1a7787
Cole Mickens <cole.mickens@gmail.com>
2025-01-03 11:12:47 -0800

    cloud: drop terraform shit

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -2,8 +2,8 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/aws" {
-  version = "5.72.0"
+  version = "5.80.0"
   hashes = [
-    "h1:sdwaMgAYWeaF+3rCzxocmqW+c3nGZs8NpfXqPnUayWo=",
+    "h1:/Y+N7ayOrqDujsz5h+YJoIp4+y5jucZs9z4Fakm2kCY=",
   ]
 }
--- cloud/default.nix
+++ cloud/default.nix
@@ -18,9 +18,12 @@ in
     type = "app";
     program =
       (pkgs.writeShellScript "apply" ''
+        set -x
+        set -euo pipefail
+
         if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
         cp ${terraformConfiguration} config.tf.json \
-          && ${terraform}/bin/terraform init \
+          && ${terraform}/bin/terraform init -upgrade \
           && ${terraform}/bin/terraform apply
       '').outPath;
   };
@@ -28,9 +31,12 @@ in
     type = "app";
     program =
       (pkgs.writeShellScript "destroy" ''
+        set -x
+        set -euo pipefail
+
         if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
         cp ${terraformConfiguration} config.tf.json \
-          && ${terraform}/bin/terraform init \
+          && ${terraform}/bin/terraform init -upgrade \
           && ${terraform}/bin/terraform destroy
       '').outPath;
   };
