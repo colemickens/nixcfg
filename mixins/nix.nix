76d4032e5b3f7064decc9443f0eec03167e89b22
Cole Mickens <cole.mickens@gmail.com>
2025-03-21 11:48:08 -0300

    mixins/nix: add flakehub cache

--- mixins/nix.nix
+++ mixins/nix.nix
@@ -56,12 +56,21 @@ in
           "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+          "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+          "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
+          "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
+          "cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU="
+          "cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8="
+          "cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ="
+          "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
+          "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
         ];
         trusted-substituters = [
           "https://cache.nixos.org"
           "https://colemickens.cachix.org"
           "https://nix-community.cachix.org"
           "https://cosmic.cachix.org"
+          "https://cache.flakehub.com"
         ];
         substituters = trusted-substituters;
         trusted-users = [
@@ -71,7 +80,7 @@ in
         ];
       };
       extraOptions = ''
-        experimental-features = nix-command flakes recursive-nix
+        experimental-features = nix-command flakes
         !include ${config.sops.secrets.nix-access-tokens.path}
       '';
     };
