33371522787fb56e41485140b167f7f628de8b9f
Cole Mickens <cole.mickens@gmail.com>
2025-06-09 19:27:44 -0700

    tree: remove del_modules

--- del_modules/cloud/init/oci-nixos-install.sh
+++
@@ -1,6 +0,0 @@
-# the netboot payload will
-# have checked to see if we need
-# to just boot or not
-
-# if not, this is what would run
-# for now, do manually & copy it in
--- del_modules/cloud/userdata/ubuntu-nixos-infect/infect.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-# we would vendor this in, but our terraform/hcl workaround isn't good enough yet:
-
-curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect \
-  | NIX_CHANNEL=nixos-21.11 bash -x
