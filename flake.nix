01db9f5a2e1d3ce7aba1c20194712e7e4058e5f4
Cole Mickens <cole.mickens@gmail.com>
2025-06-09 18:03:30 -0600

    flake.nix: re-introduce nixConfig

--- flake.nix
+++ flake.nix
@@ -82,7 +82,14 @@
     };
   };
 
-  # TODO: re-investigate nixConfig, maybe it will be less soul-crushing one day
+  nixConfig = {
+    builers-use-substitutes = true;
+    build-cores = 0;
+    narinfo-cache-negative-ttl = 0;
+    extra-substituters = "https://colemickens.cachix.org https://cache.flakehub.com";
+    extra-trusted-public-keys = "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4= cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y=";
+    always-allow-substitutes = true;
+  };
 
   ## OUTPUTS ##################################################################
   outputs =
