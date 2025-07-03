1e8e7d294689fc0fcf7f183c2144bcca08da9e4f
Cole Mickens <cole.mickens@gmail.com>
2025-07-02 20:01:19 -0700

    mixins/jj: steal some config form cole-h

--- mixins/jujutsu.nix
+++ mixins/jujutsu.nix
@@ -1,4 +1,4 @@
-{ config, inputs, ... }:
+{ config, ... }:
 
 {
   config = {
@@ -32,6 +32,40 @@
               # pager = ":builtin";
             };
             template-aliases = {
+            };
+            templates = {
+              # yoink: https://github.com/cole-h/nixos-config/blob/71f2eb15b91cb7d9e8de2fa403c5842a5b8761d5/users/_common/vin/modules/jj/conf.d/templates.toml
+              draft_commit_description = ''
+                concat(
+                  description,
+                  surround(
+                    "\nJJ: This commit contains the following changes:\n", "",
+                    indent("JJ:     ", diff.stat(72)),
+                  ),
+                  "\n",
+                  "JJ: ignore-rest\n",
+                  diff.git(),
+                )
+              '';
+              log_node = ''
+                coalesce(
+                  if(!self, label("elided", "~")),
+                  label(
+                    separate(" ",
+                      if(current_working_copy, "working_copy"),
+                      if(immutable, "immutable"),
+                      if(conflict, "conflict"),
+                    ),
+                    coalesce(
+                      if(current_working_copy, "@"),
+                      if(immutable, "◆"),
+                      if(conflict, "×"),
+                      if(empty, "◌"),
+                      "○",
+                    )
+                  )
+                )
+              '';
             };
             "--scope" = [
               {
