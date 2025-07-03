0690cc44b9c69c8d6b053dd6a63c5731a4b0b59c
Cole Mickens <cole.mickens@gmail.com>
2025-07-02 20:32:14 -0700

    mixins/nix: cleanup/simply nix conf

--- mixins/nix.nix
+++ mixins/nix.nix
@@ -22,25 +22,18 @@
     };
     nix = {
       nixPath = lib.mkForce [ "nixpkgs=${inputs.cmpkgs.outPath}" ];
-      settings = rec {
+      settings = {
         keep-derivations = true; # this is the default (?)
         builders-use-substitutes = true;
         cores = lib.mkDefault 0;
         max-jobs = lib.mkDefault "auto";
         use-xdg-base-directories = true;
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        extra-trusted-public-keys = [
           "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         ];
-        trusted-substituters = [
-          "https://cache.nixos.org"
+        extra-trusted-substituters = [
           "https://colemickens.cachix.org"
-          "https://nix-community.cachix.org"
-          "https://cosmic.cachix.org"
         ];
-        substituters = trusted-substituters;
         trusted-users = [
           "@wheel"
           "cole"
