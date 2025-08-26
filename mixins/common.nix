c83a9d8e3be6c1c2e43dd1cef4013cb7beeb7c2e
Cole Mickens <cole.mickens@gmail.com>
2025-08-26 13:20:43 -0700

    mixins/common: zfs supports 6.16, bump zeph to 6.16

--- mixins/common.nix
+++ mixins/common.nix
@@ -11,7 +11,7 @@ let
   _kernelPackages =
     if cfg.useZfs then
       if cfg.useZfsUnstable then
-        pkgs.linuxKernel.packages.linux_6_15
+        pkgs.linuxKernel.packages.linux_6_16
       else
         pkgs.linuxKernel.packages.linux_6_12 # new LTS
     else
