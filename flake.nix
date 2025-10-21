1babf863360c12ff77c22dd04ac68bf80355f0e3
Cole Mickens <cole.mickens@gmail.com>
2025-10-21 10:49:12 -0400

    tree: drop ds-ws-colemickens

--- flake.nix
+++ flake.nix
@@ -102,9 +102,6 @@
       ## NIXOS CONFIGS + TOPLEVELS ############################################
       nixosConfigsEx = {
         "x86_64-linux" = {
-          ds-ws-colemickens = {
-            pkgs = inputs.secure-pkgs;
-          };
           raisin = { };
           slynux = { };
           zeph = { };
