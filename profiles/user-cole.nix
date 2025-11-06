2f28994f1981a927a49441d73807e08b933b01e7
Cole Mickens <cole.mickens@gmail.com>
2025-11-05 22:08:30 -0600

    profiles/user-cole: add bitwarden colemickens-sshkey

--- profiles/user-cole.nix
+++ profiles/user-cole.nix
@@ -19,6 +19,8 @@ in
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCt/htvFKej/E03dE+4aYDe5HpbGhW7dJm+RBUl+gUFNyQcT9zwefQ43q+Du7KeFHbU6XB0/w/BKz/BrhdZbecC9eJ+3FN6n/yEwWtxPdcZOUyanYU0q0QvwFIFpHF75qWAwnssKLOm2gFYDJ+KKtbzXfVgJ/S3Rm6wCnAVusIYlir3laybgLeQfIPsaAJZTTaL9z8DBYcLQ7FUAgkK6qjr/fkACB056zvRJLQPPyw/I4+Jmc9Qhzj3iS4lrDe+OkpucGVmtn1ub06GOzZayqj11e2Ur/1ZDeWxcYkbfCSamlVTVmNdP+o/6pHbOzjz85TrAQqOlkTkV4T0zyqQ85Fdgmpw5T7q+NhUCnT/86fmqoo+wFl90tUq2uyNyAyiTwxwq4WQ/G6fGcbD1oZ85+XTpFuz1Rehfn/w/Jnq6jdj+Cnf3oIoUM4JCcgrMRtpSItbcKVIJoLrFF0sxA++7mSzXrdmfX8pXEopyF4YaZnwq1xMqgCeX1Yqhs/0KI3LJndnLjXR/AIENtFZpWFDj4KbcnDhg+0ZYxQWdArfhXJUFnkzge4hdTdUy0qcg7YLrtT8hNUdYNVIfUYBYlnuUztYRWAtMeVaXIy5gxMcoQCI16Tr0iN8tM9g7l8ibj6Iabmr4/Xb+BAgCMRDxIuxc7cWaZCT3io2qtH7WC33odv2w== hydra@azdev"
         # /run/secrets/github-colebot-sshkey.pub
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPZvfolmlmFF8I+wwLbBRfDJjCIhpJjVBb5uQ9ePCgvP github-colebot-sshkey"
+        # bitwarden colemickens-sshkey
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIK7kPNqHXubFXq4k+15xz9ICn7IBd3Qfz7cawBsRzEO colemickens-sshkey"
       ];
       #mkpasswd -m sha-512
       hashedPassword = "$6$k.vT0coFt3$BbZN9jqp6Yw75v9H/wgFs9MZfd5Ycsfthzt3Jdw8G93YhaiFjkmpY5vCvJ.HYtw0PZOye6N9tBjNS698tM3i/1";
