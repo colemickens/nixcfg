c97654a155d21f06e323dc055d35b6fe7dcdc93a
Cole Mickens <cole.mickens@gmail.com>
2026-03-03 16:04:07 -0300

    tree: remove rust-overlay

--- flake.nix
+++ flake.nix
@@ -30,10 +30,6 @@
     ghostty.url = "github:ghostty-org/ghostty";
     ghostty.inputs."nixpkgs".follows = "cmpkgs";
 
-    # more dev tools:
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "cmpkgs";
-
     # wip replacement for nixpkgs->github-runners module
     nixos-github-actions.url = "github:colemickens/nixos-github-actions";
     nixos-github-actions.inputs."nixpkgs".follows = "cmpkgs";
