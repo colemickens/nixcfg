9a484666c4d6a18911b0efde5b68e3a0b175fe75
Cole Mickens <cole.mickens@gmail.com>
2026-07-22 22:59:18 -0600

    tree: shift to upstream nixos-hardware/k3

--- flake.nix
+++ flake.nix
@@ -14,8 +14,6 @@
     sops-nix.inputs."nixpkgs".follows = "cmpkgs";
     lanzaboote.url = "github:nix-community/lanzaboote";
 
-    nixos-hardware-k3.url = "github:liberodark/nixos-hardware?ref=spacemit-k3";
-
     ucodenix.url = "github:e-tho/ucodenix";
 
     nix-darwin.url = "github:nix-darwin/nix-darwin";
@@ -151,6 +149,7 @@
         aarch64-darwin = { };
         riscv64-linux = {
           installer = nixosConfigurations.installer-riscv64.config.system.build.toplevel;
+          installer-iso = nixosConfigurations.installer-riscv64.config.system.build.isoImage;
           iso-bits = nixosConfigurations.installer-riscv64.config.system.build.isoImage.inputDerivation;
         };
       };
