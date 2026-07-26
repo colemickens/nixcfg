4862512726446fadac973d8b12dc6a49ebb4db47
Cole Mickens <cole.mickens@gmail.com>
2026-07-25 21:52:22 -0500

    hosts/jupitertwo: add github-act-runner

--- hosts/jupitertwo/configuration.nix
+++ hosts/jupitertwo/configuration.nix
@@ -22,7 +22,7 @@ in
   ];
 
   # nix build --narinfo-cache-negative-ttl 0 -j0 github:colemickens/nixcfg/9a8c9fa3#toplevels.jupitertwo --extra-experimental-features nix-command --extra-experimental-features flakes --option netrc-file /nix/var/determinate/netrc --extra-substituters 'https://cache.flakehub.com' --extra-trusted-public-keys 'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y='
-  # nix-store -r /nix/store/n1bqljihh24pnglpfbli0ps7c2bzic0y-nixos-system-jupitertwo-latest --option netrc-file /nix/var/determinate/netrc --extra-substituters 'https://cache.flakehub.com' --extra-trusted-public-keys 'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y='
+  # nix-store -r /nix/store/x6dp7ns41xy42s5fp9q55bixj0blrcc9-nixos-system-jupitertwo-latest --option netrc-file /nix/var/determinate/netrc --extra-substituters 'https://cache.flakehub.com' --extra-trusted-public-keys 'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y='
   config = {
     nixpkgs.hostPlatform.system = "riscv64-linux";
     system.stateVersion = "26.05";
@@ -45,6 +45,7 @@ in
 
     environment.systemPackages = with pkgs; [
       buildkite-agent
+      github-act-runner
       helix
 
       # other firmware/sdcard bits; prebuild for when HW arrives
