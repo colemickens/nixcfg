ff9ff4bd5c8fe414f335a03938309fe4e0c7ea54
Cole Mickens <cole.mickens@gmail.com>
2026-07-25 20:16:46 -0500

    hosts/jupitertwo: add helix

--- hosts/jupitertwo/configuration.nix
+++ hosts/jupitertwo/configuration.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, inputs, ... }:
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
 
 let
   hn = "jupitertwo";
@@ -17,19 +22,13 @@ in
   ];
 
   # nix build github:colemickens/nixcfg#toplevels.jupitertwo --extra-experimental-features nix-command --extra-experimental-features flakes --option netrc-file /nix/var/determinate/netrc --extra-substituters 'https://cache.flakehub.com' --extra-trusted-public-keys 'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y='
-  # nix-store -r  --option netrc-file /nix/var/determinate/netrc --extra-substituters 'https://cache.flakehub.com' --extra-trusted-public-keys 'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y='
+  # nix-store -r /nix/store/n1bqljihh24pnglpfbli0ps7c2bzic0y-nixos-system-jupitertwo-latest --option netrc-file /nix/var/determinate/netrc --extra-substituters 'https://cache.flakehub.com' --extra-trusted-public-keys 'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y='
   config = {
     nixpkgs.hostPlatform.system = "riscv64-linux";
     system.stateVersion = "26.05";
 
     nix.settings = {
-      keep-derivations = true; # this is the default (?)
-      builders-use-substitutes = true;
-      cores = lib.mkDefault 0;
-      max-jobs = lib.mkDefault "auto";
-      use-xdg-base-directories = true;
       extra-trusted-public-keys = [
-        "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4="
         "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
         "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
         "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
@@ -39,30 +38,20 @@ in
         "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
         "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
       ];
-      extra-substituters = [
-        "https://colemickens.cachix.org"
-        "https://cache.flakehub.com"
-      ];
-      extra-trusted-substituters = [
-        "https://colemickens.cachix.org"
-        "https://cache.flakehub.com"
-      ];
+      extra-substituters = [ "https://cache.flakehub.com" ];
+      extra-trusted-substituters = [ "https://cache.flakehub.com" ];
       netrc-file = "/nix/var/determinate/netrc";
-      trusted-users = [
-        "@wheel"
-        "cole"
-        "root"
-      ];
     };
 
     environment.systemPackages = with pkgs; [
       buildkite-agent
+      helix
 
       # other firmware/sdcard bits; prebuild for when HW arrives
-      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/edk2.nix" {})
-      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/fsbl.nix" {})
-      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/opensbi.nix" {})
-      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/uboot.nix" {})
+      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/edk2.nix" { })
+      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/fsbl.nix" { })
+      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/opensbi.nix" { })
+      (pkgs.callPackage "${inputs.nixos-hardware}/spacemit/k3-pico-itx/uboot.nix" { })
     ];
 
     time.timeZone = "America/Chicago";
