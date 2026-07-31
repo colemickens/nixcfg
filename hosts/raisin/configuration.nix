88e409a886fd314615aea26ed36b7608dab6335e
Cole Mickens <cole.mickens@gmail.com>
2026-07-30 21:50:41 -0500

    hosts/raisin: add devtools...

--- hosts/raisin/configuration.nix
+++ hosts/raisin/configuration.nix
@@ -13,6 +13,7 @@ in
 
     ../../profiles/interactive.nix
     ../../profiles/addon-laptop.nix
+    ../../profiles/addon-devtools.nix
 
     ../../mixins/syncthing.nix
     ../../mixins/tailscale.nix
