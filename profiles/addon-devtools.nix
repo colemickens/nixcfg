c17c514c4aa7ac755ad8756c9891ee7d7671c173
Cole Mickens <cole.mickens@gmail.com>
2026-08-09 15:13:20 -0500

    addon-devtools: more tools

--- profiles/addon-devtools.nix
+++ profiles/addon-devtools.nix
@@ -19,11 +19,15 @@
 
             radicle-node
 
+            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-code
+            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.copilot-cli
+            
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.antigravity-cli
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.goose-cli
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.pi
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.nanocoder
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode
+            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode2
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.omp
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.hermes-agent
             kraken-cli
