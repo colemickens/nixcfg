2b660578114fb132a50ef1cd0be2d16c38c9ac8b
Cole Mickens <cole.mickens@gmail.com>
2026-08-13 10:35:40 -0500

    jupitertwo: wip iteration

--- build.sh
+++ build.sh
@@ -15,35 +15,18 @@ overrides=(
   --override-input determinate/determinate-nixd ~/work/code/determinate-nixd
   --override-input determinate/determinate-nixd/fenix ~/work/code/fenix
   --override-input determinate/nix ~/work/code/nix-src
-  --override-input determinate/nixpkgs ~/code/nixpkgs
+  --override-input determinate/nixpkgs nixpkgs-riscv
 )
 thing=".#toplevels.jupitertwo-native"
 
-nix copy --to "${nixremote}" --derivation --no-check-sigs "${thing}.drvPath"
-# ssh "${remote}" nix build -L --keep-going --print-out-paths "${things}^*"
+drv="$(nix eval "${overrides[@]}" --raw "${thing}.drvPath")"
+nix copy --to "${nixremote}" --derivation --no-check-sigs "${drv}"
 
-### OPTION 1 (this is sooo frustrating, the server/client auth stuff is so limiting)
-# nix build -L --keep-going --keep-failed --print-out-paths -j0 \
-#   --builders "${nixremote} riscv64-linux" \
-#   "${overrides[@]}" "${things[@]}"
-# exit 0
+ssh "${remote}" nix-build --dry-run "${drv}"
+exit 0
 
-### OPTION 2 (doesn't --keep-building)
-# nix build -L --keep-going --eval-store auto -vvv \
-#   --store "${nixremote}" \
-#   "${overrides[@]}" "${things[@]}"
-# exit 0
-
-### OPTION 3 (yay, lol)
-# note: you can't `nix copy .drvPath --derivation` because it ALSO complains!!
-# one="$(nix eval --raw "${overrides[@]}" ".#packages.riscv64-linux.determinate-nix-cli.drvPath")"
-# two="$(nix eval --raw "${overrides[@]}" ".#packages.riscv64-linux.determinate-nixd.drvPath")"
-# tre="$(nix eval --raw "${overrides[@]}" ".#packages.riscv64-linux.magic-nix-cache.drvPath")"
-
-# nix copy --to "${nixremote}" --derivation --no-check-sigs \
-#   "${one}" \
-#   "${two}" \
-#   "${tre}"
-
-# nix build -L --keep-going --keep-failed --print-out-paths \
-#   "${one}^*" "${two}^*" "${tre}^*"
+ssh "${remote}" <<EOF
+  zellij attach --create-background jupitertwo
+  echo "layout { pane command=\"nix\" { args \"build\" \"-L\" \"--keep-going\" \"--keep-failed\" \"--print-out-paths\" \"-j2\" \"${drv}^*\"; }; }" >/tmp/task.kdl
+  zellij --session jupitertwo action new-tab --layout /tmp/task.kdl
+EOF
