bdb86f5f1adf6d1653902e2fcdaca4157e06937f
Cole Mickens <cole.mickens@gmail.com>
2026-08-14 13:31:06 -0500

    addon-devtools: add codex, herdr

--- profiles/addon-devtools.nix
+++ profiles/addon-devtools.nix
@@ -20,15 +20,17 @@
             radicle-node
 
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-code
+            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.codex
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.copilot-cli
             
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.antigravity-cli
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.goose-cli
+            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.herdr
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.pi
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.nanocoder
+            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.omp
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode2
-            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.omp
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.hermes-agent
 
             # mcp servers
