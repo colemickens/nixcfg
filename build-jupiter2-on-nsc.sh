2aa2935ff0a6bc512c63eeda651df577c0915867
Cole Mickens <cole.mickens@gmail.com>
2026-09-01 14:07:36 +0100

    jupitertwo: misc iter

--- build-jupiter2-on-nsc.sh
+++ build-jupiter2-on-nsc.sh
@@ -6,7 +6,8 @@ set -euo pipefail
 remote="runner@rendezvous.namespace.so"
 nixremote="ssh-ng://${remote}"
 
-export NIX_SSHOPTS="-p 40379"
+nixsshopts=("-p" "39073")
+export NIX_SSHOPTS="${nixsshopts[@]}"
 export SSH_AUTH_SOCK="/var/run/com.apple.launchd.yglq3pjw10/Listeners"
 
 overrides=(
@@ -23,5 +24,10 @@ drv1="$(nix eval --raw "${overrides[@]}" ".#toplevels.jupitertwo.drvPath")"
 
 nix copy --to "${nixremote}" --derivation --no-check-sigs "${drv1}"
 
-ssh -p 40379 "${remote}" \
-  nix build -L --keep-going --keep-failed --print-out-paths "'${drv1}^*'"
+cat /nix/var/determinate/netrc \
+  | ssh "${nixsshopts[@]}" "${remote}" \
+    'sudo tee /nix/var/determinate/netrc'
+ssh "${nixsshopts[@]}" "${remote}" sudo mv /tmp/netrc /nix/var/determinate/netrc
+
+ssh "${nixsshopts[@]}" "${remote}" \
+  nix build -L --keep-going --keep-failed --print-out-paths --option netrc-file /nix/var/determinate/netrc --extra-substituters \'https://cache.flakehub.com https://edge.cache.flakehub.com/\' --extra-trusted-public-keys \'cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y=\' "${drv1}^*"
