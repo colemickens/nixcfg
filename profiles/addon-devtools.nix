6176b6bb3cd4d4e7ccc83848ffe9e736391bd946
Cole Mickens <cole.mickens@gmail.com>
2026-09-20 21:18:03 +0200

    addon-devtools: drop nanocoder

--- profiles/addon-devtools.nix
+++ profiles/addon-devtools.nix
@@ -32,7 +32,6 @@
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.herdr
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.hermes-agent
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.pi
-            inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.nanocoder
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.omp
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode
             inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode2
